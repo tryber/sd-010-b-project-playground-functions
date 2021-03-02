49072b3b0351eeb9047bd4b2c8ff6cf732bb964e
Joao Vanelli <vanellijoao@gmail.com>
2021-03-02 16:06:17 -0300

    Requisito 5 - footballPoints

--- src/challenges.js
+++ src/challenges.js
@@ -16,17 +16,17 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  return `${array[array.length-1]}, ${array[0]}`
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return ((wins * 3) + (ties));
 }
 
 // Desafio 6
