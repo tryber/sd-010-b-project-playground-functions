4008ade7a53e70c68e9b50f0b60f1414edf10147
Leandro Reis <lleandrofr@gmail.com>
2021-03-02 18:32:27 -0300

    Resolvendo requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,52 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(skill, name) {
+
+  let list1 = {
+    tech: skill[0],
+    name: name,
+  }
+  let list2 = {
+    tech: skill[1],
+    name: name,
+  }
+  let list3 = {
+    tech: skill[2],
+    name: name,
+  }
+  let list4 = {
+    tech: skill[3],
+    name: name,
+  }
+  let list5 = {
+    tech: skill[4],
+    name: name,
+  }
+
+  let arrayList = [list1, list2, list3, list4, list5];
+  return arrayList;
+
 }
+console.log(techList(["React", "Jest", "HTML"], 'Leandro'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 11
 function generatePhoneNumber() {
