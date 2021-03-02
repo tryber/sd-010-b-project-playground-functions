caa6d22c1634089a46f963e5d18018fb34d1a925
Roger Franco <rogerfrancogalt@gmail.com>
2021-03-02 14:51:57 -0400

    primeiro exercicio

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 const { TestScheduler } = require("jest");
 
 // Desafio 1
-function compareTrue() {
-  if (true && true) {
+function compareTrue(valor1, valor2) {
+  if (valor1 == true && valor2 == true) {
     return true;
   } else {
     return false;
