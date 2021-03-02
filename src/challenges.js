231caf134e4987de5fc93db2a9acea14bfcc703e
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-02 15:46:51 -0300

    Mudando o ==

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,12 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  if (valor1 === false && valor2 !== true) {
+  if (valor1 === true && valor2 === true) {
     return true;
   } else {
     return false;
   }
 }
+
 // Desafio 2
 function calcArea() {
   // seu código aqui
