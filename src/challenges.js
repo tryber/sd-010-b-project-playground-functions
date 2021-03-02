b6e1df704d8fa70e8115c24c0a12ce2291c8357e
renanpbraga <renanpbraga@gmail.com>
2021-03-02 16:46:33 -0300

    adiciona desafio1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,14 @@
 // Desafio 1
-function compareTrue() {
-  // seu código
+function compareTrue(num1, num2) {
+  if (num1 % 2 == 0 && num2 % 2 == 0) {
+    return "true";
+  } else {
+    return "false";
+  }
 }
+
+let verificaNum = compareTrue(10, 20);
+console.log (verificaNum);
 
 // Desafio 2
 function calcArea() {
