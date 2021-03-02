282230e24fe62a676421298d970ae20b1776fd76
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-02 18:36:06 -0300

    cria função 2

--- src/challenges.js
+++ src/challenges.js
@@ -10,10 +10,15 @@ function compareTrue(info1, info2) {
 }
 console.log(compareTrue('maria','maria'));
 
-/* // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+// Desafio 2
+function calcArea(base,heigth) {
+    let triangleArea = (base*heigth)/2;
+    return triangleArea;
+} 
+console.log(calcArea(10,50));
+console.log(calcArea(5,2));
+console.log(calcArea(51,1));
+
 
 // Desafio 3
 function splitSentence() {
