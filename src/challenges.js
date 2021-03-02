3ea2e23b01096bbe250147e34c7c3671d5e03dbe
bidaffonso <bidaffondo@gmail.com>
2021-03-02 14:29:54 -0400

    segundo commit resolvendo requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(valor1, valor2) {
+  if (valor1 == true && valor2 == true){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 // Desafio 2
