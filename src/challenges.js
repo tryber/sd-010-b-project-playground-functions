2d9242cd41a758a2f41be4addc3a073ccf942ac7
riquee <strikedv@hotmail.com>
2021-03-02 20:15:18 -0300

    Desafio 8 finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -26,8 +26,8 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-// Referência: https://pt.stackoverflow.com/
-  let maior = (Math.max.apply(null, array));
+  // Referência: https://pt.stackoverflow.com/
+  let maior = Math.max.apply(null, array);
   let cont = 0;
   for (let index = 0; index <= array.length; index += 1) {
     if (array[index] === maior) {
@@ -38,20 +38,35 @@ function highestCount(array) {
 }
 
 // Desafio 7
+// REFERÊNCIA: https://developer.mozilla.org/
 function catAndMouse(mouse, cat1, cat2) {
-  let catt2 = cat2 - mouse;
-  cat1 = mouse - cat1;
-  if (cat1 < mouse) {
+  cat1 = Math.abs(cat1 - mouse);
+  cat2 = Math.abs(cat2 - mouse);
+
+  if (cat1 < cat2) {
     return 'cat1';
   }
-  if (catt2 > mouse) {
+  if (cat2 < cat1) {
     return 'cat2';
   }
   return 'os gatos trombam e o rato foge';
 }
+
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let string = [];
+  for (let index = 0; index <= array.length - 1; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      string.push('fizzBuzz');
+    } else if (array[index] % 3 === 0) {
+      string.push('fizz');
+    } else if (array[index] % 5 === 0) {
+      string.push('buzz');
+    } else {
+      string.push('bug!');
+    }
+  }
+  return string;
 }
 
 // Desafio 9
