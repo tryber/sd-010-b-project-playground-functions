e62af48b9ffa9e2159ac21e085c3ce0e55f334dc
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-02 16:22:27 -0300

    adicionei resposta escrita

--- src/challenges.js
+++ src/challenges.js
@@ -2,13 +2,13 @@
 function compareTrue(boo1, boo2) {
   let verdadeFalso = boo1 && boo2;
   return verdadeFalso;
-}
+}// Resposta: Criei dois parametros na função e comparei esses dois parametros na variavel verdadeiroFalso, Dei um return nessa variavel.
 
 // Desafio 2
 function calcArea(base, height) {
   let triangulo = (base * height) / 2;
   return triangulo;
-}
+}// Resposta: Fiz a formula dentro da variavel trangulo e dei um return nessa variavel.
 
 // Desafio 3
 function splitSentence() {
