e107b52ba968991bdf300f983794d6aafe473004
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-02 18:54:08 -0300

    Resolvendo requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -28,7 +28,7 @@ function footballPoints(wins, ties) {
   let pontos = (wins * 3) + ties;
   return pontos;
 }
-let array = [0, 0, 0];
+
 // Desafio 6
 function highestCount(array) {
   let count = 0;
@@ -44,18 +44,41 @@ function highestCount(array) {
     }
   } 
   return count;
-} console.log(highestCount(array));
-
+}
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = mouse - cat1; // 2
+  let distanciaCat2 = mouse - cat2; // -6
+  if (distanciaCat1 < distanciaCat2) {
+    return "cat1";
+  } 
+  else if (distanciaCat2 < distanciaCat1) {
+    return "cat2";
+  }
+  else {
+    return "os gatos trombam e o rato foge";
+  }
+} 
 
+let array = [2, 15, 7, 9, 45];
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+function fizzBuzz(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 == 0 && array[index] % 5 == 0 ) {
+      return "fizzBuzz";
+    }
+    else if (array[index] % 5 == 0) {
+      return "buzz";
+    }
+    else if (array[index] % 3 == 0) {
+      return "fizz";
+    } 
+    else {
+      return "bug!";
+    }
+  }
+} console.log(fizzBuzz(array));
 
 // Desafio 9
 function encode() {
