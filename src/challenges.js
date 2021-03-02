0d799b232430032b9535ad984a2b02e03e4c4789
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-02 20:34:40 -0300

    Corrigindo lint

--- src/challenges.js
+++ src/challenges.js
@@ -34,12 +34,12 @@ function highestCount(array) {
   let maiorNumero = 0;
   let repete = 0;
 
-  for (let index = 0; index < array.length; index++) {
+  for (let index = 0; index < array.length; index+=1) {
     if (array[index] >= maiorNumero) {
       maiorNumero = array[index];
     }
   }
-  for (let index = 0; index < array.length; index++) {
+  for (let index = 0; index < array.length; index+=1) {
     if (maiorNumero === array[index]) {
       repete += 1;
     }
