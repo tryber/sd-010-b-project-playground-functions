b1486d63a03a6904db79340afcbaf62fab376184
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-02 18:27:04 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(tech, name) {
+  let novaArray = tech.sort();
+  let objeto = [];
+  if (tech.length === 0) {
+    return "Vazio!"
+  }
+  for (let i = 0; i < novaArray.length; i += 1) {
+    objeto[i] = {
+      tech: novaArray[i],
+      name: name
+    }
+  }
+  return objeto;
+} console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
