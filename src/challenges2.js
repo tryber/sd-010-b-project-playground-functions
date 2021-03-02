bbc1b2d2e61e8e3fb3b64e23be7ef961ab7ec5d2
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-02 15:30:30 -0300

    Finalizando Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,19 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+// Requisito 10 - Receber um array de tecnologias e nomes para criar um novo array com objetos possuindo campos tech e name.
+//    - Ordenar novo array a partir do campo tech
+//    - Se array vazio retornar 'Vazio!'
+function techList(array, name) {
+  if (array.length === 0) return 'Vazio!';
+  array = array.sort();
+  let list = [];
+  for (const tech in array) {
+    list.push({ tech: array[tech], name });
+  }
+  return list;
 }
+console.log(
+  techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Gustavo')
+); // -> [{ tech: 'CSS', name: 'Gustavo' },{ tech: 'HTML', name: 'Gustavo' },{ tech: 'JavaScript', name: 'Gustavo' },{ tech: 'Jest', name: Gustavo' },{ tech: 'React', name: 'Gustavo' }]
+console.log(techList([], 'Gustavo')); // -> 'Vazio!'
 
 // Desafio 11
 function generatePhoneNumber() {
