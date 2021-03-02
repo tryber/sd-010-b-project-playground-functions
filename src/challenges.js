aab7ece6899c7886db7df8badfb4a647c4c1006b
Malbathan Lima <malbathanlima@gmail.com>
2021-03-02 16:44:01 -0300

    Calculando a area de um triangulo

--- src/challenges.js
+++ src/challenges.js
@@ -6,14 +6,17 @@ function compareTrue(bool1, bool2) {
     return false;
   }
 }
-console.log (compareTrue (false, true));
-console.log (compareTrue (false, false));
-console.log (compareTrue (true, true));
+console.log (compareTrue(false, true));
+console.log (compareTrue(false, false));
+console.log (compareTrue(true, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
 }
+console.log (calcArea (10, 50));
+console.log (calcArea (5, 2));
+console.log (calcArea (51, 1));
 
 // Desafio 3
 function splitSentence() {
