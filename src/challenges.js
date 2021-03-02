fd550fb09ab6c257990fb7caf13ee495e4b03cd9
joao966 <joaooliveira966@gmail.com>
2021-03-02 20:42:42 -0300

    volta ai requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -34,50 +34,22 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount() {
-  let higherNumber = 0;
-  let counter = 0;
-  for (const i in numbers) {
-    if (numbers[i] > higherNumber) {
-      higherNumber = numbers[i];
-      counter = 0;
-    }
-  }
-  for (const i in numbers) {
-    if (numbers[i] == higherNumber) {
-      counter += 1;
-    }
-  }
-  return counter;
+  // seu código aqui
 }
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7])); 
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1])); 
-console.log(highestCount([0, 0, 0])); 
 
 // Desafio 7
 function catAndMouse() {
-  
+  // seu código aqui
 }
 
 // Desafio 8
-function fizzBuzz(umaLista) {
-  let out = [];
-  for (let key in umaLista) {
-    if (umaLista[key] % 3 == 0 && umaLista[key] % 5 == 0){
-      out.push("fizzBuzz");
-    }else if (umaLista[key] % 3 == 0){
-      out.push("fizz");
-    }else if (umaLista[key] % 5 == 0){
-      out.push("buzz");
-    }else{
-      out.push("bug!")
-    }
-  }
-  return out;
+function fizzBuzz() {
+  // seu código aqui
 }
 
 // Desafio 9
-function encode(umaFrase) {
-  ////
+function encode() {
+  // seu código aqui
 }
 function decode() {
   // seu código aqui
