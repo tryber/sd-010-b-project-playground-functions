96cad6da545315aa2392085dc22b5196c15041d1
Alan Tanaka <alanctnk@gmail.com>
2021-03-02 14:56:29 -0300

    Preenchendo requisitos 1~3

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,24 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(bool1, bool2) {
+  let value = undefined;
+	if (bool1 && bool2){
+		value = true
+	} else {
+		value = false
+	}
+
+	return value
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base*height)/2
+	return area
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  return str.split(" ")
 }
 
 // Desafio 4
