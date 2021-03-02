4cefda2a13555e86dff27901820cbd10fd2129c7
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-02 16:47:34 -0300

    correçao lint

--- src/challenges.js
+++ src/challenges.js
@@ -1,19 +1,16 @@
 // Desafio 1
-let a = true;
-let b = false;
 function compareTrue(a, b) {
-  // seu código aqui
-  if (a === true && b === true){
+// seu código aqui
+  if (a === true && b === true) {
     return true;
-  }
-  else {
+  }  else {
     return false;
   }
 }
 // console log
-console.log(compareTrue(a, b));
-console.log(compareTrue(a, a));
-console.log(compareTrue(b, b));
+console.log(compareTrue(true, false));
+console.log(compareTrue(true, true));
+console.log(compareTrue(false, false));
 
 // Desafio 2
 function calcArea(base, height) {
@@ -26,20 +23,20 @@ console.log(calcArea(5, 2));
 console.log(calcArea(51, 1));
 
 // Desafio 3
-let splitor = " ";
+let splitor = ' ';
 let arrayString = [];
 function splitSentence(frase) {
   // seu código aqui
-  for (let index = 0; index < frase.length; index++) {
+  for (let index = 0; index < frase.length; index += 1) {
     arrayString = frase.split(splitor);
   }
- return arrayString;
+  return arrayString;
 }
 //
-console.log(splitSentence("foguete"))
-console.log(splitSentence("go Trybe"))
-console.log(splitSentence("vamo que vamo tribo"))
-console.log(splitSentence("O rato roeu a roupa do rei de Roma"))
+console.log(splitSentence('foguete'))
+console.log(splitSentence('go Trybe'))
+console.log(splitSentence('vamo que vamo tribo'))
+console.log(splitSentence('O rato roeu a roupa do rei de Roma'))
 
 // Desafio 4
 function concatName() {
