32312875a5ef7163b26f5e5eb6edc2c3a2268aed
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-02 15:42:59 -0300

    6º Exercicio 'Highest Count' feito

--- src/challenges.js
+++ src/challenges.js
@@ -17,19 +17,17 @@ function splitSentence(string) {
   /* //Jeito simples:
   const arrayOfStrings = string.split("");
   */
-
-  //Jeito menos simples:
+  //Jeito menos simples:  
   let arrayOfStrings = [];
-  let word = "";
+  let word = '';
 
   for (let index in string) {
-    if (string[index] === " ") {
+    if (string[index] === ' ') {
       arrayOfStrings.push(word);
-      word = "";
-      continue;
+      word = '';
+    } else {
+      word += string[index];
     }
-
-    word += string[index];
   }
   arrayOfStrings.push(word);
 
@@ -50,12 +48,39 @@ function footballPoints(wins, ties) {
   return totalPoints;
 }
 
-console.log(footballPoints(14, 8));
-
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(highestCount) {
+  //Procura o maior valor 
+  let highestNum = highestCount.reduce((num1, num2) => {
+    return Math.max(num1, num2)
+  })
+
+  //Filtra os valores que são iguais ao maior valor 
+  let count = highestCount.filter(value => {
+    return value == highestNum;
+  })
+
+  //Retorna o tamanho do array do count 
+  return count.length;
 }
+
+/* function highestCount(arrayOfNum) {
+  let highestNumber = 0;
+  let counter = 0;
+  for (let number in arrayOfNum) {
+    if (arrayOfNum[number] > highestNumber) {
+      highestNumber = arrayOfNum[number];
+    }
+  }
+
+  for (let number in arrayOfNum) {
+    if (highestNumber == arrayOfNum[number]) {
+      counter += 1;
+    }
+  }
+
+  return counter;
+} */
 
 // Desafio 7
 function catAndMouse() {
