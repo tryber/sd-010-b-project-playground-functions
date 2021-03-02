ad2c8c7b63c6e312ca462238bf4bbe33d7599176
Thiago Marchini <thiago@msn.com>
2021-03-02 20:31:09 -0300

    feito o Encode e o Decode do Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -119,10 +119,26 @@ function encode(string) {
   }
   return answer;
 }
-console.log(encode('hi there!'));
+
+function translateNumbers(int) {
+  let table = { 1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u' };
+  for (let key in table) {
+    if (int === key) {
+      return table[key];
+    }
+  }
+}
 
 function decode(string) {
-  // seu código aqui
+  let answer = '';
+  for (let letter in string) {
+    if (translateNumbers(string[letter])) {
+      answer += translateNumbers(string[letter]);
+    } else {
+      answer += (string[letter]);
+    }
+  }
+  return answer;
 }
 module.exports = {
   calcArea,
