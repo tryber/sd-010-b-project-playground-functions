91c34401a355dccceb1ab53bcb90a529c76f7d57
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-03-02 16:16:16 -0300

    desafios 1-5

--- src/challenges.js
+++ src/challenges.js
@@ -43,8 +43,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return ((wins*3)+ties);  
 }
 
 // Desafio 6
