0df6ff6ecdd4367928146d66d305ea0c80d5d595
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-02 18:04:32 -0300

    tentando acerta o desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -45,13 +45,13 @@ function highestCount() {
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
-  if((cat1+mouse)<(cat2+mouse)){
-    return "cat1";
-  }
-  else if ((cat2+mouse)<(cat1+mouse)){
+  if((cat1-mouse)>(cat2-mouse)){
     return "cat2";
   }
-  else if ((cat1+mouse) === (cat2+mouse)){
+  else if ((cat1-mouse)<(cat2-mouse)){
+    return "cat1";
+  }
+  else if ((cat1-mouse) === (cat2-mouse)){
     return "os gatos trombam e o rato foge";
   }
 
