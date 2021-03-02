070f43f3926d67d81a2dcd591d5addf526850563
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-02 14:47:52 -0300

    add desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -13,9 +13,13 @@ console.log(compareTrue(false, false));
 console.log(compareTrue(true, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
