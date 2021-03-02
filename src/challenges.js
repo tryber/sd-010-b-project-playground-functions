4043f7244b4eaf14912c9728c59231ce9adfd1f9
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-02 18:33:24 -0300

    Executado requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
-// Desafio 1
-function compareTrue(boolean1, boolean2) {
+// // Desafio 1
+// function compareTrue() {
  
-}
-console.log(compareTrue(false, true))
-console.log(compareTrue(false, false))
-console.log(compareTrue(true, true))
+// }
+// console.log(compareTrue(false, true))
+// console.log(compareTrue(false, false))
+// console.log(compareTrue(true, true))
 
 // Desafio 2
 function calcArea(base, height) {
@@ -25,14 +25,22 @@ console.log(splitSentence('vamo que vamo'));
 console.log(splitSentence('foguete'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+// function concatName(string) {
+//   let array = (concatName.length - 1) + concatName[0];
+//   return array;
+// }
+// console.log(concatName('Lucas', 'Cassiano', 'Ferraz', 'Paolillo'))
+// console.log(concatName('foguete', 'não', 'tem', 'ré'))
+// console.log(concatName('captain', 'my', 'captain'))
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + (ties * 1);
+  return points;
 }
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
