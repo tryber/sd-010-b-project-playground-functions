9334b41d3e9847252bede219309196d983e2cc4a
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-02 14:41:33 -0300

    Iniciando projeto playground functions

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,12 @@
 // Desafio 1
 function compareTrue() {
-  // seu código aqui
+  let boo1 = true;
+  let boo2 = true;
+  if(boo1 && boo2 === true){
+    return true;
+  }else{
+    return false;
+  }  
 }
 
 // Desafio 2
