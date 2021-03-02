4d0bf909ec0195d543a8d54e39b294cd25095736
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-02 16:08:28 -0300

    footballpoints

--- src/challenges.js
+++ src/challenges.js
@@ -11,9 +11,8 @@ function calcArea(base, height) {
   return (base * height) / 2
 } 
 console.log(calcArea(10, 50));
-console.log(calcArea(5,2));
-console.log(calcArea(51, 1))
-
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 
 // Desafio 3
@@ -21,23 +20,28 @@ function splitSentence(string) {
   let array = string.split(' ');
   return array;
 }
-console.log(splitSentence('go trybe'))
-console.log(splitSentence('vamo que vamo'))
-console.log(splitSentence('foguete'))
+console.log(splitSentence('go trybe'));
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
 
 // Desafio 4
 function concatName(concatString) {
-  let array= concatString.pop();
+  let array = concatString.pop();
   return (array + ', ' + concatString[0]); 
 }
+
 console.log(concatName( ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
+
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0))
 
 // Desafio 6
 function highestCount() {
