5a14e420170bebac0dac3cecac448eca30a9b157
legalvao1 <legalvao1@gmail.com>
2021-03-02 15:23:14 -0300

    Funcao que calcula a area do triangulo

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,18 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a , b) {
   return a && b;
-}
+};
 console.log(compareTrue(true, true));
 console.log(compareTrue(false, false));
 console.log(compareTrue(false, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base , height) {
+  return (base * height) / 2;
+};
+console.log(calcArea(10 , 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51 , 1));
 
 // Desafio 3
 function splitSentence() {
