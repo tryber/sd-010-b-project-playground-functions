b80cc58cc3292be3ea2a941102103fb22a1a498a
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-02 19:10:23 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,15 +8,44 @@ function techList(tech, name) {
   for (let i = 0; i < novaArray.length; i += 1) {
     objeto[i] = {
       tech: novaArray[i],
-      name: name};
+      name: name
+    }
   }
   return objeto;
-} 
+}
 
 // Desafio 11
 function generatePhoneNumber(array) {
- 
-}
+  if (array.length > 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  let num = {};
+  let contaRepetido = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    let valor = array[i];
+    if (num[valor] === undefined) {
+      num[valor] = 1;
+    } else {
+      num[valor] = num[valor] + 1;
+    }
+  }
+  for (let prop in num) {
+    if (contaRepetido < num[prop]) {
+      contaRepetido = num[prop];
+    }
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if ((array[i] < 0) || (array[i] > 9 || (contaRepetido >= 3))) {
+      return 'Não é possível gerar um número de telefone com esses valores.';
+    }
+  }
+  let numTelefone;
+  for (let i = 0; i < array.length; i += 1) {
+    numTelefone = '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10] 
+  }
+  return numTelefone;
+} 
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -30,7 +59,7 @@ function triangleCheck(lineA, lineB, lineC) {
     trianguloPossivel = false;
   }
   return trianguloPossivel;
-} console.log(triangleCheck(10, 14, 8))
+} 
 
 // Desafio 13
 function hydrate() {
