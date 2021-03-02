23c50c394c9f139e41c06e84e29e9dea5ddf6f77
hzozimo <hzozimo@gmail.com>
2021-03-02 20:52:57 -0300

    reenvio

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,13 +12,49 @@ function techList(nomesTecnologias, nome) {
       });
     }
   }
-    return objeto;
+  return objeto;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayDeNumeros) {
+  let saida = `(${arrayDeNumeros[0]}${arrayDeNumeros[1]})${arrayDeNumeros[2]}${arrayDeNumeros[3]}${arrayDeNumeros[4]}${arrayDeNumeros[5]}${arrayDeNumeros[6]}-${arrayDeNumeros[7]}${arrayDeNumeros[8]}${arrayDeNumeros[9]}${arrayDeNumeros[10]}`;
+  saida = tamanho(arrayDeNumeros);
+  for (numero of arrayDeNumeros) {
+    if ((numero < 0) || (numero > 9) || repeticaoDeTresOuMais(arrayDeNumeros)) {
+      saida = 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (saida === ''){
+    
+  }
+  return saida
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+
+function tamanho (arrayDeNumeros) {
+  let saida=`(${arrayDeNumeros[0]}${arrayDeNumeros[1]})${arrayDeNumeros[2]}${arrayDeNumeros[3]}${arrayDeNumeros[4]}${arrayDeNumeros[5]}${arrayDeNumeros[6]}-${arrayDeNumeros[7]}${arrayDeNumeros[8]}${arrayDeNumeros[9]}${arrayDeNumeros[10]}`;
+    if (arrayDeNumeros.length != 11) {
+    saida = 'Array com tamanho incorreto';
+  }
+  return saida;
+}
+
+function repeticaoDeTresOuMais (arrayDeNumeros) {
+  let maisQueTres = false;
+  let contagem ={ }
+  for (let itens of arrayDeNumeros) {
+    contagem[itens] = 0;
+  }
+  for (let itens of arrayDeNumeros) {
+    contagem[itens] += 1;
+  }
+  for (let itens of arrayDeNumeros) {
+    if (contagem[itens] > 3) {
+      maisQueTres = true;
+    }
+  }
+  return maisQueTres;
 }
 
 // Desafio 12
