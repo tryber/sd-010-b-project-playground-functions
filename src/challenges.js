a493c09ef19fda32ad1a207701cc11def1078630
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-02 15:18:05 -0300

    Adiciona o desafio 2 que calcula a area de um triangulo

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,7 @@
 // Desafio 1
 function compareTrue(bool1, bool2) {
   // seu código aqui
-  if(bool1 === true && bool2 === true){
+  if (bool1 === true && bool2 === true){
     return true;
   }else{
     return false;
@@ -10,9 +10,11 @@ function compareTrue(bool1, bool2) {
 
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return area = (base * height) / 2
 }
+
 
 // Desafio 3
 function splitSentence() {
