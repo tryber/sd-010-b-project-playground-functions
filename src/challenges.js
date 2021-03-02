ef700259142f5a1f4a41eee8df7c372def19b0c3
Alan Tanaka <alanctnk@gmail.com>
2021-03-02 18:42:25 -0300

    Preenchendo requisitos 7~10

--- src/challenges.js
+++ src/challenges.js
@@ -53,21 +53,73 @@ function highestCount(arr) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let nearCat = ''
+	if(cat1 === cat2) {
+		nearCat = "os gatos trombam e o rato foge"
+	} else {
+		nearCat = Math.abs(mouse - cat1) < Math.abs(mouse - cat2) ? "cat1" : "cat2"
+	}
+
+	return nearCat
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arr) {
+  let fizzBuzzArr = []
+	for (let index = 0; index < arr.length; index += 1) {
+		if (arr[index]%3 && arr[index]%5){
+			fizzBuzzArr.push("bug!")
+		} else if (arr[index]%3 || arr[index]%5) {
+			fizzBuzzArr.push(arr[index]%3 ? "buzz" : "fizz")
+		} else {
+			fizzBuzzArr.push("fizzBuzz!")
+		}
+	}
+	return fizzBuzzArr
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  let encoderDict = {
+		a: 1,
+		e: 2,
+		i: 3,
+		o: 4,
+		u: 5
+	}
+
+	let arr = str.split("")
+
+
+	for (let index = 0; index < arr.length; index += 1){
+		for (key in encoderDict){
+			arr[index] = arr[index]===key ? encoderDict[key] : arr[index]
+		}
+	}
+
+	return arr.join("")
+
 }
-function decode() {
-  // seu código aqui
+function decode(str) {
+  let decoderDict = {
+		1:"a",
+		2:"e",
+		3:"i",
+		4:"o",
+		5:"u"
+	}
+
+	let arr = str.split("")
+
+
+	for (let index = 0; index < arr.length; index += 1){
+		for (key in decoderDict){
+			arr[index] = arr[index]===key ? decoderDict[key] : arr[index]
+		}
+	}
+
+	return arr.join("")
 }
 
 module.exports = {
@@ -82,3 +134,10 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+// REFERÊNCIAS >>
+
+// https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
+// https://stackoverflow.com/questions/4652104/convert-a-negative-number-to-a-positive-one-in-javascript
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+// https://stackoverflow.com/questions/8260156/how-do-i-create-dynamic-variable-names-inside-a-loop
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arr, name) {
+  if(arr.length === 0) {
+		return "Vazio!"
+	} 
+	
+	let arrSorted = arr.sort()
+	let objArr = []
+	for (let index = 0; index < arrSorted.length; index += 1){		
+		objArr[index] = {}
+		objArr[index]["tech"] = arrSorted[index]
+		objArr[index]["name"] = name
+
+	}
+	return objArr
 }
 
 // Desafio 11
