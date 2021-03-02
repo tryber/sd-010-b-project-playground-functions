7bdf478fc7db04baafa865616b6ec4efb1d3cd67
Fernando <fhomaia@gmail.com>
2021-03-02 16:32:06 -0300

    Adiciona até o requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -14,7 +14,6 @@ function calcArea(base, height) {
 function splitSentence(string) {
   return string.split(' ');
 }
-console.log(splitSentence("go Trybe"))
 
 // Desafio 4
 function concatName(string) {
@@ -22,18 +21,32 @@ function concatName(string) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return ((wins * 3) + (ties * 1));
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(count) {
+  let maior = count[0];
+  let contador = 0;
+  for (index = 0; index < count.length ; index++) {
+    if (maior < count[index]) {
+      maior = count[index];
+      contador = 1;
+    } else if (maior === count[index]) {
+      contador++;
+    }
+  }
+  return contador;
+}  
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs((cat1 - mouse)) < Math.abs((cat2 - mouse))) {
+    return 'cat1';
+  } else if ((cat1 - mouse) > (cat2 - mouse)) {
+    return 'cat2';
+  } return 'os gatos trombam e o rato foge' ;
 }
 
 // Desafio 8
