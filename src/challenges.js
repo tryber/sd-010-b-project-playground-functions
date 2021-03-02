6421e6082a946c190c893d3d3d805d241669e789
riquee <strikedv@hotmail.com>
2021-03-02 19:01:49 -0300

    Exercicio 6 finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(n1, n2) {
-  return (n1 && n2);
+  return n1 && n2;
 }
 
 // Desafio 2
@@ -13,32 +13,42 @@ function splitSentence(string) {
   return string.split(' ');
 }
 
+// Desafio 4
 function concatName(array) {
-  let newArray = `${array[array.length - 1]} ${array[0]}`;
+  let newArray = `${array[array.length - 1]}, ${array[0]}`;
   return newArray;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins * 3) + (ties * 1);
+  return wins * 3 + ties * 1;
 }
 
 // Desafio 6
-function highestCount() {
-}
-// Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  let position;
-  if (cat2 === mouse && cat1 === mouse) {
-    position = 'os gatos trombam e o rato foge';
-  } else if (cat1 > cat2 && cat1 <= mouse) {
-    position = 'cat1';
-  } else {
-    position = 'cat2';
+function highestCount(array) {
+// Referência: https://pt.stackoverflow.com/
+  let maior = (Math.max.apply(null, array));
+  let cont = 0;
+  for (let index = 0; index <= array.length; index += 1) {
+    if (array[index] === maior) {
+      cont += 1;
+    }
   }
-  return position;
+  return cont;
 }
 
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+  let catt2 = cat2 - mouse;
+  cat1 = mouse - cat1;
+  if (cat1 < mouse) {
+    return 'cat1';
+  }
+  if (catt2 > mouse) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
+}
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
