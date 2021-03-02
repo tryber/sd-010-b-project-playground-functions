c8db7055c0aa02abf94ac25c1d209b63b3e6ac24
lucasportella <lucasportellaagu@gmail.com>
2021-03-02 16:09:32 -0300

    requirement 6

--- src/challenges.js
+++ src/challenges.js
@@ -51,8 +51,22 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let biggestNumber = 0;
+  let bigNumberReps = 0;
+  for (let index in array) {
+      if (index == 0) {
+          biggestNumber = array[index]
+      } else if (biggestNumber < array[index]) {
+          biggestNumber = array[index]
+      }
+  }
+  for (let index in array) {
+      if (array[index] == biggestNumber) {
+          bigNumberReps += 1
+      }
+  }
+  return bigNumberReps;
 }
 
 // Desafio 7
--- src/tests.js
+++ src/tests.js
@@ -1,6 +1,19 @@
-function footballPoints(wins, ties) {
-  let winsPoints = wins * 3;
-  let tiesPoints = ties * 1;
-  return (winsPoints + tiesPoints);
-}
-console.log(footballPoints(0,0))
+function highestCount(array) {
+    let biggestNumber = 0;
+    let bigNumberReps = 0;
+    for (let index in array) {
+        if (index == 0) {
+            biggestNumber = array[index]
+        } else if (biggestNumber < array[index]) {
+            biggestNumber = array[index]
+        }
+    }
+    for (let index in array) {
+        if (array[index] == biggestNumber) {
+            bigNumberReps += 1
+        }
+    }
+    return bigNumberReps;
+  }
+  console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
+  
