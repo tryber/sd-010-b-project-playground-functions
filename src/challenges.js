4c37ad0b2cc44fa69e0f698ccf4e310946cc3fe4
Camila Arruda <camilaam1802@gmail.com>
2021-03-02 16:17:26 -0300

    Desafio 4 feito

--- src/challenges.js
+++ src/challenges.js
@@ -1,29 +1,32 @@
 // Desafio 1
 function compareTrue(val1, val2) {
   // seu código aqui
-  if (val1 === true && val2 === true){
-      return true;
-    } 
-      return false;
+  if (val1 === true && val2 === true) {
+    return true;
+  }
+    return false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  let calcularArea = (base * height) / 2; 
-  return calcularArea; 
+  let calcularArea = (base * height) / 2;
+  return calcularArea;
 }
 
 // Desafio 3
 function splitSentence(sentence) {
   // seu código aqui
-  let arrayString = sentence.split(" "); 
-  return arrayString; 
+  let arrayString = sentence.split(' ');
+  return arrayString;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(arrayStrings) {
   // seu código aqui
+  let stringAux = arrayStrings; 
+  let stringConcat = stringAux.pop() + ', ' + stringAux.shift();  
+  return stringConcat;
 }
 
 // Desafio 5
@@ -50,6 +53,7 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
