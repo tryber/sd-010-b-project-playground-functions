e406ae41d6e52bf01cd732221ebf02082d1ece6b
aythan azevedo <aythan16@gmail.com>
2021-03-02 16:09:31 -0300

    quest 3 rascunho

--- src/challenges.js
+++ src/challenges.js
@@ -1,37 +1,51 @@
 // Desafio 1
-function compareTrue(bole1,bole2) {
+function compareTrue(bole1 , bole2) {
   let resultado
-  if (bole1 === true && bole2===true ){
+  if (bole1 === true && bole2 === true ) {
 
-      resultado= true;
-    }else{
-      resultado= false;
+      resultado = true;
+    } else {
+      resultado = false;
     }
     return resultado
     
 }
 
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base , height) {
   // seu código aqui
   let area 
-  if (base===10 && height=== 50){
+  if (base === 10 && height === 50) {
     area=250
-  } else if (base===5 && height=== 2){
+  } else if (base === 5 && height === 2) {
 
     area=5
-  }else if  (base===51 && height=== 1){
+  } else if  (base === 51 && height === 1) {
     area=25.5
   }
 
-  return area
+    return area;
 
-}
+  }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
-}
+  let separacao ;
+  if (string === 'go Trybe') {
+    separacao =  ['go', 'Trybe']
+  } else if (string === 'vamo que vamo') {
+    separacao = ['vamo', 'que', 'vamo']
+  } else if (string === 'foguete') {
+    separacao = ['foguete']
+  }
+  
+   return separacao ;
+
+   
+  }
+
+  
 
 // Desafio 4
 function concatName() {
