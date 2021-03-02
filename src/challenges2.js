3207cdc19980a15120825167fcef3405d192243a
Cristian <cristian.bugs@gmail.com>
2021-03-02 17:29:09 -0300

    exercicios 12 e 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,14 +9,29 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+    
+  if (lineA < lineB + lineC && lineA > (math.abs(lineB - lineC)) && lineA > math.abs(lineC - lineB)) {
+    return true;
+  } else if (lineB < lineA + lineC && lineB > (math.abs(lineA - lineC)) && lineB > math.abs(lineC - lineA) {
+    return true;
+  } else if (lineC < lineB + lineA && lineC > (math.abs(lineB - lineA)) && lineC > math.abs(lineA - lineB){
+    return true;
+  } else {return false};
+
 }
 
+console.log(triangleCheck(10,14,8))
+
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate("string") {
+  let number = string.replace(/\D/g,'');
+  let sum =0;
+  for (index =0; index < number.lenght; index =+1){
+    sum =+
+  }
 }
+//https://stackoverflow.com/questions/30607419/return-only-numbers-from-string
 
 module.exports = {
   generatePhoneNumber,
