8b615e986ab71e9707f0fec9e45605571395f9fc
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-02 18:55:52 -0300

    atualiza exercicios

--- src/challenges.js
+++ src/challenges.js
@@ -8,18 +8,18 @@ function compareTrue(param1, param2) {
     return false;
   }
 }
-console.log(compareTrue(false, true));
+/* console.log(compareTrue(false, true));
 console.log(compareTrue(false, false));
-console.log(compareTrue(true, true));
+console.log(compareTrue(true, true)); */
 
 // Desafio 2
 function calcArea(base, height) {
   let area = (base * height) / 2;
   return area;
 }
-console.log(calcArea(10, 50));
+/* console.log(calcArea(10, 50));
 console.log(calcArea(5, 2));
-console.log(calcArea(51, 1));
+console.log(calcArea(51, 1)); */
 
 // Desafio 3
 function splitSentence(string) {
@@ -27,9 +27,9 @@ function splitSentence(string) {
   array = string.split(" ");
   return array;
 }
-console.log(splitSentence("go Trybe"));
+/* console.log(splitSentence("go Trybe"));
 console.log(splitSentence("vamo que vamo"));
-console.log(splitSentence("foguete"));
+console.log(splitSentence("foguete")); */
 
 // Desafio 4
 function concatName(arrayStrings) {
@@ -43,9 +43,9 @@ function concatName(arrayStrings) {
   
   return words;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+/* console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
-console.log(concatName(['captain', 'my', 'captain']));
+console.log(concatName(['captain', 'my', 'captain'])); */
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -57,9 +57,9 @@ function footballPoints(wins, ties) {
 
   return points;
 }
-console.log(footballPoints(14, 8));
+/* console.log(footballPoints(14, 8));
 console.log(footballPoints(1, 2));
-console.log(footballPoints(0, 0));
+console.log(footballPoints(0, 0)); */
 
 // Desafio 6
 function highestCount() {
@@ -95,9 +95,9 @@ function catAndMouse(mouse, cat1, cat2) {
   return resultado;
 }
 
-console.log(catAndMouse(10, 13, 8));
+/* console.log(catAndMouse(10, 13, 8));
 console.log(catAndMouse(10, 4, 22));
-console.log(catAndMouse(10, 5, 15));
+console.log(catAndMouse(10, 5, 15)); */
 
 // Desafio 8
 function fizzBuzz(arrayNumbers8) {
@@ -116,17 +116,36 @@ function fizzBuzz(arrayNumbers8) {
   }
   return stringsArray;
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
+/* console.log(fizzBuzz([2, 15, 7, 9, 45]));
 console.log(fizzBuzz([7, 9]));
-console.log(fizzBuzz([9, 25]));
+console.log(fizzBuzz([9, 25])); */
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let newString = string.toLowerCase();
+  let objectCodes = {
+    1: "a",
+    2: "e",
+    3: "i",
+    4: "o",
+    5: "u"
+  }
+
+  for(index = 0; index < string.length; index += 1) {
+    for(let key in objectCodes) {
+      if(newString[index] === objectCodes[key]) {
+        newString.replace(newString[index], objectCodes[key])
+      }
+    }
+  }
+  return newString;
 }
+/* console.log(encode("hi there")); */
+
 function decode() {
   // seu código aqui
 }
+
 
 module.exports = {
   calcArea,
