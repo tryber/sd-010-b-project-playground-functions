e884ad74f199a0966d106643c5afa898dc02ee3d
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-02 17:11:33 -0300

    Adiciona a resoluresolução do desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -31,15 +31,28 @@ function concatName(array) {
   return result;
 }
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(pointsWins, pointsTies) {
+  let wins = pointsWins * 3;
+  let ties = pointsTies;
+  let result = wins + ties;
+  return result
 }
-
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let cont = 0;
+  let biggestNumber = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (biggestNumber < numbers[index]) {
+      biggestNumber = numbers[index];
+    }
+  }
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (biggestNumber == numbers[index]) {
+      cont += 1
+    }
+  }
+  return cont;
 }
-
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
