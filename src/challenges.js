00a74e4df32462328eb1a60f60479ab945169636
joao966 <joaooliveira966@gmail.com>
2021-03-02 19:14:01 -0300

    teste parte 1.2

--- src/challenges.js
+++ src/challenges.js
@@ -34,40 +34,13 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount() {
-  let higherNumber = 0;
-  let counter = 0;
-  for (const i in numbers) {
-    if (numbers[i] > higherNumber) {
-      higherNumber = numbers[i];
-      counter = 0;
-    }
-  }
-  for (const i in numbers) {
-    if (numbers[i] == higherNumber) {
-      counter += 1;
-    }
-  }
-  return counter;
+  // seu código aqui
 }
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7])); 
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1])); 
-console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let dist1 = Math.abs(mouse - cat1);
-  let dist2 = Math.abs(mouse - cat2);
-  if (dist1 < dist2) {
-    return 'cat1';
-  }
-  if (dist1 > dist2) {
-    return 'cat2';
-  }
-  return 'os gatos trombam e o rato foge';
+  return  Math . abs ( cat1  -  rato )  <  Matem . abs ( cat2  -  rato ) ? "cat1" : Matemática . abs ( cat2  -  rato )  <  Matem . abs ( cat1  -  rato ) ? "cat2" : "os gatos trombam e o rato foge" ;
 }
-console.log(catAndMouse(0, 3, 2)); 
-console.log(catAndMouse(0, 6, 12)); 
-console.log(catAndMouse(0, 5, 5));
 
 // Desafio 8
 function fizzBuzz(umaLista) {
@@ -87,8 +60,29 @@ function fizzBuzz(umaLista) {
 }
 
 // Desafio 9
-function encode() {
-  ///
+function encode(umaFrase) {
+  lista = umaFrase.split("");
+
+  for (let key in lista) {
+    switch(lista[key]) {
+      case "a":
+      lista[key] = 1;
+      break;
+      case "e":
+      lista[key] = 2;
+      break;
+      case "i":
+      lista[key] = 3;
+      break;
+      case "o":
+      lista[key] = 4;
+      break;
+      case "u":
+      lista[key] = 5;
+      break;
+    }
+  }
+  return lista.join("");
 }
 function decode() {
   // seu código aqui
