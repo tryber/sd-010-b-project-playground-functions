9affd9b33a1f53936125f08708d1934bc564727a
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-02 18:42:16 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,26 +3,34 @@ function techList(tech, name) {
   let novaArray = tech.sort();
   let objeto = [];
   if (tech.length === 0) {
-    return "Vazio!"
+    return 'Vazio!';
   }
   for (let i = 0; i < novaArray.length; i += 1) {
     objeto[i] = {
       tech: novaArray[i],
-      name: name
-    }
+      name: name};
   }
   return objeto;
-} console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
+} 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+ 
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let trianguloPossivel;
+  let condicao1 = (lineA < (lineB + lineC)) && (lineA > Math.abs(lineB - lineC));
+  let condicao2 = (lineB < (lineA + lineC)) && (lineB > Math.abs(lineA - lineC));
+  let condicao3 = (lineC < (lineA + lineB)) && (lineC > Math.abs(lineA - lineB));
+  if (condicao1 && condicao2 && condicao3) {
+    trianguloPossivel = true;
+  } else {
+    trianguloPossivel = false;
+  }
+  return trianguloPossivel;
+} console.log(triangleCheck(10, 14, 8))
 
 // Desafio 13
 function hydrate() {
