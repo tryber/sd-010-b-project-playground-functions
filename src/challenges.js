544e6d993955e0492f2eec46eb6c5b26c6bd758b
lucasportella <lucasportellaagu@gmail.com>
2021-03-02 17:09:34 -0300

    requiriment 7

--- src/challenges.js
+++ src/challenges.js
@@ -70,8 +70,39 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  posCounterCat1 = mouse - cat1;
+  posCounterCat2 = mouse - cat2;
+  counter1 = 0;
+  counter2 = 0;
+
+  if (posCounterCat1 < 0) {
+    posCounterCat1 *= (-1)
+  }
+
+  if (posCounterCat2 < 0) {
+    posCounterCat2 *= (-1)
+  }
+
+  for (let index = 0; index < posCounterCat1; index += 1) {
+    counter1 += 1;
+  }
+  for (let index = 0; index < posCounterCat2; index += 1) {
+    counter2 += 1;
+  }
+
+  let resultado = ''
+  if (counter1 < counter2) {
+    resultado = 'cat1'
+    return resultado;
+  } else if (counter2 < counter1) {
+    resultado = 'cat2'
+    return resultado;
+  } else {
+    resultado = "os gatos trombam e o rato foge"
+    return resultado;
+  }
+
 }
 
 // Desafio 8
--- src/tests.js
+++ src/tests.js
@@ -1,19 +1,35 @@
-function highestCount(array) {
-    let biggestNumber = 0;
-    let bigNumberReps = 0;
-    for (let index in array) {
-        if (index == 0) {
-            biggestNumber = array[index]
-        } else if (biggestNumber < array[index]) {
-            biggestNumber = array[index]
-        }
-    }
-    for (let index in array) {
-        if (array[index] == biggestNumber) {
-            bigNumberReps += 1
-        }
-    }
-    return bigNumberReps;
+function catAndMouse(mouse, cat1, cat2) {
+  posCounterCat1 = mouse - cat1;
+  posCounterCat2 = mouse - cat2;
+  counter1 = 0;
+  counter2 = 0;
+
+  if (posCounterCat1 < 0) {
+    posCounterCat1 *= (-1)
   }
-  console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
-  
+
+  if (posCounterCat2 < 0) {
+    posCounterCat2 *= (-1)
+  }
+
+  for (let index = 0; index < posCounterCat1; index += 1) {
+    counter1 += 1;
+  }
+  for (let index = 0; index < posCounterCat2; index += 1) {
+    counter2 += 1;
+  }
+
+  let resultado = ''
+  if (counter1 < counter2) {
+    resultado = 'cat1'
+    return resultado;
+  } else if (counter2 < counter1) {
+    resultado = 'cat2'
+    return resultado;
+  } else {
+    resultado = "os gatos trombam e o rato foge"
+    return resultado;
+  }
+
+}
+console.log(catAndMouse(10, 10, 10));
