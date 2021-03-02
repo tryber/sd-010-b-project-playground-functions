aefe9e52b00416376673f4e79c7bd971b20c4526
MileneLariessa <milbremm@gmail.com>
2021-03-02 17:16:39 -0300

    Indentar codigo

--- src/challenges.js
+++ src/challenges.js
@@ -10,7 +10,7 @@ console.log(result);
 
 // Desafio 2
 function calcArea(base, height) {
- return ((base * height) / 2)
+  return ((base * height) / 2);
 }
 result = calcArea(10, 50);
 console.log(result);
