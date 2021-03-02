ca522f17856c9e3fe1f4f9bf7d533c1bd0602dfd
Abimael <abimaelr.albu@gmail.com>
2021-03-02 17:20:16 -0300

    techList Correction

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,20 @@
 // Desafio 10
 function techList(tec,name) {
+  let order = tec.sort();
+
+
   let saida = [];
   if(tec.length == 0) return('Vazio!')
-  for(let index = 0; index < tec.length; index++){
+  for(let index = 0; index < order.length; index++){
     const info ={
-      tech: tec[index],
+      tech: order[index],
       name: name
     }
     saida.push(info);
   }
   return(saida);
 }
-console.log(techList([],"Abimael"));
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Abimael"));
 
 // Desafio 11
 function generatePhoneNumber() {
