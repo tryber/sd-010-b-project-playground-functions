b3cf9228f54a5cf6d9d2b1c541225223f524898e
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-03-02 19:10:20 -0300

    fixed the triangleCheck and PhoneGenerator bug that appeared after linter was fixed

--- src/challenges2.js
+++ src/challenges2.js
@@ -40,16 +40,16 @@ function findMostRepeatedNumberInObject(arrMap) {
   return mostRepeatedNumber;
 }
 
-function generatePhoneNumber(a) {
-  // starting checks
-  if (a.length !== 11) return 'Array com tamanho incorreto.';
-  const arrMap = getMappedArray(a);
-  // finding the most repeated number in array, also could do another function for this.
+function generatePhoneNumber(arr) {
+  const a = arr.slice();
+  if (arr.length !== 11) return 'Array com tamanho incorreto.';
+  const arrMap = getMappedArray(arr);
   let mostRepeatedNumber = findMostRepeatedNumberInObject(arrMap);
-  // last checks
-  const sortedArray = a.sort((b, c) => b - c);
+  const sortedArray = arr.sort((b, c) => b - c);
   if (
-    mostRepeatedNumber > 2 || sortedArray[0] < 0 || sortedArray[sortedArray.length - 1] > 9
+    mostRepeatedNumber > 2
+    || sortedArray[0] < 0
+    || sortedArray[sortedArray.length - 1] > 9
   ) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
@@ -57,7 +57,6 @@ function generatePhoneNumber(a) {
   return `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Challenge 12
 
 function returnTrueOrFalse(a, b, c) {
@@ -68,7 +67,7 @@ function triangleCheck(a, b, c) {
   const validA = b + c > a && Math.abs(b - c) < a;
   const validB = a + c > b && Math.abs(c - a) < b;
   const validC = a + b > c && Math.abs(b - a) < c;
-  returnTrueOrFalse(validA, validB, validC);
+  return returnTrueOrFalse(validA, validB, validC);
 }
 
 // Challenge 13
