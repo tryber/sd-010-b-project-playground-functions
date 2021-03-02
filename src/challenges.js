8e6c631657ab3f29e551ab105125c2d3e9d8e58d
Carlos <cemargato@gmail.com>
2021-03-02 17:12:24 -0300

    resolucao do exercicio 4

--- src/challenges.js
+++ src/challenges.js
@@ -24,22 +24,9 @@ return sentence;
 
 // Desafio 4
 function concatName(arrayString) {
-  // seu código aqui
-//   Escreva uma função com o nome concatName que, ao receber uma array de strings, retorne uma string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da array.
 
-// Isso quer dizer que, caso o parâmetro passado para concatName seja a Array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.
-
-// O que será verificado:
-
-// Retorne 'Paolillo, Lucas' quando o parâmetro passado na funcão concatName seja ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
-
-// Retorne 'ré, foguete' quando o parâmetro passado na funcão concatName seja ['foguete', 'não', 'tem', 'ré']
-
-// Retorne 'captain, captain' quando o parâmetro passado na funcão concatName seja ['captain', 'my', 'captain']
-
-arrayString[0]
-arrayString[arrayString.length-1]
-
+let finalString = (arrayString[arrayString.length-1] + arrayString[0]);
+return finalString;
 }
 
 // Desafio 5
