1e7a0e4605c16dc14921ebf920c2b6efdbfd1549
Flavio Andrade <foa12381@gmail.com>
2021-03-02 16:14:30 -0300

    exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -10,9 +10,10 @@ function compareTrue(num1, num2) {
 
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2;
 }
+
 
 // Desafio 3
 function splitSentence() {
