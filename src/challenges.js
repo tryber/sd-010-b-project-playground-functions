e2252843b0caefef9b31151da1bc6c9590f2c0ce
André De Bem <andrebemferraz@gmail.com>
2021-03-02 14:47:14 -0300

    Request I

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,11 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(boolean1, boolean2) {
   // seu código aqui
-  let
+  if (boolean1 === true && boolean2 === true) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
