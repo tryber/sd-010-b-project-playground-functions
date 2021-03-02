e2a0139044c14d1bb17ecc39ced396be155a9a6c
Paulo Xavier <pxavierf@gmail.com>
2021-03-02 16:59:40 -0300

    corrigindo lints

--- src/challenges.js
+++ src/challenges.js
@@ -36,34 +36,33 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numArray) {
-  let repeatedNumbers = {};
-  for (let index = 0; index < numArray.length; index += 1){
-    if (repeatedNumbers[numArray[index]] === undefined){
-      repeatedNumbers[numArray[index]] = 1;
+  let objNum = {};
+  for (let index = 0; index < numArray.length; index += 1) {
+    if (objNum[numArray[index]] === undefined) {
+      objNum[numArray[index]] = 1;
     } else {
-      repeatedNumbers[numArray[index]] += 1;
+      objNum[numArray[index]] += 1;
     }
   }
-  let bigNum = Object.keys(repeatedNumbers)[0];
-  let sizeRepeatedNumbers = Object.keys(repeatedNumbers).length;
+  let bigNum = Object.keys(objNum)[0];
+  let sizeobjNum = Object.keys(objNum).length;
   let repeatedBigNum = 0;
-  if(sizeRepeatedNumbers != 1){
-    for(let key in repeatedNumbers){
-      if (bigNum < key){
-        repeatedBigNum = repeatedNumbers[key];
+  if (sizeobjNum !== 1) {
+    for (let key in objNum) {
+      if (bigNum < key) {
+        repeatedBigNum = objNum[key];
       } else {
         repeatedBigNum = repeatedBigNum;
       }
     }
   } else {
-    repeatedBigNum = repeatedNumbers[Object.keys(repeatedNumbers)[0]]
+    repeatedBigNum = repeatedNumbers[Object.keys(repeatedNumbers)[0]];
   }
   return repeatedBigNum;
 }
-//REFERENCIA => Primeira parte do Gabarito do course da Trybe em exercício similar 
-//REFERENCIA => https://pt.stackoverflow.com/questions/134393/como-obter-o-primeiro-item-de-um-objeto-em-javascript
-//REFERENCIA => https://pt.stackoverflow.com/questions/15058/como-saber-o-tamanho-quantidade-de-propriedades-atributos-de-um-objeto-em-ja
-console.log(highestCount([0, 0, 0]))
+//  REFERENCIA => Primeira parte do Gabarito do course da Trybe em exercício similar
+//  REFERENCIA => https://pt.stackoverflow.com/questions/134393/como-obter-o-primeiro-item-de-um-objeto-em-javascript
+//  REFERENCIA => https://pt.stackoverflow.com/questions/15058/como-saber-o-tamanho-quantidade-de-propriedades-atributos-de-um-objeto-em-ja
 
 // Desafio 7
 function catAndMouse() {
