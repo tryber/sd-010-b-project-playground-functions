a30064c71d928dbc48a4c08743b25374861bd6b4
Thiago Marchini <thiago@msn.com>
2021-03-02 15:23:25 -0300

    completado o Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -29,8 +29,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (3 * wins) + (1 * ties);
+  return points;
 }
 
 // Desafio 6
