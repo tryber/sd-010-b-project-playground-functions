14525f25cb09b63c31ffb41ae460c8ccf866f838
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-02 15:41:26 -0300

    Resolvendo o requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -19,9 +19,9 @@ function splitSentence(string) {
 } 
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+function concatName(array) {
+  return array[0] + ", " + array[array.length-1];
+} 
 
 // Desafio 5
 function footballPoints() {
