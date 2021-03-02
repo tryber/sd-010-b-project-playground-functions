fd2c8d7ff2c56faaf951c43f02b04264a99c0612
Camila <camilafabiabh9@gmail.com>
2021-03-02 18:53:01 -0300

    adicionando nova skill function

--- src/challenges.js
+++ src/challenges.js
@@ -2,26 +2,29 @@
 function compareTrue(valor1, valor2) {
   if (valor1 === true && valor2 === true) {
     return true;
-  } else if ( valor1 === true && valor2 === false||valor1 === false && valor2 === true){
+  }else if ( valor1 === true && valor2 === false || valor1 === false && valor2 === true) {
     return false;
   } else if (valor1 === false && valor2 === false) {
     return false;
   }
 }
-console.log(compareTrue(false,false)); 
+console.log(compareTrue(false, false)); 
 // Desafio 2
 function calcArea(base, altura) {
-  let calcArea = (base*altura)/2;
+  let calcArea = (base * altura) / 2;
   return calcArea;  
 }
-console.log(calcArea(10,50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1));
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence() { // Referencias: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split  
+  let myString = 'Vamos que vamos';
+  let splits = myString.split(' ',);
+  return splits;
 }
+console.log(splitSentence());
 
 // Desafio 4
 function concatName() {
