905679a7ff9ab459089bc956685a3fdae091cb26
André De Bem <andrebemferraz@gmail.com>
2021-03-02 19:07:59 -0300

    Update II Request X

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,9 @@
 // Desafio 10
 function techList(arrayTech, nam) {
   // seu código aqui
+  if (arrayTech.length == 0) {
+    return "Vazio!"
+  } else {
   let arrayTechSort = arrayTech.sort()
   let arrayFinal = []
 
@@ -12,24 +15,24 @@ function techList(arrayTech, nam) {
     arrayFinal.push(final)
   }
   return arrayFinal
+  }
 }
 
 // Desafio 11
 function generatePhoneNumber(arrayPhoneNumber) {
-//   // seu código aqui
+  // seu código aqui
+  for (index = 0; index < arrayPhoneNumber.length; index += 1) {
+  if (arrayPhoneNumber[index] < 0 || arrayPhoneNumber[index] > 9 || )
+  }
 
-//   // for (index = 0; index < arrayPhoneNumber.length; index += 1) {
-//   // if (arrayPhoneNumber[index] < 0 || arrayPhoneNumber[index] > 9 || )
-//   // }
+  let DDD = (`(${arrayPhoneNumber[0]}${arrayPhoneNumber[1]}) `);
+  let firstPart = (`${arrayPhoneNumber[2]}${arrayPhoneNumber[3]}${arrayPhoneNumber[4]}${arrayPhoneNumber[5]}${arrayPhoneNumber[6]}-`)
+  let secondPart = (`${arrayPhoneNumber[7]}${arrayPhoneNumber[8]}${arrayPhoneNumber[9]}${arrayPhoneNumber[10]}`)
 
-//   let DDD = (`(${arrayPhoneNumber[0]}${arrayPhoneNumber[1]}) `);
-//   let firstPart = (`${arrayPhoneNumber[2]}${arrayPhoneNumber[3]}${arrayPhoneNumber[4]}${arrayPhoneNumber[5]}${arrayPhoneNumber[6]}-`)
-//   let secondPart = (`${arrayPhoneNumber[7]}${arrayPhoneNumber[8]}${arrayPhoneNumber[9]}${arrayPhoneNumber[10]}`)
+  return (`${DDD}${firstPart}${secondPart}`)
+}
 
-//   return (`${DDD}${firstPart}${secondPart}`)
-// }
-
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
