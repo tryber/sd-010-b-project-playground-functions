229044a1e90d601931beab524f3e2411c408dd92
Daniel <dsouzaborini@gmail.com>
2021-03-02 18:36:15 -0300

    Add ordem alfabética no ex 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,19 +1,20 @@
 // Desafio 10
 function techList(tecnologias, name) {
   let resultado = [];
-  for (let index = 0; index < tecnologias.length; index++) {
+  let ordemCrescente = tecnologias.sort();
+  for (let index = 0; index < ordemCrescente.length; index++) {
     let objeto = {
-      tech: tecnologias[index],
+      tech: ordemCrescente[index],
       name: name,
     }
     resultado.push(objeto);
   }
   return resultado;
 }
+// Auxilio por orientação:
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
-
-
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
