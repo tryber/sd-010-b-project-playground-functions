a2a4640909029b923264261b63f216e2be0ca7be
Vinicius <vini_bodra@hotmail.com>
2021-03-02 18:09:57 -0300

    10º requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
+/** Consultei o repositório do Cristiano Lopes Duarte para descobrir como conto ao leitor do arquivo que eu quero criar um objeto. É usando as chaves {} pra conter as keys com suas respectivas propriedades.
+https://github.com/tryber/sd-09-project-playground-functions/blob/tkdcris-project-playground-functions/src/challenges.js
+*/
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arei, name){
+
+    let objetoTechName = [];
+    if(arei.length!==0){
+        for(i in arei.sort()){
+            objetoTechName.push({tech: arei[i], name})
+        
+        }
+        return objetoTechName
+    } else {
+        return "Vazio!"
+    }
 }
 
 // Desafio 11
