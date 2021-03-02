51d1c89c92f004eb034d7d3f8c29903e3f0b895f
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-03-02 15:10:03 -0300

    requisiot 1 correçoes

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,6 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if (a == true && b == true) {
-    return true;
-  } else {
-    return false;
-  }
+  return a === true && b === true ? true : false;
 }
 
 // Desafio 2
