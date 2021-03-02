1b1f0ccc7b272f4dc0ca4388a674bfd36f979c2b
Anderson <trkz.anderson@gmail.com>
2021-03-02 15:35:45 -0300

    #03. Crie uma função que divida a frase

--- src/challenges.js
+++ src/challenges.js
@@ -16,8 +16,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(frase) {
   // seu código aqui
+  novaFrase = frase.splitSentence;
+  return novaFrase;
 }
 
 // Desafio 4
