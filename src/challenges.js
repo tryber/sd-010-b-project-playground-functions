5d9e0ba1482ed38e635494d6cc0ba2aa083ae209
DanielTrybe <darkdanielr@gmail.com>
2021-03-02 19:36:43 -0300

    exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -39,29 +39,40 @@ console.log(footballPoints(0, 0))
 function highestCount(valores) {
   for (let iArray = 0; iArray < valores.length; iArray += 1) {
     let repete = 0
-    for (let indexArray = 0; indexArray < valores.length; indexArray += 1) {
-      if (valores[iArray] === valores[indexArray]) {
+      if  {
         return repete += 1
       }
     }
   }
-}
+
 console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let gato1 = mouse - cat1;
-  let gato2 = mouse - cat2;
-  if (gato1 > gato2) {
-    return 'cat1'
+  let gato1 = 0;
+  let gato2 = 0;
+  if (cat1 > mouse) {
+    gato1 = cat1 - mouse;
+  } 
+  if (cat1 < mouse) {
+    gato1 = mouse - cat1;
   }
-  else if (gato1 < gato2) {
+  if (cat2 > mouse) {
+    gato2 = cat2 - mouse;
+  }
+  if (cat2 < mouse) {
+    gato2 = mouse - cat2;
+  } 
+  if (gato1 === gato2) {
+    return 'os gatos trombam e o rato foge'
+  } else if (gato1 > gato2) {
     return 'cat2'
   } else {
-    return 'os gatos trombam e o rato foge'
+    return 'cat1'
   }
 }
-console.log(catAndMouse(0,12,12))
+
+console.log(catAndMouse(1,0,2))
 
 // Desafio 8
 function fizzBuzz() {
