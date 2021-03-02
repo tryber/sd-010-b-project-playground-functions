bee43f1821a1e1fa7c2d7e87c9f80b60b04a4e0e
joao966 <joaooliveira966@gmail.com>
2021-03-02 18:12:52 -0300

    requisito '3'

--- src/challenges.js
+++ src/challenges.js
@@ -14,7 +14,7 @@ function calcArea(base, height) {
 /////////////////////////////////////////////////////////////////////////////////////
 // Desafio 3
 function splitSentence(string) {
-  for  ( let  index  =  0 ;  index  <  frase . length ;  index ++ )  {
+  for  ( let  index  =  0 ;  index  <  frase . length ;  index += 1 )  {
     arrayString  =  frase . split ( split ) ;
   }
  return  arrayString ;
