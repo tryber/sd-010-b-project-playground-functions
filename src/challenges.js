150a7d22403b0e53975e2e99980bb13ea7b63967
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-02 14:51:45 -0300

    Adicionado os calculos a function calcArea

--- src/challenges.js
+++ src/challenges.js
@@ -4,8 +4,10 @@ function compareTrue() {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let result = (base * height) / 2;
+  return result;
 }
 
 // Desafio 3
