677cd377891aa2ae0308d527648b5de7435a0f55
Malbathan Lima <malbathanlima@gmail.com>
2021-03-02 16:56:55 -0300

    Divisao de frase string-to-array

--- src/challenges.js
+++ src/challenges.js
@@ -19,9 +19,13 @@ console.log (calcArea (5, 2));
 console.log (calcArea (51, 1));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(words) {
+   separeted = words.split(" ");
+    return separeted
 }
+console.log(splitSentence("go trybe"));
+console.log(splitSentence("vamo que vamo"));
+console.log(splitSentence("foguete"));
 
 // Desafio 4
 function concatName() {
