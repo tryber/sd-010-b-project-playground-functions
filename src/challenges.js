0ded58a4b75a99eda91652be75bc2bf72291b84c
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-02 16:42:28 -0300

    requisito 2 corrigido

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, height) {
   return (base * height) / 2;
 }
 console.log(calcArea(10, 50));
-console.log(calcArea(5, 2))
-console.log(calcArea(51, 1))
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
