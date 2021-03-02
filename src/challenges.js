946b49bf02003f1838555f3c4e442125fca2952a
Junior030 <juniorhenrique030@gmail.com>
2021-03-02 16:30:16 -0300

    Fim Requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -6,9 +6,7 @@ function compareTrue(check1, check2) {
   }
   return check3;
 }
-let check1 = true;
-let check2 = false;
-console.log(compareTrue(check1, check2));
+console.log(compareTrue(true, false));
 
 // Desafio 2
 function calcArea(base, height) {
@@ -23,22 +21,31 @@ function splitSentence(string) {
   let array = [];
   let help = '';
   for (let i = 0; i <= string.length; i += 1) {
-    if(string[i] === ' ' || string[i] === undefined) {
+    if (string[i] === ' ' || string[i] === undefined) {
       array.push(help);
       help = '';
     } else if (string[i] !== ' ') {
       help += string[i];
-    }    
+    } 
   }
   return array;
 }
-let string = 'Go trybe';
-console.log(splitSentence(string));
+console.log(splitSentence('Go trybe'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let help = '';
+  let first, last;
+  for (let i = 0; i < array.length; i++) {
+    if (i == array.length - 1) {
+      last = array[i];
+    } else if (i == 0) {
+      first = array[i];
+    }
+  }
+  return help = last + ', ' + first;
 }
+console.log(concatName(['foquete', 'não', 'tem', 'ré']));
 
 // Desafio 5
 function footballPoints() {
