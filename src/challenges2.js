282cb90db7140f0ef3a4b9a4c023f083e24c7272
André <hammel.engineer@gmail.com>
2021-03-02 18:57:22 -0300

    ->Inclusao item 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,28 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  if (array.length != 0) {
+    let ordenada = array.sort();
+    let lista = [];
+    for (let key in ordenada) {
+      let dicionario = {
+        tech: ordenada[key],
+        name: name
+      }
+      lista.push(dicionario);
+    }
+    return lista;
+  }else{
+    return 'Vazio!'
+  }
 }
+
+// teste itam 10
+// namePessoal = 'Lucas';
+// array = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// console.log(techList(array, namePessoal));
+// namePessoal = 'Lucas';
+// array1 = [];
+// console.log(techList(array1, namePessoal));
 
 // Desafio 11
 function generatePhoneNumber() {
---
+++ teste.js
@@ -0,0 +1,19 @@
+
+
+let lista = [];
+array = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+ordenada = array.sort();
+let dicionario = {
+  tech: 'css',
+  nome: 'lucas'
+}
+
+lista.push(dicionario);
+
+dicionario = {
+  tech: 'ssa',
+  nome: 'lu'
+}
+lista.push(dicionario);
+console.log(lista);
+console.log(ordenada);
