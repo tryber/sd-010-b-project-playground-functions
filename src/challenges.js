d1465c95836f9f718c416babe6c9b3c6bfd9d693
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-02 14:45:07 -0300

    add desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(param1, param2) {
+  if(param1 === true && param2 === true) {
+    return true;
+  } else if (param1 === false && param2 === false) {
+    return false
+  } else {
+    return false;
+  }
 }
+console.log(compareTrue(false, true));
+console.log(compareTrue(false, false));
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea() {
