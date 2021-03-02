b232435b56c7edacf9213f82a9a935fe7d308d2c
RafaMaths <rmathscoach@gmail.com>
2021-03-02 14:42:09 -0300

    Inicio do Projeto Playground Functions

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,7 @@
 // Desafio 1
 function compareTrue() {
   // seu código aqui
+  bora começar
 }
 
 // Desafio 2
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,7 @@
 // Desafio 10
 function techList() {
   // seu código aqui
+  bora começar
 }
 
 // Desafio 11
