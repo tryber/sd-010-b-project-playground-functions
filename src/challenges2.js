5ed825468e637a5c155129a231a925f72014bdd8
André De Bem <andrebemferraz@gmail.com>
2021-03-02 18:31:50 -0300

    Request X

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,32 @@
 // Desafio 10
-function techList() {
+function techList(arrayTech, nam) {
   // seu código aqui
+  let arrayTechSort = arrayTech.sort()
+  let arrayFinal = []
+
+  for (index = 0; index < arrayTechSort.length; index += 1) {
+    let final = {}
+    final["tech"] = arrayTechSort[index]
+    final["name"] = nam
+    
+    arrayFinal.push(final)
+  }
+  return arrayFinal
 }
 
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Gerald"))
+
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayPhoneNumber) {
   // seu código aqui
+  for (index = 0; index < arrayPhoneNumber.length; index += 1) {
+  if (arrayPhoneNumber[index] < 0 || arrayPhoneNumber[index] > 9 || )
+  }
+
+  return (`(${arrayPhoneNumber[1]}${arrayPhoneNumber[2]}) ${arrayPhoneNumber[3]}${arrayPhoneNumber[4]}${arrayPhoneNumber[5]}${arrayPhoneNumber[6]}${arrayPhoneNumber[7]}-${arrayPhoneNumber[8]}${arrayPhoneNumber[9]}${arrayPhoneNumber[10]}${arrayPhoneNumber[11]}`)
 }
+
+console.log(generatePhoneNumber([9,9,2,5,7,8,9,1,2,3]))
 
 // Desafio 12
 function triangleCheck() {
