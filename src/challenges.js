60777dfca013e663f32fadc8cffaf76d7c562552
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-02 20:44:51 -0300

    adicionando a resolução da nona questão

--- src/challenges.js
+++ src/challenges.js
@@ -88,11 +88,25 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+
+function encode(lowercase){
+  let encode = lowercase
+  .replace(/a/g, 1)
+  .replace(/e/g, 2)
+  .replace(/i/g, 3)
+  .replace(/o/g, 4)
+  .replace(/u/g, 5)  
+  return encode
 }
-function decode() {
-  // seu código aqui
+
+function decode(text) {
+  let decode = text
+  .replace(/1/g, 'a')
+  .replace(/2/g, 'e')
+  .replace(/3/g, 'i')
+  .replace(/4/g, 'o')
+  .replace(/5/g, 'u') 
+  return decode
 }
 
 module.exports = {
