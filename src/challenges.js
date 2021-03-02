68c9629077c8c773cb661fd19af01574fd755d0b
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-02 19:45:42 -0300

    requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -26,9 +26,13 @@ console.log(splitSentence('vamo que vamo'));
 console.log(splitSentence('foguete'));
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  return array[array.length -1] + ", " + array[0];
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
 function footballPoints() {
