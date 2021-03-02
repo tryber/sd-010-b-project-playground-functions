5dbf77353990e35d16d5bf9a914a4d86a58fa5e8
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-02 14:43:44 -0300

    true/false

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(number1, number2) {
+  if (number1 === true && number2 === true) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
