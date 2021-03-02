01da8a6f7a73016aee0c0f94db98d85dd1f6c429
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-03-02 20:17:56 -0300

    desafios 1-10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  array.sort(); //Source: https://www.w3schools.com/jsref/jsref_sort.asp
+
+  let ar = [];
+  let ob = {}
+
+  for(let i=0; i<array.length; i++){
+      ob.tech = array[i];
+      ob.name = name;
+      ar.push(ob); //Source: https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
+      ob={};
+    }
+return (ar);
 }
 
 // Desafio 11
