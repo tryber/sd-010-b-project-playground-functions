6a745d8cf57e6dc7ec0fd17be8d9a3072b939a6a
Italo Matos <italomatos764@gmail.com>
2021-03-02 19:09:24 -0400

    Adicionando funcao encode e decode

--- src/challenges.js
+++ src/challenges.js
@@ -66,11 +66,28 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(palavra) {
+  let array = palavra.split("");
+  for(let i=0; i<array.length; i++){
+    if(array[i] == 'a') array[i] = '1';
+    else if(array[i] == 'e') array[i] = '2';
+    else if(array[i] == 'i') array[i] = '3';
+    else if(array[i] == 'o') array[i] = '4';
+    else if(array[i] == 'u') array[i] = '5';
+  }
+  return array.join("");
 }
-function decode() {
-  // seu código aqui
+
+function decode(palavraCodificada) {
+  let array = palavraCodificada.split("");
+  for(let i=0; i<array.length; i++){
+    if(array[i] == '1') array[i] = 'a';
+    else if(array[i] == '2') array[i] = 'e';
+    else if(array[i] == '3') array[i] = 'i';
+    else if(array[i] == '4') array[i] = 'o';
+    else if(array[i] == '5') array[i] = 'u';
+  }
+  return array.join("");
 }
 
 module.exports = {
