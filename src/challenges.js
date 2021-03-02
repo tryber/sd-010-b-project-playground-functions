9d1aa3efaef3c9379edb51e995a9b5fa12017bba
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-02 17:05:52 -0300

    desafio 7 funcionou no console.log verificar se vai funcionar e liberar etapa

--- src/challenges.js
+++ src/challenges.js
@@ -47,22 +47,43 @@ function highestCount() {
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
-  if((cat1-mouse)<(cat2-mouse)){
+  if((cat1+mouse)<(cat2+mouse)){
     return "cat1";
   }
-  else if ((cat1-mouse)>(cat2-mouse)){
+  else if ((cat2+mouse)<(cat1+mouse)){
     return "cat2";
   }
-  else{
+  else if ((cat2+mouse) == (cat1+mouse)){
     return "os gatos trombam e o rato foge";
   }
 
 }
 
+console.log(catAndMouse(1,3,2));
+
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(vetor) {
   // seu código aqui
+  let vetorderetorno=[];
+    for (let i = 0; i<vetor.length ; i++){
+      if((vetor[i]%3 == 0) && (vetor[i] % 5 == 0)){
+        vetorretorno [i] = "fizzBuzz";
+      }
+      else if ((vetor[i]%3 == 0) && (vetor[i] % 5 !=0)) {
+        vetorretorno [i] = "fizz";
+      }
+      else if ((vetor[i]%3 != 0) && (vetor[i] % 5 == 0)) {
+        vetorretorno [i] = "buzz";
+
+      }
+      else {
+        vetorderetorno[i] = "bug!";
+      }
+    }
+ return vetorderetorno;
 }
+let vetnumber = [2, 15, 7, 9, 45];
+console.log(fizzBuzz(vetnumber));
 
 // Desafio 9
 function encode() {
