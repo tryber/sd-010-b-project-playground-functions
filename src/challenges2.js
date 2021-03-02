f1d50cf6ebb8c9e3cbb4499fff4621015b9b0d2c
Antenor <antenorzapata@gmail.com>
2021-03-02 16:49:07 -0300

    ex11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,40 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  let numbers = {};
+
+  // verifica a quantidade de elementos no array
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  // "Preenche" o objeto numbers com os elementos do array e passa o value de 0 pra todos.
+  for (let item of arr) {
+    numbers[item] = 0;
+  }
+  // conta o número de ocorrências de cada elemento do array comparando com o objeto numbers.
+  for (let num of arr) {
+    numbers[num] += 1;
+  }
+  // verifica se os números passados estão dentro das condições exigidas.
+  for (let occ of arr) {
+    if (occ < 0 || occ > 9 || numbers[occ] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let telefone = '(';
+  for (let i = 0; i < arr.length; i++) {
+    if (telefone.length === 3) {
+      telefone += ') ' + arr[i];
+    } else if (telefone.length === 10) {
+      telefone += '-' + arr[i];
+    } else {
+      telefone += arr[i];
+    }
+  }
+  return telefone;
 }
 
 // Desafio 12
