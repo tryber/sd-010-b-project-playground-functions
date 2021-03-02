74bc641eb22c0bb6514809a103910551919f4854
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-03-02 17:58:41 -0300

    Alterando exercicio 6

--- src/challenges.js
+++ src/challenges.js
@@ -2,7 +2,7 @@
 
 function compareTrue(boolean1, boolean2) {
 
-  if (boolean1 === true && boolean2 === true){
+  if (boolean1 === true && boolean2 === true) {
     return true;
   } else {
     return false;
@@ -12,7 +12,7 @@ function compareTrue(boolean1, boolean2) {
 // Desafio 2
 function calcArea(base, height) {
   let areaTriangulo;
-  areaTriangulo = (base*height)/2
+  areaTriangulo = (base*height)/2;
   
   return areaTriangulo;
 }
@@ -28,16 +28,16 @@ function splitSentence(frase) {
 // Desafio 4
 function concatName(ultimoPrimeiroItem) {
   
-  let conectaItem = (ultimoPrimeiroItem[ultimoPrimeiroItem.length-1]) + ", " + (ultimoPrimeiroItem[0]);
+  let conectaItem = (ultimoPrimeiroItem[ultimoPrimeiroItem.length - 1]) + ", " + (ultimoPrimeiroItem[0]);
   return conectaItem;
 
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  pontosWins = wins * 3;
-  pontosTies = ties * 1;
-  pontosTotais = pontosTies + pontosWins;
+  let pontosWins = wins * 3;
+  let pontosTies = ties * 1;
+  let pontosTotais = pontosTies + pontosWins;
   
   return pontosTotais;
 }
@@ -48,13 +48,13 @@ function highestCount(repeteMaior) {
   
   let maiorNumero;
   let qntNumeroMaior = 1;
-  for(let cont=0;cont < repeteMaior.length; cont += 1){
-    if(repeteMaior[cont] > repeteMaior[repeteMaior.length-1]){
+  for(let cont=0;cont < repeteMaior.length-1; cont += 1) {
+    if (repeteMaior[cont] > repeteMaior[cont+1]) {
       maiorNumero = repeteMaior[cont];
     }
   }
-  for(let cont=0;cont < repeteMaior.length; cont+=1){
-    if(maiorNumero === repeteMaior[cont]){
+  for(let cont=0;cont < repeteMaior.length; cont+=1) {
+    if (maiorNumero === repeteMaior[cont]) {
       qntNumeroMaior += 1;
     }
   }
