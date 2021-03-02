c31b034f27985d3faaa6b0098c42b24a8442d0bf
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-02 16:00:36 -0300

    adicionando a terceira questão

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(palavra) {
+  let separacao = palavra.split(' ')
+  return separacao
 }
 
 // Desafio 4
