64f3715a0debe5dd23167c3064c7b0effe984982
Matheus Bodra <matheusbodra@gmail.com>
2021-03-02 15:27:28 -0300

    exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+
+/*
+    Consultei o repositório de Cristiano Lopes Duarte para resolver o próximo exercício.
+    Link: https://github.com/tryber/sd-09-project-playground-functions/blob/tkdcris-project-playground-functions/src/challenges.js
+*/
+
+function techList(array, name) {
+  let arrayAux = [];
+
+  if (array.length>0) {
+      for (i in array.sort()) {
+          arrayAux.push({tech: array[i], name});
+      }
+      return arrayAux
+  } else {
+      return "Vazio!"
+  }
 }
 
 // Desafio 11
