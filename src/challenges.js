14b7e2a4112dc620eabd0f9ad41f410c3c5ed9be
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-02 15:26:46 -0300

    challenge 1 complete

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  //initial commit
+function compareTrue(a, b) {
+  if (a === true && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(compareTrue(true,true));
 
 // Desafio 2
 function calcArea() {
