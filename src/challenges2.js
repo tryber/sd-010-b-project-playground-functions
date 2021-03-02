745056d5d7d23b4b6ff2b0bf09d4f05cc113d923
Leandro Reis <lleandrofr@gmail.com>
2021-03-02 19:45:02 -0300

    Resolvendo requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,14 +11,43 @@ function techList(techs, name) {
   return resultList;
 }
 
-
-
-
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  phoneNumber = '(';
+  if (numbers.length !== 11) {
+    return "Array com tamanho incorreto.";
+  } else {
+    for (let index = 0; index < 2; index += 1) {
+      phoneNumber = phoneNumber + numbers[index];
+    }
+    phoneNumber =phoneNumber + ')';
+    for (let index = 2; index < 7; index += 1) {
+      phoneNumber = phoneNumber + numbers [index];
+    }
+    phoneNumber =phoneNumber + '-'; 
+    for (let index = 7; index < numbers.length; index += 1) {
+      phoneNumber = phoneNumber + numbers [index];
+    }
+  }
+  return phoneNumber;
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1 ]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 12
 function triangleCheck() {
