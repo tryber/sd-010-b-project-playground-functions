bb9009c8ea90f0b269b9cdfae18dadf837fb89d5
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-03 00:28:25 -0300

    atualizando

--- src/challenges.js
+++ src/challenges.js
@@ -35,17 +35,17 @@ function footballPoints(wins, ties) {
 function highestCount(numeros) {
   let indiceMaior = 0;
   let contRepetido = 0;
-  for (index = 1; index <= numeros.length; index += 1) {
+  for (index = 0; index <= numeros.length; index += 1) {
     if (numeros[index] > indiceMaior) {
       indiceMaior = numeros[index];
     } 
   }
-  for (index2 = 1; index2 <= numeros.length; index2 += 1) {
+  for (index2 = 0; index2 <= numeros.length; index2 += 1) {
     if (numeros[index2] === indiceMaior) {
       contRepetido += 1;
     }
   } return contRepetido;
-} console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+} console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -58,73 +58,69 @@ function catAndMouse(mouse, cat1, cat2) {
     distanciaCat2 = cat2 - mouse;
   }
   if(distanciaCat1 - mouse === distanciaCat2 - mouse) {
-    return "os gatos trombam e o rato foge";          
-    }   
-  else if (distanciaCat1 < distanciaCat2 ) {
-    return "cat1";
-  } else {
-    return "cat2";
+    return 'os gatos trombam e o rato foge';          
+  } else if (distanciaCat1 < distanciaCat2) {
+    return 'cat1';
   } 
-} console.log(catAndMouse(5, 8, 3))
- 
-// Desafio 8
+    return 'cat2';
+} console.log(catAndMouse(5, 8, 3));
+ // Desafio 8
 function fizzBuzz(Array) {
   let resultado = [];
-  for (let index = 0; index <= Array.length-1; index ++) {
+  for (let index = 0; index <= Array.length - 1; index += 1) {
     if (Array[index] % 3 === 0 && Array[index] % 5 === 0) {
-      resultado.push('fizzBuzz'); 
-    } else if (Array[index] % 5 === 0 && Array[index] % 2 !== 0 && Array[index] % 3 !== 0){
-     resultado.push("buzz");
+      resultado.push('fizzBuzz');
+    } else if (Array[index] % 5 === 0 && Array[index] % 2 !== 0 && Array[index] % 3 !== 0) {
+      resultado.push('buzz');
     } else if (Array[index] % 3 === 0 && Array[index] % 2 !== 0 && Array[index] % 5 !== 0) {
-     resultado.push("fizz");
+      resultado.push('fizz');
     } else {
-      resultado.push("bug!");
+      resultado.push('bug!');
     }
   }
-return resultado
-}console.log(fizzBuzz([7, 9]))
+  return resultado;
+} console.log(fizzBuzz([7, 9]));
 
 // Desafio 9
 function encode(palavra) {
-  let resultado = "";
-  for (index = 0; index <= palavra.length-1; index++){
-    if (palavra[index] === "a"){
+  let resultado = '';
+  for (index = 0; index <= palavra.length - 1; index += 1){
+    if (palavra[index] === 'a') {
       resultado += 1; 
-    } else if (palavra[index] === "e") {
+    } else if (palavra[index] === 'e') {
       resultado += 2;
-    } else if (palavra[index] === "i") {
-      resultado += 3; 
-    } else if (palavra[index] === "o") {
+    } else if (palavra[index] === 'i') {
+      resultado += 3;
+    } else if (palavra[index] === 'o') {
       resultado += 4;
-    } else if (palavra[index] === "u") {
+    } else if (palavra[index] === 'u') {
       resultado += 5;
     } else {
       resultado += palavra[index];
     }
   }
   return resultado;
-} console.log(encode("paralelepipedo"))
+} console.log(encode("paralelepipedo"));
 
 function decode(palavra) {
-  let resultado = "";
-  for (index = 0; index <= palavra.length-1; index++){
+  let resultado = '';
+  for (index = 0; index <= palavra.length - 1; index += 1) {
     if (palavra[index] === 1) {
-      resultado += "a";
+      resultado += 'a';
     } else if (palavra[index] === 2) {
-      resultado += "e";
+      resultado += 'e';
     } else if (palavra[index] === 3) {
-      resultado += "i";
+      resultado += 'i';
     } else if (palavra[index] === 4) {
-      resultado += "o";
+      resultado += 'o';
     } else if (palavra[index] === 5) {
-      resultado += "u";
+      resultado += 'u';
     } else {
       resultado += palavra[index];
     }
   }
   return resultado;
-} console.log(decode("p1r1l2l2p3p2d4"))
-
+} console.log(decode("p1r1l2l2p3p2d4"));
 
 module.exports = {
   calcArea,
