6db90cfee2a4215f2e584fa64b17762661bf733c
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-03-03 17:02:34 -0300

    Adiciona o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,12 +13,45 @@ function techList(array, name) {
   }
   return listOfObjetcts;
 }
-console.log(techList([], 'Renan'));
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function countNumberAppears(array, value) {
+  let n = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === value) {
+      n += 1;
+    }
+  }
+  return n;
 }
 
+// Desafio 11
+function generatePhoneNumber(numbers) {
+  let feedback = '';
+  for (let i = 0; i < numbers.length; i += 1) {
+    switch (i) {
+      case 0:
+        feedback += '(';
+        break;
+      case 2:
+        feedback += ') ';
+        break;
+      case 7:
+        feedback += '-';
+        break;
+    }
+    feedback += numbers[i];
+  }
+  if (numbers.length != 11) {
+    feedback = 'Array com tamanho incorreto.';
+  }
+  for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+    let appers = countNumberAppears(numbers, numbers[index2]);
+    if (appers > 2 || numbers[index2] < 0 || numbers[index2] > 9) {
+      feedback = 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return feedback;
+}
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
