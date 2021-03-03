3fc54f7f4cfd8bee6b4801f8a3add1e45a6bbecc
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-03 13:00:09 -0300

    correçao do item 7

--- src/challenges.js
+++ src/challenges.js
@@ -71,7 +71,7 @@ function catAndMouse(mouse, cat1, cat2) {
   if (distanceCat1 > distanceCat2) {
     return 'cat2';
   }
-  return 'os gatos trombam e o rato foge';  
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
