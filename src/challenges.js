88e3711b9d6f4d5162d8e1cafae8aa7f0ff47e0e
riquee <strikedv@hotmail.com>
2021-03-03 13:14:30 -0300

    Exercicio 10 e 13 finalizados

--- src/challenges.js
+++ src/challenges.js
@@ -70,9 +70,34 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let numero = { a: 1, e: 2, i: 3, o: 4, u: 5 };
+  let newString = '';
+  for (let index = 0; index < string.length; index += 1) {
+    switch (string[index]) {
+      case 'a':
+        newString += 1;
+        break;
+      case 'e':
+        newString += 2;
+        break;
+      case 'i':
+        newString += 3;
+        break;
+      case 'o':
+        newString += 4;
+        break;
+      case 'u':
+        newString += 5;
+        break;
+      default:
+        newString += string[index];
+    }
+  }
+  console.log(newString);
 }
+encode('hello');
+
 function decode() {
   // seu código aqui
 }
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,32 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+
+function techList(array, nome) {
+  array = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+  array = array.sort();
+  for (let index = 0; index < array.length; index += 1) {
+    array[index] = {
+      tech: array[index],
+      name: nome,
+    };
+  }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber() {}
 
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
 }
 
+// REFERÊNCIAS: https://stackoverflow.com/ , https://developer.mozilla.org/
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let regex = /\d+/g;
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let matches = string.match(regex);
+  matches = matches.map(Number);
+  return `${matches.reduce(reducer)} copos de água`;
 }
 
 module.exports = {
