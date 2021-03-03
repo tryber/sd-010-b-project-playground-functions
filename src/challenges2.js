d8f036f16143c53dd82b5c3ae9e042f4c98e790e
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-03 01:38:13 -0300

    desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,25 +17,28 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 console.log(techList([], 'Angelo'));
 
 // Desafio 11
+let ddd = [];
+let part1 = [];
+let part2 = [];
 function generatePhoneNumber(phoneNumber) {
   // seu código aqui
-  let numbers = phoneNumber;
-  let ddd = [];
-  let part1 = [];
-  let part2 = [];
+  if (phoneNumber.length > 11) {
+    return 'Array com tamanho incorreto';
+  }
   for (let index = 0; index < 2; index += 1) {
-    ddd.push(numbers[index]);
+    ddd.push(phoneNumber[index]);
   }
   for (let index = 2; index < 7; index += 1) {
-    part1.push(numbers[index]);
+    part1.push(phoneNumber[index]);
   }
   for (let index = 7; index < 11; index += 1) {
-    part2.push(numbers[index]);
+    part2.push(phoneNumber[index]);
   }
   return `(${ddd.join('')}) ${part1.join('')}-${part2.join('')}`;
 }
 // console log
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 8]));
 
 // Desafio 12
 function triangleCheck() {
@@ -43,10 +46,29 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
+
+function hydrate(bebidas) {
+  let count = 0;
+  let regex = /\d/g;
   // seu código aqui
+  let numeros = bebidas.match(regex);
+  for (let index = 0; index < numeros.length; index += 1) {
+    count += parseInt(numeros[index], 0);
+  }
+  if (count === 1) {
+    return `${count} copo de água`;
+  }
+  return `${count} copos de água`;
 }
 
+// console log
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
+console.log(hydrate('1 cachaça, 4 cervejas e 7 copo de vinho'));
+console.log(hydrate('3 vodkas e 1 copo de vinho'));
+console.log(hydrate('5 cervejas'));
+console.log(hydrate('1 cachaça'));
+
+//
 module.exports = {
   generatePhoneNumber,
   techList,
