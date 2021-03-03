fbe5d66dad1ef57dad737dcf5cf5aa5818cff5da
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-03 10:51:29 -0300

    tetando a numero 7

--- src/challenges.js
+++ src/challenges.js
@@ -20,9 +20,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName(umaLista) {
-  let concat = umaLista[umaLista.length - 1] + ", " + umaLista[0];
-  return concat;
+function concatName(listaDeNomes) {
+  let concatena = listaDeNomes[listaDeNomes.length - 1] + ", " + listaDeNomes[0];
+  return concatena;
 }
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -38,27 +38,34 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-  bigger = numbers[0];
-  for (let key in numbers) {
-    if (numbers[key] > bigger) {
-      bigger = numbers[key];
+  let maior = 0;
+  for (let index in array) {
+    if (array[index] > maior) {
+      maior = array[index];
     }
   }
-  //contar quantas vezes ele aparece
-  let counter = 0;
-  for (let key in numbers) {
-    if ( numbers[key] === bigger) {
-      counter ++;
-    if (numbers[key] === bigger) {
-      counter++;
-    }
+  let cont = 0;
+  for (let index in array) {
+    if (array[index === maior]) {
+      cont++;
+   }
   }
-  return counter;
+    return cont;
 }
-}
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distancia1 = mouse - cat1;
+  let distancia2 = mouse - cat2
+  if (distancia1 === distancia2 ){
+    return "os gatos trombam e o rato foge"
+  }
+  else if (distancia1 < distancia2){
+    return cat2;
+  }
+  else {
+    return cat1;;
+  }
 }
 
 // Desafio 8
