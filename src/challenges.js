433c4d3098d3c841c8e54348ade60075c4d4b0b6
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-02 22:46:21 -0300

    Atualizando as alteraçoes do lit

--- src/challenges.js
+++ src/challenges.js
@@ -2,41 +2,36 @@
 function compareTrue(bool1, bool2) {
   // seu código aqui
   let resp = '';
-  if (bool1 === true && bool2 === true){
-     resp = true;
+  if (bool1 === true && bool2 === true) {
+    resp = true;
   } else {
     resp = false;
   }
   return resp;
 }
 
-
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  let area = (base * height) / 2
+  let area = (base * height) / 2;
   return area;
 }
-
 
 // Desafio 3
 function splitSentence(sentence) {
   // seu código aqui
-  let words = sentence.split(" ");
+  let words = sentence.split(' ');
   return words;
 }
-
 
 // Desafio 4
 function concatName(arrayNames) {
   // seu código aqui
   let firstName = arrayNames[0];
-  let lastName = arrayNames[arrayNames.length -1];
-  let fullName = lastName + ", " + firstName;
+  let lastName = arrayNames[arrayNames.length - 1];
+  let fullName = lastName + ', ' + firstName;
   return fullName;
 }
-
-
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -45,43 +40,39 @@ function footballPoints(wins, ties) {
   return score;
 }
 
-
 // Desafio 6
 function highestCount(arrayNumbers) {
   // seu código aqui
   let contMaior = 0;
   let maior = arrayNumbers[0];
   
-  for (let num in arrayNumbers){
-    if(maior < arrayNumbers[num]){
+  for (let num in arrayNumbers) {
+    if (maior < arrayNumbers[num]) {
       maior = arrayNumbers[num];
     }
   }
-  for (let highest in arrayNumbers){
-    if(maior === arrayNumbers[highest]){
+  for (let highest in arrayNumbers) {
+    if (maior === arrayNumbers[highest]) {
       contMaior += 1;
     }
   }
-
   return contMaior;
 }
-
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let distCat1 = Math.abs(cat1 - mouse);
   let distCat2 = Math.abs(cat2 - mouse);
-  let capture = "";
+  let capture = '';
 
-  if(distCat1 < distCat2){
-    capture = "cat1";
-  } else if(distCat1 > distCat2){
-    capture = "cat2";
+  if (distCat1 < distCat2) {
+    capture = 'cat1';
+  } else if (distCat1 > distCat2) {
+    capture = 'cat2';
   } else {
-    capture = "os gatos trombam e o rato foge";
+    capture = 'os gatos trombam e o rato foge';
   }
-
   return capture;
 }
 
@@ -94,21 +85,18 @@ function fizzBuzz(arrayNums) {
 
   for(let num in arrayNums){
     if ((arrayNums[num] % 3 === 0) && (arrayNums[num] % 5 === 0)) {
-      resp = "fizzBuzz";
+      resp = 'fizzBuzz';
     }  else if (arrayNums[num] % 3 === 0) {
-        resp = "fizz";
+        resp = 'fizz';
     } else if((arrayNums[num]) % 5 === 0) {
-      resp = "buzz";
+      resp = 'buzz';
     } else {
-      resp = "bug!";
+      resp = 'bug!';
     }
-
     arrayResp.push(resp);
   }
-
   return arrayResp;
 }
-
 
 // Desafio 9
 function encode() {
