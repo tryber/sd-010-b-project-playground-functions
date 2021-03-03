446aab6e4bf3ff1a0d52dffd6200488fd30b4380
Italo Matos <italomatos764@gmail.com>
2021-03-03 17:19:44 -0400

    Adicionando funcao generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,11 +18,44 @@ function techList(array, name) {
   return resultado;
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length > 11) return "Array com tamanho incorreto.";
+  else {
+  let frequente = array[0];
+  let count;
+  for (let i = 0; i < array.length; i++){
+    count = 0;
+    
+    for(let j = 0; j< array.length; j++){
+      if (array[j] == array[i]) count ++;
+    }
+    if (count >= 3 || array[i] < 0 || array[i] > 9){
+      return "não é possível gerar um número de telefone com esses valores";
+      break;
+    }
+  }
+
+  let ddd = [];
+  let numero = [];
+  let numero2 = [];
+  let letra;
+  for(let k = 0; k < array.length; k++){
+    letra = array[k].toString();
+    if(k == 0 || k == 1){
+      ddd.push(letra);
+    }
+    else if(k > 1 && k < 7){
+      numero.push(letra);
+    }
+    else{
+      numero2.push(letra);
+    }
+  }
+  let resultado = ddd.join('') + ' ' + numero.join('') + '-' + numero2.join('');
+  
+  return resultado;
+  }
 }
 
 // Desafio 12
@@ -40,4 +73,4 @@ module.exports = {
   techList,
   hydrate,
   triangleCheck,
-};
+}
