df408282be567af9e4c8d5ac03dde3f0b320dcac
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-02 23:02:48 -0300

    .

--- src/challenges.js
+++ src/challenges.js
@@ -8,7 +8,7 @@ function compareTrue(a, b) {
 }
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height)/2;
+  let area = (base * height) / 2;
   return area;
 }
 // Desafio 3
@@ -18,10 +18,10 @@ function splitSentence(palavra) {
 }
 // Desafio 4
 function concatName(array) {
-  return (array[array.length -1] + ', ' + array[0])
+  return (array[array.length -1] + ', ' + array[0]);
 }
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   let points = wins * 3 + ties * 1;
   return (points);
 }
@@ -43,103 +43,94 @@ function highestCount(array) {
 }
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-
+  let mouse=0
+  let cat1=3
+  let cat2=2
   let distcat1 = (mouse + cat1);
   let distcat2 = (mouse + cat2);
-  if (distcat1 < distcat2){
-    return('cat1');
-  } else if ( distcat2 < distcat1) {
-    return ('cat2');
+  if (distcat1 < distcat2) {
+    console.log('cat1');
+  } else if (distcat2 < distcat1) {
+    console.log('cat2');
   } else {
-    return ('os gatos trombam e o rato foge');
+    console.log('os gatos trombam e o rato foge');
   }
 
 }
 // Desafio 8
 function fizzBuzz(array) {
-  let fizzBuzz = [];
+  let arrayFizzBuzz = [];
   for (let index = 0; index < array.length; index += 1) {
     if ((array[index] % 3 === 0) && (array[index] % 5 != 0)) {
-      fizzBuzz.push('fizz');
+      arrayFizzBuzz.push('fizz');
     } else if ((array[index] % 5 === 0) && (array[index] % 3 != 0)) {
-      fizzBuzz.push('buzz');
+      arrayFizzBuzz.push('buzz');
     } else if ((array[index] % 3 === 0) && (array[index] % 5 === 0)) {
-      fizzBuzz.push('fizzBuzz');
+      arrayFizzBuzz.push('fizzBuzz');
     } else {
-      fizzBuzz.push('bug!');
+      arrayFizzBuzz.push('bug!');
     }
 
   }
-  return (fizzBuzz)
+  return (arrayFizzBuzz)
 }
-
-
-
 // Desafio 9
 function encode(string) {
   string.split('');
   let frase = '';
-  for (let index = 0; index < string.length; index++) {
+  for (let index = 0; index < string.length; index +=1) {
     switch (string[index]) {
       case 'a' :
-        string[index] = '1';
-        break;
-
+      string[index] = '1';
+      break;
       case 'e' :
-        string[index] = '2';
-        break;
-
+      string[index] = '2';
+      break;
       case 'i' :
-        string[index] = '3';
-        break;
+      string[index] = '3';
+      break;
       case 'o' :
-        string[index] = '4';
-        break;
+      string[index] = '4';
+      break;
       case 'u' :
-        string[index] = '5';
-        break;
+      string[index] = '5';
+      break;
     }
   }
   for (let index = 0; index < string.length; index += 1) {
     frase += `"${string[index]}"`;
   }
-return frase;
+  return frase;
 
 }
 
 
 function decode(string) {
   string.split('');
-  let frase = "";
+  let frase = '';
   for (let index = 0; index < string.length; index += 1) {
-    switch (string[index]){
-
+    switch (string[index]) {
       case '1' :
-        string[index] = 'a';
-        break;
-
+      string[index] = 'a';
+      break;
       case '2' :
-        string[index] = 'e';
-        break;
-
+      string[index] = 'e';
+      break;
       case '3' :
-        string[index] = 'i';
-        break;
-
+      string[index] = 'i';
+      break;
       case '4' :
-        string[index] = 'o';
-        break;
-
+      string[index] = 'o';
+      break;
       case '5' :
-        string[index] = 'u';
-        break;
+      string[index] = 'u';
+      break;
   }
 }
   for (let index = 0; index < string.length; index += 1) {
     frase += `"${string[index]}"`;
 }
 return (frase);
-
 }
 
 module.exports = {
