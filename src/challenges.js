4089c3dbfd3cd853eed6287a756f82da7826312f
Camila Arruda <camilaam1802@gmail.com>
2021-03-03 09:47:50 -0300

    Ajustando erros no lint

--- src/challenges.js
+++ src/challenges.js
@@ -4,7 +4,7 @@ function compareTrue(val1, val2) {
   if (val1 === true && val2 === true) {
     return true;
   }
-    return false;
+  return false;
 }
 
 // Desafio 2
@@ -25,19 +25,19 @@ function splitSentence(sentence) {
 function concatName(arrayStrings) {
   // seu código aqui
   let stringAux = arrayStrings; 
-  let stringConcat = stringAux.pop() + ', ' + stringAux.shift();  
+  let stringConcat = stringAux.pop() + ", " + stringAux.shift();  
   return stringConcat;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   // seu código aqui
   let somatorio = 0; 
-    if(wins > 0 && ties > 0){
-      somatorio = (wins * 3) + ties;
-      return somatorio;  
-    } 
-      return somatorio; 
+  if (wins > 0 && ties > 0) {
+    somatorio = (wins * 3) + ties;
+    return somatorio;  
+  } 
+    return somatorio; 
 }
 
 // Desafio 6
@@ -49,15 +49,15 @@ function highestCount() {
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let distCat1 = Math.abs(cat1 - mouse);
-    let distCat2 = Math.abs(cat2 - mouse); 
+  let distCat2 = Math.abs(cat2 - mouse); 
 
-    if(distCat1 < distCat2){
-        return "cat1"; 
-    } else if (distCat2 < distCat1) {
-        return "cat2"; 
-    } else {
-        return "Os gatos trombam e o rato foge"; 
-    }
+  if (distCat1 < distCat2) {
+    return 'cat1'; 
+  } if (distCat2 < distCat1) {
+    return 'cat2'; 
+  } else {
+    return 'Os gatos trombam e o rato foge'; 
+  }
 }
 
 // Desafio 8
