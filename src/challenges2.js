5b27601dfec7bd4b822dc5a77763095a00160da4
Matheus F. Carretta <matheuscarretta@gmail.com>
2021-03-03 01:20:43 -0300

    Desafio 10 - Retorna uma lista de objetos ordenados

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayList, name) {
+  let firstElementAux = '';
+  if(arrayList === []){
+    return 'Vazio!';
+  } else {
+    arrayList.sort();
+    let tamanhoArray = arrayList.length;
+    for(let index = 0; index < tamanhoArray; tamanhoArray -= 1){
+      firstElementAux = arrayList[index];
+      arrayList.shift()
+      arrayList.push(
+        {
+          tech: firstElementAux,
+          name: name,
+        })
+    }
+    return arrayList;
+  }
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
