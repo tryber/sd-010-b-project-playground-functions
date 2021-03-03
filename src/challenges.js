813a288775ba2ef8f60ee0243b73b0a8915bff36
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-03 17:27:30 -0300

    Adiciona alterações nos desafios realizados com switch-case, substituindo com if/else

--- src/challenges.js
+++ src/challenges.js
@@ -11,7 +11,7 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = base * height / 2;
+  let area = (base * height) / 2;
   return area;
 }
 
@@ -25,9 +25,8 @@ function splitSentence(string) {
 // Desafio 4
 function concatName(array) {
   let ultimoItem = '';
-  let primeiroItem = '';
-  for (let i in array) {
-    primeiroItem = array[0];
+  let primeiroItem = array[0];
+  for (let i = 0; i < array.length; i += 1) {
     ultimoItem = array[i];
   }
   return ultimoItem + ', ' + primeiroItem;
@@ -45,26 +44,25 @@ function highestCount(arrayOfValues) {
   let biggest = arrayOfValues[0];
   let sum = 0;
   for (let i in arrayOfValues) {
-    if (arrayOfValues[i] >= biggest){
+    if (arrayOfValues[i] >= biggest) {
       biggest = arrayOfValues[i];
     }
   }
-  for (let counter in arrayOfValues){
-    if (arrayOfValues[counter] === biggest){
+  for (let counter in arrayOfValues) {
+    if (arrayOfValues[counter] === biggest) {
       sum += 1;
     }
   }
   return sum;
 }
 
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let lesserDistance = [];
-  for (let index = 0; index < 3; index += 1){
+  for (let index = 0; index < 3; index += 1) {
     if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
       lesserDistance = 'cat1';
-    } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)){
+    } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
       lesserDistance = 'cat2';
     } else {
       lesserDistance = 'os gatos trombam e o rato foge';
@@ -76,20 +74,15 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(arrayOfNumbers) {
   let result = [];
-  for(let key in arrayOfNumbers){
-    switch(true){
-    case ((arrayOfNumbers[key] % 3 === 0) && (arrayOfNumbers[key] % 5 !== 0)):
+  for (let key in arrayOfNumbers) {
+    if ((arrayOfNumbers[key] % 3 === 0) && (arrayOfNumbers[key] % 5 !== 0)) {
       result.push('fizz');
-      break;
-    case ((arrayOfNumbers[key] % 3 !== 0) && (arrayOfNumbers[key] % 5 === 0)):
+    } else if ((arrayOfNumbers[key] % 3 !== 0) && (arrayOfNumbers[key] % 5 === 0)) {
       result.push('buzz');
-      break;
-    case ((arrayOfNumbers[key] % 3 === 0) && (arrayOfNumbers[key] % 5 === 0)):
+    } else if ((arrayOfNumbers[key] % 3 === 0) && (arrayOfNumbers[key] % 5 === 0)) {
       result.push('fizzBuzz');
-      break;
-    case ((arrayOfNumbers[key] % 3 !== 0) && (arrayOfNumbers[key] % 5 !== 0)):
-      result.push('bug!');
-      break;
+    } else if ((arrayOfNumbers[key] % 3 !== 0) && (arrayOfNumbers[key] % 5 !== 0)) {
+      result.push('bug!'); 
     }
   }
   return result;
@@ -99,24 +92,18 @@ function fizzBuzz(arrayOfNumbers) {
 function encode(string) {
   let result = [];
   for(let key in string){
-    switch (true){
-    case (string[key] === 'a'):
+    if (string[key] === 'a'){
       result += 1;
-      break;
-    case (string[key] === 'e'):
+    } else if (string[key] === 'e') {
       result += 2;
-      break;
-    case (string[key] === 'i'):
+    } else if (string[key] === 'i') {
       result += 3;
-      break;
-    case (string[key] === 'o'):
+    } else if (string[key] === 'o') {
       result += 4;
-      break;
-    case (string[key] === 'u'):
+    } else if (string[key] === 'u') {
       result += 5;
-      break;
-    default:
-      result += string[key];      
+    } else {
+      result += string[key];
     }
   }
   return result;
@@ -125,23 +112,17 @@ function encode(string) {
 function decode(stringWithNumbers) {
   let result = [];
   for(let key in stringWithNumbers){
-    switch (true) {
-    case (stringWithNumbers[key] == 1):
+    if (stringWithNumbers[key] == 1){
       result += 'a';
-      break;
-    case (stringWithNumbers[key] == 2):
+    } else if (stringWithNumbers[key] == 2) {
       result += 'e';
-      break;
-    case (stringWithNumbers[key] == 3):
+    } else if (stringWithNumbers[key] == 3) {
       result += 'i';
-      break;
-    case (stringWithNumbers[key] == 4):
+    } else if (stringWithNumbers[key] == 4) {
       result += 'o';
-     break;
-    case (stringWithNumbers[key] == 5):
+    } else if (stringWithNumbers[key] == 5) {
       result += 'u';
-      break;
-    default:
+    } else {
       result += stringWithNumbers[key];
     }
   }
