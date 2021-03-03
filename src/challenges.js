af479dd93e0170e36eb01eac30fb0fd660f6c6cb
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-03-03 12:19:31 -0300

    exercise 2

--- src/challenges.js
+++ src/challenges.js
@@ -7,11 +7,12 @@ function compareTrue(item1, item2) {
     return false;
   }
 }
-console.log(compareTrue(false, true));
+
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
