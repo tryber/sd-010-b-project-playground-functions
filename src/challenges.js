83ed41b29f9dccfdba9756d632813f85423ee462
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-02 21:28:08 -0300

    cria função 6

--- src/challenges.js
+++ src/challenges.js
@@ -31,25 +31,41 @@ console.log(splitSentence('vamo que vamo'));
 console.log(splitSentence('foguete'));
 
 // Desafio 4
-function concatName(parametro) {
+function concatName(parameter) {
   let result;
-  for (let index = 0; index < parametro.length; index+=1) {
-    result = (parametro[parametro.length-1] + ', ' +parametro[0]);
+  for (let index = 0; index < parameter.length; index+=1) {
+    result = (parameter[parameter.length-1] + ', ' +parameter[0]);
   }
     return result;
 }
 console.log(concatName(['Lucas','Cassiano','Ferraz','Paolillo']));
 
-
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+let result;
+
+function footballPoints(wins, ties) {
+  let winsvalue = (wins * 3);
+  let tiesvalue = (ties * 1);
+    result = winsvalue + tiesvalue;
+    return result;
 }
+console.log(footballPoints(14,8));
+console.log(footballPoints(1,2));
+console.log(footballPoints(0,0));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(numbers) {
+  let higherNumber = numbers[0];
+  let qtd = 0;
+
+  for (let index=0; index < numbers.length; index+=1) {
+    if (higherNumber == numbers[index] ) {
+        qtd = qtd + 1;
+      }
+    }
+    return qtd;
+  }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse() {
--- src/teste.js
+++ src/teste.js
@@ -1,7 +1,10 @@
-let parametro = ['Lucas','Cassiano','Ferraz','Paolillo'];
-let result;
+let numbers = [9, 1, 2, 3, 9, 5, 7];
+let higherNumber = numbers[0];
+let qtd = 0;
 
-for (let index = 0; index < parametro.length; index+=1) {
-    result = (parametro[parametro.length-1] + ', ' +parametro[0]);
+for (let index=0; index < numbers.length; index+=1) {
+c8  if (higherNumber == numbers[index] ) {
+    qtd = qtd + 1;
   }
-  console.log(result);
+}
+console.log(qtd);
