929e8f8f0abb96ceef3cc1bfdf3867285a9846e7
Mariana Mohr <marianamohr@gmail.com>
2021-03-03 13:12:09 -0300

    Ajustado erro no desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,18 +17,19 @@ function generatePhoneNumber(num) {
   contadorMaiorNumero = Math.max(...contador);
 
   console.log(contador);
-  if (maiorNum > 10 || menorNumero < 0 || contadorMaiorNumero >= 3) {
+  if (num.length < 11 || num.length > 11) {
+    mensagem = 'Array com tamanho incorreto.';
+    
+   } else if (maiorNum > 10 || menorNumero < 0 || contadorMaiorNumero >= 3) {
     mensagem = 'não é possível gerar um número de telefone com esses valores';
 
-  } else if (num.length < 11 || num.length > 11) {
-    mensagem = 'Array com tamanho incorreto.';
-  } else {
+  }  else {
     mensagem = '(' + num[0] + num[1] + ')' + ' ' + num[2] + num[3] + num[4] + num[5] + num[6] + '-' + num[7] + num[8] + num[9] + num[10];
   }
   return mensagem;
 }
 
-let numeros = [0, 2, 3, 4, 5, 7, 7, 8, 9, 0, 7];
+let numeros = [0, 2, 3, 2, 5, 7, 7, 8, 9, 0, 4, 6];
 console.log(generatePhoneNumber(numeros));
 
 // Desafio 12
