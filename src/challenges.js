0c97d1031612078c9491acba50687c28630a615d
aline <aline.melryaa@gmail.com>
2021-03-03 18:47:51 -0400

    atualizaçao do eslit

--- src/challenges.js
+++ src/challenges.js
@@ -1,39 +1,45 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a, b) {
   // seu código aqui
-     return (a===b && b===a);
-   
-  }
-let d = compareTrue(true,false);
+  return a === b && b === a;
+}
+let d = compareTrue(true, false);
 console.log(d);
 
 // Desafio 2
-function calcArea(base,heigth) {
+function calcArea(base, heigth) {
   // seu código aqui
-  return (base*heigth)/2;
+  return (base * heigth) / 2;
 }
-console.log(calcArea(51,1));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence(arryNames) {
   // seu código aqui
-return arryNames.split('/');
+  return arryNames.split('/');
 }
-console.log(splitSentence("go/trybe"));
+console.log(splitSentence('go\trybe'));
 
 // Desafio 4
 function concatName(arrNames) {
   // seu código aqui
-  let arryNames=['captain', 'my', 'captain'];
-  let result= arryNames.concat();
+  let arryNames = ['captain', 'my', 'captain'];
+  let result = arryNames.concat();
   return arrNames;
 }
-console.log(concatName('captain, captain' ));
+console.log(concatName('captain, captain'));
 // Desafio 5
-function footballPoints() {
+function footballPoints(quantPoints) {
   // seu código aqui
-}
+  let wins = 3;
+  let ties = 1;
 
+  return quantPoints;
+  if (partida >= 3) {
+  } else {
+    quatEmpate = partida + 1;
+  }
+}
 // Desafio 6
 function highestCount() {
   // seu código aqui
