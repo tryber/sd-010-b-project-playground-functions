349fe01696b221c20288eb3cb95c7ecdec573b7e
Mariana Mohr <marianamohr@gmail.com>
2021-03-03 12:21:43 -0300

    Ajustado teste ter algum numero mais que tres vezes

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,22 +9,26 @@ function generatePhoneNumber(num) {
   let maiorNum = Math.max(...num);
   let menorNumero = Math.min(...num);
   let contadorMaiorNumero = 0;
+  let contador = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
   for (let key in num) {
-    if (num[key] === maiorNum) {
-      contadorMaiorNumero += 1;
-    }
+    contador[num[key]] += 1;
   }
-  if (num.length < 11 || num.length > 11) {
-    mensagem = 'Array com tamanho incorreto.';
-  } else if (maiorNum > 10 || menorNumero > 0 || contadorMaiorNumero >= 3) {
+  contadorMaiorNumero = Math.max(...contador);
+
+  console.log(contador);
+  if (maiorNum > 10 || menorNumero < 0 || contadorMaiorNumero >= 3) {
     mensagem = 'não é possível gerar um número de telefone com esses valores';
 
+  } else if (num.length < 11 || num.length > 11) {
+    mensagem = 'Array com tamanho incorreto.';
   } else {
     mensagem = '(' + num[0] + num[1] + ')' + ' ' + num[2] + num[3] + num[4] + num[5] + num[6] + '-' + num[7] + num[8] + num[9] + num[10];
   }
   return mensagem;
 }
-let numeros = [9, 2, 3, 0, 5, -6, 7, 8, -7];
+
+let numeros = [0, 2, 3, 4, 5, 7, 7, 8, 9, 0, 7];
 console.log(generatePhoneNumber(numeros));
 
 // Desafio 12
