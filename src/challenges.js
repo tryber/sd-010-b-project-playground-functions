2fffb08dd0d19f5a18ef84b3a1360dd394971133
Camila <camilafabiabh9@gmail.com>
2021-03-03 14:33:25 -0300

    adicionando nova skill

--- src/challenges.js
+++ src/challenges.js
@@ -21,25 +21,33 @@ console.log(calcArea(51, 1));
 // Desafio 3
 function splitSentence() { // Referencias: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split  
   let myString = 'Vamos que vamos';
-  let splits = myString.split(' ',);
+  let splits = myString.split(' ');
   return splits;
 }
 console.log(splitSentence());
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(lassFirstArray) {
+  return lassFirstArray[lassFirstArray.length -1] + ',' + lassFirstArray[0]; 
 }
-
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (3*wins)+ties;
+  return points;
 }
-
+console.log(footballPoints(14,8));
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numArray) {
+  let maiorArray = 0;
+  for(let index = 0; index < numArray.length; index += 1 ){
+    if( numArray[index] > maiorArray) {     
+      maiorArray = maiorArray + 1;  
+    }   
+  }
+  return maiorArray;
 }
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
 
 // Desafio 7
 function catAndMouse() {
