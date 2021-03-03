b9c9d5281865ecf567ca069326a7e8053302effc
Aladino Borges <eualadino@gmail.com>
2021-03-03 10:58:04 -0300

    Requisito 9.B: algoritmo que decodifica uma dada string.

--- src/challenges.js
+++ src/challenges.js
@@ -140,8 +140,7 @@ function fizzBuzz(nums) {
 function encode(uncodedString) {
   // seu código aqui
   let letrasCodificar = ['a', 'e', 'i', 'o', 'u'];
-  let algoritmo = [1, 2, 3, 4, 5]
-  
+  let algoritmo = [1, 2, 3, 4, 5];
   let saveToCode = '';
   let saveCodedString = '';
 
@@ -158,9 +157,28 @@ function encode(uncodedString) {
 // O console.log() abaixo serve somente para finalidade de teste.
 // console.log(encode("hi there!"));
 
-function decode() {
+function decode(codedString) {
   // seu código aqui
+  let leAlgoritmo = [1, 2, 3, 4, 5];
+  let letrasDescodificar = ['a', 'e', 'i', 'o', 'u'];
+  let saveToDecode = '';
+  let saveDecodedString = '';
+
+  for (let indexDecode = 0; indexDecode < codedString.length; indexDecode += 1) {
+    saveToDecode = codedString[indexDecode];
+    for (let indexDecoding = 0; indexDecoding < leAlgoritmo.length; indexDecoding += 1) {
+
+      if (leAlgoritmo[indexDecoding] == saveToDecode) {
+        saveToDecode = letrasDescodificar[indexDecoding];
+      }
+    }
+    saveDecodedString += saveToDecode;
+  }
+  return saveDecodedString;
 }
+
+// O console.log() abaixo serve somente para finalidade de teste.
+// console.log(decode("h3 th2r2!"));
 
 module.exports = {
   calcArea,
