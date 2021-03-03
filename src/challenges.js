0d558c99090304b9e584ea8926f642942ca563f3
Caio <thehaseol@gmail.com>
2021-03-02 21:37:42 -0300

    tentativa desafio do 07, parte 1

--- src/challenges.js
+++ src/challenges.js
@@ -35,8 +35,15 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  }
+  if ((Math.abs(mouse - cat1) > Math.abs(mouse - cat2))) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
