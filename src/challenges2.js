d2ecaba523fc64f03b24f57f71243167b6ad5f93
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-03 02:07:06 -0300

    desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -41,12 +41,20 @@ console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 8]));
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  let difA = Math.abs(lineB - lineC);
+  if (lineA < lineB + lineC && lineA > difA) {
+    return true;
+  }
+  return false;
+
 }
+// console log
+console.log(triangleCheck(5, 2, 5));
 
 // Desafio 13
-
+// utilizado para esse desafio: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 function hydrate(bebidas) {
   let count = 0;
   let regex = /\d/g;
