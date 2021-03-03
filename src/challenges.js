8a2240dae67fcebfda5ad5335c369f7899484009
Breno Rodrigues <brenorm@gmail.com>
2021-03-02 23:37:59 -0300

    Implementaçao Desafio 4: concatName

--- src/challenges.js
+++ src/challenges.js
@@ -1,28 +1,29 @@
 // ok - Desafio 1
 function compareTrue(boolean1, boolean2) {
-  // Ref: https://stackoverflow.com/questions/45817581/replace-this-if-then-else-statement-by-a-single-return-statement //
-  // Após sugestão do ESlint, usei a referência acima para refatoração do cógico, antes implementado com uso do IF/ELSE //
+  // Ref: https://stackoverflow.com/questions/45817581/replace-this-if-then-else-statement-by-a-single-return-statement
+  // Após sugestão do ESlint, usei a referência acima para refatoração do cógico, antes implementado com uso do IF/ELSE
   return boolean1 && boolean2;
 }
-console.log('Resultado do 1º Desafio: ' + compareTrue(false, false));
+console.log(compareTrue(false, false));
 
 // ok - Desafio 2
 function calcArea(base, height) {
   return ((base * height) / 2);
-}
-console.log('Resultado do 2° Desafio: ' + calcArea(51, 1));
+  }
+console.log(calcArea(51, 1));
 
 // ok - Desafio 3
 function splitSentence(sentence) {
-  let wordsArray = sentence.split(" ");
-  return wordsArray;
+  return(sentence.split(' '));
 }
-console.log('Resultado do 2° Desafio: ' + splitSentence('Go Trybe'));
+console.log(splitSentence('Go Trybe'));
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+// ok - Desafio 4
+function concatName(stringsArray) {
+  let ultimoPrimeiroItem = (stringsArray[stringsArray.length -1] + ", " + stringsArray[0]);
+  return ultimoPrimeiroItem;
 }
+console.log(concatName( ['captain', 'my', 'captain']));
 
 // Desafio 5
 function footballPoints() {
