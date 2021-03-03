d4b594e33fb6ffe9b356b702ce796a0b1c10e037
Camila Arruda <camilaam1802@gmail.com>
2021-03-03 09:54:25 -0300

    Corrigindo mais erros lint

--- src/challenges.js
+++ src/challenges.js
@@ -24,21 +24,21 @@ function splitSentence(sentence) {
 // Desafio 4
 function concatName(arrayStrings) {
   // seu código aqui
-  let stringAux = arrayStrings; 
-  let stringConcat = stringAux.pop() + ", " + stringAux.shift();  
+  let stringAux = arrayStrings;
+  let stringConcat = (stringAux.pop() + ', ' + stringAux.shift());
   return stringConcat;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  let somatorio = 0; 
+  let somatorio = 0;
   if (wins > 0 && ties > 0) {
     somatorio = (wins * 3) + ties;
-    return somatorio;  
-  } 
-    return somatorio; 
-}
+    return somatorio;
+  }
+  return somatorio;
+  }
 
 // Desafio 6
 function highestCount() {
@@ -49,16 +49,14 @@ function highestCount() {
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let distCat1 = Math.abs(cat1 - mouse);
-  let distCat2 = Math.abs(cat2 - mouse); 
-
+  let distCat2 = Math.abs(cat2 - mouse);
   if (distCat1 < distCat2) {
-    return 'cat1'; 
+    return 'cat1';
   } if (distCat2 < distCat1) {
-    return 'cat2'; 
-  } else {
-    return 'Os gatos trombam e o rato foge'; 
+    return 'cat2';
+  } 
+    return 'Os gatos trombam e o rato foge';
   }
-}
 
 // Desafio 8
 function fizzBuzz() {
