4cfc5ed9d580441d74c301451bd1a7b6371777b3
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-02 22:44:16 -0300

    10 - Crie uma função de Lista de tecnologias DONE

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+let techs = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+let name = "Jonnes";
+console.log(techList(techs, name));
+function techList(techs, name) {
+  let list = [];
+  for (let i = 0; i < techs.length; i++) {
+    newTech = {
+      tech: techs[i],
+      name: name
+    };
+    list.push(newTech) // I found this push() function at https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
+  }
+  list.sort(sortByTech);
+  return list;
+}
+
+// I found this logic at https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+function sortByTech(a, b) {
+  if ( a.tech < b.tech ){
+    return -1;
+  }
+  if ( a.tech > b.tech ){
+    return 1;
+  }
+  return 0;
 }
 
 // Desafio 11
