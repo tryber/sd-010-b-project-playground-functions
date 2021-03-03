dd5b5af0886820d06bfe182ea9fd8ffc27dcdd8d
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-03 13:16:35 -0300

    space

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,10 +4,11 @@ function techList(tech, name) {
   tech.sort();
   if (tech.length <= 0) {
     return 'Vazio!';
-  } for (let index in tech){
-    jobs.push({
-      tech: tech[index],
-      name: name,
+  }
+    for (let index in tech){
+      jobs.push({
+        tech: tech[index],
+        name: name,
     });
   }
   return jobs;
