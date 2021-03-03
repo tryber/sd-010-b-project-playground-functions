96d68b3078b687e8730709c4d28f70ddbd3004e6
Thiago Kachinsky <tjk091@gmail.com>
2021-03-02 22:53:12 -0300

    7 - Caça ao rato - arrumando

--- src/challenges.js
+++ src/challenges.js
@@ -56,14 +56,14 @@ console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let gato1 = cat1 - mouse
-  let gato2 = cat2 - mouse
+  let gato1 = Math.abs(cat1 - mouse);
+  let gato2 = Math.abs(cat2 - mouse);
 
   if (gato1 < gato2) {
-    return 'cat1'
+    return 'cat1';
   } 
   if (gato2 < gato1) {
-    return 'cat2'
+    return 'cat2';
   }
 
   return 'os gatos trombam e o rato foge'
