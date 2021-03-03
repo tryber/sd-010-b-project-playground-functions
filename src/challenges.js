50757e0b897cbab4d895a901050299ad52c6555f
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-02 23:42:08 -0300

    deixando mais semantico

--- src/challenges.js
+++ src/challenges.js
@@ -39,8 +39,8 @@ function highestCount(numeros) {
     if (numeros[index] > indiceMaior) {
       indiceMaior = numeros[index];
     } 
-}
-  for(index2 = 1; index2 <= numeros.length; index2 += 1) {
+  }
+  for (index2 = 1; index2 <= numeros.length; index2 += 1) {
     if (numeros[index2] === indiceMaior) {
       contRepetido += 1;
     }
@@ -49,24 +49,23 @@ function highestCount(numeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanciaCat1 = mouse - cat1; 
-  
-  if (distanciaCat1 < 0){
-    distanciaCat1 = cat1 - mouse
+  let distanciaCat1 = mouse - cat1;  
+  if (distanciaCat1 < 0) {
+    distanciaCat1 = cat1 - mouse;
   }
-     let distanciaCat2 = mouse - cat2;
-     if (distanciaCat2 < 0){
-      distanciaCat2 = cat2 - mouse
-    }
-     if(distanciaCat1 - mouse === distanciaCat2 - mouse) {
-      return "os gatos trombam e o rato foge";          
+  let distanciaCat2 = mouse - cat2;
+  if (distanciaCat2 < 0) {
+    distanciaCat2 = cat2 - mouse;
+  }
+  if(distanciaCat1 - mouse === distanciaCat2 - mouse) {
+    return "os gatos trombam e o rato foge";          
     }   
-     else if (distanciaCat1 < distanciaCat2 ) {
-        return "cat1"
-    } else{
-        return "cat2"
-    } 
-  } console.log(catAndMouse(5, 8, 3))
+  else if (distanciaCat1 < distanciaCat2 ) {
+    return "cat1";
+  } else {
+    return "cat2";
+  } 
+} console.log(catAndMouse(5, 8, 3))
  
 // Desafio 8
 function fizzBuzz(Array) {
@@ -74,34 +73,32 @@ function fizzBuzz(Array) {
   for (let index = 0; index <= Array.length-1; index ++) {
     if (Array[index] % 3 === 0 && Array[index] % 5 === 0) {
       resultado.push('fizzBuzz'); 
-    } else if(Array[index] % 5 === 0 && Array[index] % 2 !== 0 && Array[index] % 3 !== 0){
+    } else if (Array[index] % 5 === 0 && Array[index] % 2 !== 0 && Array[index] % 3 !== 0){
      resultado.push("buzz");
-    } else if(Array[index] % 3 === 0 && Array[index] % 2 !== 0 && Array[index] % 5 !== 0) {
-      resultado.push("fizz");
+    } else if (Array[index] % 3 === 0 && Array[index] % 2 !== 0 && Array[index] % 5 !== 0) {
+     resultado.push("fizz");
     } else {
-      resultado.push("bug!")
-    }  
-}
+      resultado.push("bug!");
+    }
+  }
 return resultado
-}
-console.log(fizzBuzz([7, 9]))
+}console.log(fizzBuzz([7, 9]))
 
 // Desafio 9
 function encode(palavra) {
   let resultado = "";
-  for(index = 0; index <= palavra.length-1; index++){
-    
-    if(palavra[index] === "a"){
+  for (index = 0; index <= palavra.length-1; index++){
+    if (palavra[index] === "a"){
       resultado += 1; 
-    } else if(palavra[index] === "e"){
+    } else if (palavra[index] === "e") {
       resultado += 2;
-    } else if(palavra[index] === "i"){
+    } else if (palavra[index] === "i") {
       resultado += 3; 
-    } else if(palavra[index] === "o"){
+    } else if (palavra[index] === "o") {
       resultado += 4;
-    } else if(palavra[index] === "u"){
+    } else if (palavra[index] === "u") {
       resultado += 5;
-    } else{
+    } else {
       resultado += palavra[index];
     }
   }
@@ -110,19 +107,18 @@ function encode(palavra) {
 
 function decode(palavra) {
   let resultado = "";
-  for(index = 0; index <= palavra.length-1; index++){
-    
-    if(palavra[index] === 1){
-      resultado += "a"; 
-    } else if(palavra[index] === 2){
+  for (index = 0; index <= palavra.length-1; index++){
+    if (palavra[index] === 1) {
+      resultado += "a";
+    } else if (palavra[index] === 2) {
       resultado += "e";
-    } else if(palavra[index] === 3){
-      resultado += "i"; 
-    } else if(palavra[index] === 4){
+    } else if (palavra[index] === 3) {
+      resultado += "i";
+    } else if (palavra[index] === 4) {
       resultado += "o";
-    } else if(palavra[index] === 5){
+    } else if (palavra[index] === 5) {
       resultado += "u";
-    } else{
+    } else {
       resultado += palavra[index];
     }
   }
