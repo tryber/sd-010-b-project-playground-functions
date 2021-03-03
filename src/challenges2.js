537b376040ba32d19115fff1d9384f862a4cb7f8
Thiago Kachinsky <tjk091@gmail.com>
2021-03-03 09:22:51 -0300

    10 - Lista de tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  if(tech.length === 0) {
+    return 'Vazio!'
+  }
+
+  const arr = tech.sort().map( function(nomes) {
+     return Object.assign({tech: nomes},{name: name})
+  })
+
+  return arr
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Thiago'))
 
 // Desafio 11
 function generatePhoneNumber() {
