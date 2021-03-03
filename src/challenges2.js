60c1d7ad044fd448ce5d234a3f0591926dadbde9
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-03 13:37:08 -0300

    atualiza o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,10 +19,28 @@ console.log(techList([],"Lucas")); */
 
 // Desafio 11
 function generatePhoneNumber(arrayNumbers) {
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9) {
+      return 'Array com tamanho incorreto.';
+    }
+    let cont = 0;
+    for (let index2 = 0; index2 < arrayNumbers.length; index2 += 1) {
+      if(arrayNumbers[index] === arrayNumbers[index2]) {
+        cont += 1;
+      }
+    }
+    if(cont >= 3) {
+      return 'Array com tamanho incorreto';
+    }
+  }
+  let arrayNumber = ['(' + arrayNumbers[0],arrayNumbers[1] +') ', 
+  arrayNumbers[2], arrayNumbers [3], arrayNumbers[4], arrayNumbers[5] + '-',
+  arrayNumbers[6], arrayNumbers[7], arrayNumbers[9], arrayNumbers[10]];
 
+  let number = arrayNumber.join('');
+  return number;
 }
-
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+/* console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])); */
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
