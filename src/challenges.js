72cea1d024a64c1fbe2715c0225c421aa395b552
Thiago da Paz <thiagopazjunior@gmail.com>
2021-03-03 20:32:22 -0300

    Continuando a ajustar o ESlint.

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,7 @@
 // Desafio 1
 function compareTrue(a, b) {
   // seu código aqui
+  let c;
   if (a && b) {
     c = true;
   } else {
@@ -11,19 +12,19 @@ function compareTrue(a, b) {
 console.log(compareTrue(0, 1));
 
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base, height) {
   // seu código aqui
   let resultado = (base * height) / 2;
   return resultado;
 }
-console.log(calcArea(2,5));
+console.log(calcArea(2, 5));
 
 // Desafio 3
 function splitSentence(string) {
   // seu código aqui
-  let separador = string.split(' ')
+  let separador = string.split(' ');
   return separador;
-};
+}
 console.log(splitSentence('Agora vai !!!'));
 
 // Desafio 4
@@ -31,17 +32,17 @@ function concatName(nomes) {
   // seu código aqui
   let concatenacao = nomes[nomes.length - 1] + ', ' + nomes[0];
   return concatenacao;
-};
+}
 console.log(concatName(['Thiago', 'Beatriz', 'Pedro', 'Barbára']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let pontos = 0;
-  pontos += wins * 3; 
+  pontos += wins * 3;
   pontos += ties;
   return pontos;
-};
+}
 console.log(footballPoints(4, 2));
 
 // Desafio 6
@@ -52,15 +53,15 @@ function highestCount(numArray) {
   for (let key in numArray) {
     if (numArray[key] > highestNum) {
       highestNum = numArray[key];
-    };
-  };
+    }
+  }
   for (let key2 in numArray) {
     if (numArray[key2] === highestNum) {
       counter += 1;
-    }; 
-  };
+    }
+  }
   return counter;
-};  
+} 
 console.log(highestCount([9, 1, 10, 9, 9, 2, 3, 9, 5, 7]));
 
 // Desafio 7
