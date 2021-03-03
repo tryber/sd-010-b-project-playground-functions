398ed68498574ff3be7ae4ffd7a7b8c3dd7db302
Caio <thehaseol@gmail.com>
2021-03-02 22:24:04 -0300

    tentativa desafio do 06 novamente

--- src/challenges.js
+++ src/challenges.js
@@ -1,102 +1,90 @@
 // Desafio 1
-function compareTrue(boolean1, boolean2) {
-  let bolfalse = false;
-  if (boolean1 === true && boolean2 === true) {
-    bolfalse = true;
+function  compareTrue ( boolean1 ,  boolean2 )  {
+  deixe  bolfalse  =  false ;
+  if  ( boolean1  ===  true  &&  boolean2  ===  true )  {
+    bolfalse  =  verdadeiro ;
   }
-  return bolfalse;
+  retornar  bolfalse ;
 }
-console.log(compareTrue(true, false));
+console . log ( compareTrue ( true ,  false ) ) ;
 
 // Desafio 2
-function calcArea(base, height) {
-  return (base * height) / 2;
+function  calcArea ( base ,  height )  {
+  retorno  ( base * altura ) / 2 ;
 }
 // Desafio 3
-function splitSentence(frase) {
-  let palavras = frase.split(' ');
-  return palavras;
+function  splitSentence ( frase )  {
+  deixe  Palavras  =  frase . dividir ( '' ) ;
+  return  palavras ;
 }
 // Desafio 4
-function concatName(array) {
-  const concatenated = `${array[(array.length - 1)]}, ${array[0]}`;
+function  concatName ( array )  {
+  const  concatenado  =  ` $ { array [ ( array . comprimento  -  1 ) ] } , $ { array [ 0 ] } ` ;
 
-  return concatenated;
+  retorno  concatenado ;
 }
 // Desafio 5
-function footballPoints(wins, ties) {
-  const totalpoints = (wins * 3) + ties;
+função  footballPoints ( vitórias ,  empates )  {
+  const  totalpoints  =  ( vitórias * 3 )  +  empates ;
 
-  return totalpoints;
+  retornar  totalpoints ;
 }
 // Desafio 6
-function highestCount(arrayDeNumeros) {
-  let contagem = { };
-  let maiorNumero = 0;
-  for (let itens of arrayDeNumeros) {
-    contagem[itens] = 0;
-  }
-  for (let itens of arrayDeNumeros) {
-    contagem[itens] += 1;
-  }
-  for (let itens in arrayDeNumeros) {
-    if (itens > maiorNumero) {
-      itens = maiorNumero;
+function  higherCount ( array )  {
+  deixe  max  =  array [ 0 ] ;
+  deixe  nVezes  =  0 ;
+  // Verificar qual maior número
+  para  ( deixe a  chave  na  matriz )  {
+    if  ( array [ chave  +  1 ] > = max )  {
+      max  =  matriz [ tecla  +  1 ] ;
     }
   }
-  return contagem[maiorNumero];
+
+  para  ( deixe a  chave  no  array )  if  ( array [ chave ]  ===  max )  nVezes  + =  1 ;
 }
 
 // Desafio 7
+function  catAndMouse ( mouse ,  cat1 ,  cat2 )  {
+  deixe  distCat1  =  Math . abs ( cat1  -  camundongo ) ;
+  deixe  distCat2  =  Math . abs ( cat2  -  camundongo ) ;
+  deixe  alcancaPrimeiro  =  '' ;
 
-function catAndMouse(mouse, cat1, cat2) {
-  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
-    return 'cat1';
+  if  ( distCat1  <  distCat2 )  {
+    alcancaPrimeiro  =  'cat1' ;
   }
-  if ((Math.abs(mouse - cat1) > Math.abs(mouse - cat2))) {
-    return 'cat2';
+  if  ( distCat2  <  distCat1 )  {
+    alcancaPrimeiro  =  'cat2' ;
   }
-  return 'os gatos trombam e o rato foge';
+  if  ( distCat1  ===  distCat2 )  {
+    alcancaPrimeiro  =  'os gatos trombam e o rato foge' ;
+  }
+  voltar  alcancaPrimeiro ;
+}
+
 }
 
 // Desafio 8
-function  fizzBuzz ( arrayDeNumeros )  {
-  let  resposta  =  [ ] ;
-  for  ( let  index  =  0 ;  index  <  arrayDeNumeros . length ;  index  + =  1 )  {
-    if  ( ( arrayDeNumeros [ index ] % 3  ===  0 )  &&  ( arrayDeNumeros [ index ] % 5  ===  0 ) ) {
-      RESPOSTA . push ( 'fizzbuzz' ) ;
-    }
-    else  if  ( arrayDeNumeros [ index ] % 3  ===  0 ) {
-      RESPOSTA . empurre ( 'fizz' ) ;
-    }
-    else  if  ( arrayDeNumeros [ index ] % 5  ===  0 ) {
-      RESPOSTA . push ( 'zumbido' ) ;
-    }
-    else  {
-      RESPOSTA . push ( 'bug!' ) ;
-    }
-  }
-  retorno  resposta ;
+function  fizzBuzz ( )  {
+  // seu código aqui
 }
-
 
 // Desafio 9
-function encode() {
+function  encode ( )  {
   // seu código aqui
 }
-function decode() {
+function  decode ( )  {
   // seu código aqui
 }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+módulo . exportações  =  {
+  calcArea ,
+  catAndMouse ,
+  compareTrue ,
+  concatName ,
+  decodificar ,
+  codificar ,
+  fizzBuzz ,
+  footballPoints ,
+  maximumCount ,
+  splitSentence ,
+} ;
