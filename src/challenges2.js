3c2d5c18047948465928fd6fdef1f3f48657da01
esionascimento <esio.392@protonmail.com>
2021-03-03 00:27:33 -0300

    11-generatePhoneNumber adicionado variavel telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,7 +17,7 @@ function techList(tech, name) {
 // Desafio 11
 function generatePhoneNumber(a) {
   if (a.length !== 11) {
-    return 'Array com tamanho incorreto';
+    return 'Array com tamanho incorreto.';
   }
   for (let i = 0; i < a.length; i += 1) {
     let count = 1;
@@ -30,7 +30,8 @@ function generatePhoneNumber(a) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
   }
-  return `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
+  let telefone = `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
+  return telefone;
 }
 
 // Desafio 12
