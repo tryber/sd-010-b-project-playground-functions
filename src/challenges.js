2ec61a3f155e3a40d72d9bd8bb0d076e330741da
Thiago Leite <leite.tjs@gmail.com>
2021-03-03 20:55:25 -0300

    challenge 8

--- src/challenges.js
+++ src/challenges.js
@@ -25,13 +25,13 @@ console.log(splitSentence('foguete nao tem ré'))
 
 // Desafio 4
 function concatName(array) {
-  conca = "";
+  let conca = "";
   conca = array[array.length-1];
   conca = conca + ", " + array[array.length - array.length]
   
   return conca;   
 } 
-console.log(concatName(['captain', 'my', 'captain']))
+console.log(concatName(['thiago', 'jonas', 'marcos','captain']))
 
 
 // Desafio 5
@@ -56,9 +56,24 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+     
+  let string = [];
+  for (i = 0; i < numbers.length; i += 1){
+    if (numbers[i]% 3 == 0 && numbers[i] % 5 == 0) {
+      string[i] = 'fizzBuzz';
+    } else if (numbers[i] % 3 == 0) {
+      string[i]= 'fizz';
+    } else if (numbers[i] % 5 == 0) {
+      string[i] = 'buzz';
+    } else {
+      string[i]= 'bug!';
+    }
+
+  }
+  return string
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
 
 // Desafio 9
 function encode() {
