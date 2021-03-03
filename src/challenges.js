336509fb74a529631eef81adc09dc0bb32d94451
root <diegoserafim1@gmail.com>
2021-03-02 22:39:37 -0300

    D01 condiçao esta entre aspas

--- src/challenges.js
+++ src/challenges.js
@@ -11,16 +11,19 @@ function compareTrue(valorUm, valorDois) {
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  let formula = 0;
-  if ((base * height) / 2 === 250) {
-    return formula = 250;
-  }
-  if ((base * height) / 2 === 5) {
-    return formula = 5;
-  }
-  if ((base * height) / 2 === 25.5) {
-    return formula = 25.5;
-  }
+  let formula = (base * height) / 2;
+  
+  switch (formula) {
+  case 250:
+    return 250;
+    break;
+
+  case 5:
+    return 5;
+    break;
+  
+  case 25.5:
+    return 25.5
 }
 
 // Desafio 3
