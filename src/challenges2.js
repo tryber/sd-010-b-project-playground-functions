5377e8990049f0db20e5d93fe7614590ef149e19
Italo Matos <italomatos764@gmail.com>
2021-03-02 21:06:02 -0400

    Adicionando funcao techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  if (array.length == 0){
+    return 'Vazio!';
+  }
+
+  let tecnologias = {
+    tech: 'NomeTech',
+    name: name
+  };
+
+  let resultado = [];
+  array.sort();
+  for (let i = 0; i < array.length; i++){
+    resultado.push({'tech':array[i], 'name':name});
+  }
+
+  return resultado;
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
