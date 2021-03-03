9d46be94b9f44d6650ec6969bfcd183d464bdb35
hzozimo <hzozimo@gmail.com>
2021-03-03 01:18:20 -0300

    challenges2.js totalmente no eslint

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,7 +2,7 @@
 function techList(nomesTecnologias, nome) {
   let nomesTecnologiasEmOrdem = nomesTecnologias.sort();
   let objeto = [];
-    if (nomesTecnologias.length === 0) { //linha de comparação vista no código de vbodra https://github.com/tryber/sd-010-b-project-playground-functions/blob/vbodra-playground-functions/src/challenges2.js
+  if (nomesTecnologias.length === 0) { // linha de comparação vista no código de vbodra https://github.com/tryber/sd-010-b-project-playground-functions/blob/vbodra-playground-functions/src/challenges2.js
     objeto = 'Vazio!';
   } else {
     for (let tecnologias of nomesTecnologiasEmOrdem) {
@@ -16,14 +16,6 @@ function techList(nomesTecnologias, nome) {
 }
 
 // Desafio 11
-
-function tamanho(arrayDeNumeros) {
-  let saida = `(${arrayDeNumeros[0]}${arrayDeNumeros[1]}) ${arrayDeNumeros[2]}${arrayDeNumeros[3]}${arrayDeNumeros[4]}${arrayDeNumeros[5]}${arrayDeNumeros[6]}-${arrayDeNumeros[7]}${arrayDeNumeros[8]}${arrayDeNumeros[9]}${arrayDeNumeros[10]}`;
-  if (arrayDeNumeros.length !== 11) {
-    saida = 'Array com tamanho incorreto.';
-  }
-  return saida;
-}
 
 function repeticaoDeTresOuMais(arrayDeNumeros) {
   let maisQueTres = false;
@@ -41,30 +33,42 @@ function repeticaoDeTresOuMais(arrayDeNumeros) {
   }
   return maisQueTres;
 }
-
-function generatePhoneNumber(arrayDeNumeros) {
-  let saida = `(${arrayDeNumeros[0]}${arrayDeNumeros[1]})${arrayDeNumeros[2]}${arrayDeNumeros[3]}${arrayDeNumeros[4]}${arrayDeNumeros[5]}${arrayDeNumeros[6]}-${arrayDeNumeros[7]}${arrayDeNumeros[8]}${arrayDeNumeros[9]}${arrayDeNumeros[10]}`;
-  saida = tamanho(arrayDeNumeros);
+function numerosImpossiveis(arrayDeNumeros) {
+  let saida = false;
   for (let numero of arrayDeNumeros) {
     if ((numero < 0) || (numero > 9) || repeticaoDeTresOuMais(arrayDeNumeros)) {
-      saida = 'não é possível gerar um número de telefone com esses valores.';
+      saida = true;
     }
   }
   return saida;
 }
 
+function generatePhoneNumber(arrayDeNumeros) {
+  let saida = '';
+  if (arrayDeNumeros.length !== 11) {
+    saida = 'Array com tamanho incorreto.';
+  } else if (numerosImpossiveis(arrayDeNumeros)) {
+    saida = 'não é possível gerar um número de telefone com esses valores.';
+  } else {
+    saida = `(${arrayDeNumeros[0]}${arrayDeNumeros[1]})${arrayDeNumeros[2]}${arrayDeNumeros[3]}`;
+    saida += `${arrayDeNumeros[4]}${arrayDeNumeros[5]}${arrayDeNumeros[6]}-${arrayDeNumeros[7]}`;
+    saida += `${arrayDeNumeros[8]}${arrayDeNumeros[9]}${arrayDeNumeros[10]}`;
+  }
+  return saida;
+}
+
 // Desafio 12
-function priT (lineA, lineB, lineC) {
+function priT(lineA, lineB, lineC) {
   let primeiro = ((lineA < (lineB + lineC)) && (lineA > Math.abs(lineB - lineC)));
   return primeiro;
 }
 
-function segT (lineA, lineB, lineC) {
+function segT(lineA, lineB, lineC) {
   let segundo = ((lineB < (lineA + lineC)) && (lineB > Math.abs(lineA - lineC)));
   return segundo;
 }
 
-function terT (lineA, lineB, lineC) {
+function terT(lineA, lineB, lineC) {
   let terceiro = (lineC < (lineB + lineA)) && ((lineC > Math.abs(lineB - lineA)));
   return terceiro;
 }
@@ -84,7 +88,9 @@ function hydrate(frase) {
   let soma = 0;
   let saida = '';
   for (let index in numeros) {
-    soma += parseInt(numeros[index], 10);
+    if ({}.hasOwnProperty.call(numeros, index)) {
+      soma += parseInt(numeros[index], 10);
+    }
   }
   if (soma > 1) {
     saida = `${soma} copos de água`;
