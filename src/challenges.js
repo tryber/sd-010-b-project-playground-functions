d9805d0fb13751f8e8afa8e32bdde075458773a6
Dieghonm <dieghonm@gmail.com>
2021-03-03 01:15:53 -0300

    Refatorando o  7

--- src/challenges.js
+++ src/challenges.js
@@ -57,29 +57,15 @@ function highestCount(numeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let gato1 = 0;
-  let gato2 = 0;
-  if (cat1 > mouse) {
-    gato1 = cat1 - mouse;
-  } else {
-    gato1 = mouse - cat1;
-  }
-  if (cat2 > mouse) {
-    gato2 = cat2 - mouse;
-  } else {
-    gato2 = mouse - cat2;
-  }
-  if (gato1 !== gato2) {
-    if (gato1 < gato2) {
+  if (Math.abs(cat1 - mouse) !== Math.abs(cat2 - mouse)) {
+    if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
       return 'cat1';
     } else {
-    return 'cat2';
+      return 'cat2';
     }
   }
   return 'os gatos trombam e o rato foge';
 }
-// Math.abs
-
 // Desafio 8
 function fizzBuzz(numeros) {
   let resposta = [];
