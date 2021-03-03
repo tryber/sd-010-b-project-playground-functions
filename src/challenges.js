6d57e6b4045befdc820eff045265380264208ecb
Camila <camilafabiabh9@gmail.com>
2021-03-03 14:51:34 -0300

    Adicionando nova skil

--- src/challenges.js
+++ src/challenges.js
@@ -2,30 +2,20 @@
 function compareTrue(valor1, valor2) {
   if (valor1 === true && valor2 === true) {
     return true;
-  }else if ( valor1 === true && valor2 === false || valor1 === false && valor2 === true) {
-    return false;
-  } else if (valor1 === false && valor2 === false) {
+  } else{
     return false;
   }
 }
-console.log(compareTrue(false, false)); 
 // Desafio 2
 function calcArea(base, altura) {
   let calcArea = (base * altura) / 2;
   return calcArea;  
 }
-console.log(calcArea(10, 50));
-console.log(calcArea(5, 2));
-console.log(calcArea(51, 1));
-
 // Desafio 3
-function splitSentence() { // Referencias: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split  
-  let myString = 'Vamos que vamos';
+function splitSentence(myString) { // Referencias: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split;
   let splits = myString.split(' ');
   return splits;
 }
-console.log(splitSentence());
-
 // Desafio 4
 function concatName(lassFirstArray) {
   return lassFirstArray[lassFirstArray.length -1] + ',' + lassFirstArray[0]; 
@@ -36,7 +26,6 @@ function footballPoints(wins, ties) {
   let points = (3*wins)+ties;
   return points;
 }
-console.log(footballPoints(14,8));
 // Desafio 6
 function highestCount(numArray) {
   let maiorArray = 0;
