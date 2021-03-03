50ed75a201356ff92b361392d27dc02c52579e8d
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-02 22:38:46 -0300

    atualizando desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -82,13 +82,13 @@ function fizzBuzz(arrayNumber) {
   let arraySaida = [];
   for (let key in arrayNumber) {
     if (arrayNumber[key] % 3 == 0 && arrayNumber[key] % 2 != 0 && arrayNumber[key] % 5 != 0) {
-      arraySaida.push("fizz");
+      arraySaida[key] = "fizz";
     } else if (arrayNumber[key] == 5 || arrayNumber[key] == 25) {
-      arraySaida.push("buzz");
+      arraySaida[key] = "buzz";
     } else if (arrayNumber[key] % 3 == 0 && arrayNumber[key] % 5 == 0) {
-      arraySaida.push("fizzBuzz");
+      arraySaida[key] = "fizzBuzz";
     } else if (arrayNumber[key] % 3 != 0 && arrayNumber[key] % 5 != 0) {
-      arraySaida.push("bug!");
+      arraySaida[key] = "bug!";
     }
   }
   return arraySaida;
