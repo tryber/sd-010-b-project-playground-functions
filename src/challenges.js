71736a3d741603033ddc0fea4e2374b84a8e3c10
Thiago Leite <leite.tjs@gmail.com>
2021-03-02 21:34:55 -0300

    desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,12 @@
 // Desafio 1
 function compareTrue(boolean1, boolean2) {
-  if(boolean1 === true && boolean2 === true){
+  if (boolean1 === true && boolean2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false 
  
 }
-console.log(compareTrue(false, false));
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, altura) {
@@ -18,21 +17,30 @@ console.log(calcArea(10, 5));
 
 // Desafio 3
 function splitSentence(a) {
-  frase = a
+  let frase = a
   return a.split(" ")
 }
 
-console.log(splitSentence('foguete'))
+console.log(splitSentence('foguete nao tem ré'))
 
 // Desafio 4
 function concatName() {
   
+
+
+
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = 0;
+  if (wins > 0 || ties > 0) {
+    return pontos = (wins * 3) + (ties * 1);
+  } else {
+    return pontos;
+  }
 }
+console.log(footballPoints(14,8))
 
 // Desafio 6
 function highestCount() {
