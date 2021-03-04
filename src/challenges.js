5f2adfda6124d3f13eb892f26939763374aa0c84
Camila <camilafabiabh9@gmail.com>
2021-03-04 19:09:25 -0300

    function

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,18 @@
 function compareTrue(valor1, valor2) {
   if (valor1 === true && valor2 === true) {
     return true;
-  } else{
+  } else {
     return false;
   }
 }
+//console.log(compareTrue(true,false));
 // Desafio 2
 function calcArea(base, altura) {
   let calcArea = (base * altura) / 2;
   return calcArea;  
 }
 // Desafio 3
-function splitSentence(myString) { // Referencias: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split;
+function splitSentence(myString) { // (Referencias sobre splits nesse site): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split;
   let splits = myString.split(' ');
   return splits;
 }
@@ -23,20 +24,20 @@ function concatName(lassFirstArray) {
 // console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = (3*wins)+ties;
+  let points = (3 * wins) + ties;
   return points;
 }
 // Desafio 6
 function highestCount(numbersArray) {
   let maiorArray = 0;
   let calcRep = 0;
-  for(let index = 0; index < numbersArray.length; index += 1 ){
-    if( numbersArray[index] > maiorArray) {     
+  for(let index = 0; index < numbersArray.length; index += 1 ) {
+    if (numbersArray[index] > maiorArray) {     
       maiorArray = numbersArray[index];  
     }   
   }
   for(let index2 = 0; index2 < numbersArray.length; index2 += 1) {
-    if(numbersArray[index2] == maiorArray ) {
+    if (numbersArray[index2] == maiorArray ) {
       calcRep += 1;
     }
   }
@@ -44,13 +45,13 @@ function highestCount(numbersArray) {
 }
 // Desafio 7
 function catAndMouse(cat1, cat2, mouse) {
- let distCat1 = Math.abs(mouse - cat1);
- let distCat2 = Math.abs(mouse - cat2);
- if (distCat1 < distCat2) {
-   return 'cat1';
+  let distCat1 = Math.abs(mouse - cat1);
+  let distCat2 = Math.abs(mouse - cat2);
+   if (distCat1 < distCat2) {
+    return 'cat1';
  } else if (distCat2 < distCat1) {
    return 'cat2';
- } else{
+ } else {
    return 'os gatos trombam e o rato foge';
  }
 }
@@ -74,18 +75,19 @@ function fizzBuzz(numArray) {
 //console.log(fizzBuzz([2, 15, 7, 9, 45, 45, 15, 2]));
 // Desafio 9
  function encode(vowel) { // (Apredi como usar funcão replace) https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-  return vowel.replace(/a/g,"1")
-    .replace(/e/g, "2") // (Sobre colocar colocar g) https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
-    .replace(/i/g, "3")
-    .replace(/o/g, "4")
-    .replace(/u/g, "5");
+  return vowel.replace(/a/g, '1')
+    .replace(/e/g, '2') //  https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
+    .replace(/i/g, '3')
+    .replace(/o/g, '4')
+    .replace(/u/g, '5');
 }
+//console.log(encode("he there"));
 function decode(numberVowel) {
-  return numberVowel.replace(/1/g, "a")
-    .replace(/2/g, "e")
-    .replace(/3/g, "i")
-    .replace(/4/g, "o")
-    .replace(/5/g, "u");
+  return numberVowel.replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
 }
 module.exports = {
   calcArea,
