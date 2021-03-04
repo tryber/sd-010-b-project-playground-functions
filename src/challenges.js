97bbcd575e2d722f3b32dfb378c85efb6d12067a
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-04 10:55:09 -0300

    executado requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -4,8 +4,13 @@ function compareTrue() {
 }
 
 // Desafio 2
-function calcArea() {
-  
+function calcArea(base, height) {
+ let area = (base * height) / 2;
+  return area;
+}
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1)); 
 }
 
 // Desafio 3
