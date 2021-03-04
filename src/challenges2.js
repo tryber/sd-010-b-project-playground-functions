debc47e1d502512bb9c06f236b07c97df8617fc6
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-03 21:10:07 -0300

    11 - Crie uma funcao de Numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,10 +25,70 @@ function techList(array, nameUser) {
 /* console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
 "Lucas")) */
 
+function tresVezesOuMais(numbers) {
+
+	numQtd = {}
+
+	for (let index in numbers) {
+		numQtd[numbers[index]] = 0
+	}
+
+	for (let i = 0; i < numbers.length; i++) {
+		for (let key in numQtd) {
+			if(numbers[i] == key){
+				numQtd[key] = numQtd[key]+1
+			}
+		}
+	}
+
+	numQtdToArray = []
+	cont = 0
+	for (let key in numQtd) {
+		numQtdToArray[cont++] = numQtd[key]
+	}
+
+	if (numQtdToArray.sort()[numQtdToArray.length-1] >=3) {
+
+		return false
+
+	} else {
+
+		return true
+
+	} 
+
+}
+
+
+
 // Desafio 11
 function generatePhoneNumber(numbers) {
 
-	stringNumber = ''
+	let menorQueZero = false
+	let maiorQueNove = false
+	let maiorQueTresNumeros = false
+
+	for (let index in numbers) {
+		if (numbers[index] < 0) {
+			menorQueZero = true
+		} else if (numbers[index] > 9) {
+			maiorQueNove = true
+		}
+	}
+
+	if (tresVezesOuMais(numbers) == false) {
+		maiorQueTresNumeros = true
+	}
+
+	if (numbers.length != 11) {
+
+		return 'Array com tamanho incorreto'
+
+	} else if (maiorQueNove || menorQueZero || maiorQueTresNumeros) {
+
+		return "não é possível gerar um número de telefone com esses valores"
+	} else {
+		stringNumber = ''
 
 	numbers.unshift('(')
 	numbers.splice(3,0,')')
@@ -39,11 +99,12 @@ function generatePhoneNumber(numbers) {
 		stringNumber += numbers[index]
 	}
 
-	console.log(stringNumber)
+	return stringNumber
+	}
   
-}
+} 
 
-//generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
