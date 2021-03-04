eca346aa88f9f716bfa0ee28331dc260cf876cac
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-04 19:07:15 -0300

    Adiciona desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  tech.sort();
+  let techArray = [];
+  if (tech == (0)) {
+    techArray = techArray.toString();
+    techArray = "Vazio!";
+  } else {
+    for (let index = 0; index < tech.length; index += 1) {
+      let technology = {};
+      technology["tech"] = tech[index];
+      technology["name"] = name;
+      techArray.push(technology);
+    }
+  }
+  console.log(techArray);
+  return techList;
 }
 
 // Desafio 11
