25a499cdf772752b0052b6f06e741324d8f864e3
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-03 21:32:08 -0300

    Corrigindo erro lint desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -46,12 +46,12 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-    let distance1 = (cat1 - mouse);
-    let distance2 = (cat2 - mouse);
-    if (distance1 < distance2) return 'cat1';
-    if (distance2 < distance1) return 'cat2';
-    if (distance1 === distance2) return 'os gatos trombam e o rato foge';
-  }
+  let distance1 = (cat1 - mouse);
+  let distance2 = (cat2 - mouse);
+  if (distance1 < distance2) return 'cat1';
+  if (distance2 < distance1) return 'cat2';
+  if (distance1 === distance2) return 'os gatos trombam e o rato foge';
+}
 
 // Desafio 8
 function fizzBuzz(myArray) {
@@ -72,11 +72,9 @@ function fizzBuzz(myArray) {
 
 // Desafio 9
 function encode() {
-
 }
 
 function decode() {
-
 }
 
 
