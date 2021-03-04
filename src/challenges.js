7403b6402500af3410d0b49e6bafe8ce03e7bf7d
tomsantos07 <xotmsantosx@live.com>
2021-03-04 20:05:00 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -68,8 +68,18 @@ function highestCount(arrayEntry) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 > cat2) {
+    return cat1;
+  }
+
+   if (cat2 > cat1) {
+    return cat2;
+  } 
+
+  else if (cat1 === cat2) {
+    return ("os gatos trombam e o rato foge.")
+  }
 }
 
 // Desafio 8
