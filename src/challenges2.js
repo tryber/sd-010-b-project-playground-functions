357f0efca9662c866d0a2a885244f0cba5082bd5
legalvao1 <legalvao1@gmail.com>
2021-03-04 09:31:11 -0300

    Funçao lista de tecnologias, adicionando 'vazio' caso a lista esteja vazia

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,18 +2,27 @@
 function techList(array, name) {
   let listaDeTecnologias = [];
   for (let index = 0; index < array.sort().length; index += 1) {
-    listaDeTecnologias.push({
-      'tech': array[index],
-      'name': name,
-    } )
+    listaDeTecnologias.push( {
+      tech: array[index],
+      name: name,
+    });
   }
+  if (listaDeTecnologias.length === 0) {
+    return 'Vazio!'
+  };
   return listaDeTecnologias;
 }
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'))
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let telefone = ''
+  if (array.length != 12) {
+    console.log("Array com tamanho incorreto.")
+  }
+  for (let i = 0; i < array.length; i +=1) {
+    if (array[i] < 0 || array[i] > 9)
+  }
 }
 
 // Desafio 12
