0f1ed3a3fe32c2adb33507f51d04d43ae02e3f44
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-03-03 22:33:13 -0300

    requsioto 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,28 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let qtt = array.length;
+  let list = [];
+  
+  if (qtt < 1) {
+    return 'Vazio!';
+  }
+  
+  for (let i in array) {
+    list.push({
+      teach: array[i], 
+      name: name,
+    });
+  }
+
+  //Utilizei esse questão do stackoverflow para resolver o prbolema abaixo
+  //https://pt.stackoverflow.com/questions/46600/como-ordenar-uma-array-de-objetos-com-array-sort
+  list.sort((a, b) => {
+    if (a.teach < b.teach) return -1;
+    if (a.teach > b.teach) return 1;
+    return 0;
+  });
+
+  return list;
 }
 
 // Desafio 11
