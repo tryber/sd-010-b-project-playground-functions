8c542b5a84e10f038c9375c91991bbfcf4f2b877
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-03 23:17:10 -0300

    Desafio 7 mudando forma

--- src/challenges.js
+++ src/challenges.js
@@ -48,9 +48,13 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let distance1 = (mouse - cat1);
   let distance2 = (mouse - cat2);
-  if (distance1 < distance2) return 'cat1';
-  if (distance2 < distance1) return 'cat2';
-  if (distance1 === distance2) return 'os gatos trombam e o rato foge';
+  if (distance1 < distance2) {
+    return "cat2";
+  } if (distance2 < distance1) {
+    return 'cat1';
+  } if (distance1 === distance2) {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
