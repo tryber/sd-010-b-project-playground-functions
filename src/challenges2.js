840e49aeabc9cbe46c195b0396b51a81c02e8375
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-03 23:47:42 -0300

    11º exercicio 'Phone Number' feito e 10º arrumado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,7 @@
 // Desafio 10
 function techList(array, name) {
   let techArray = [];
+  array = array.sort();
   for (let element of array) {
     let techObject = {
       tech: element,
@@ -16,11 +17,78 @@ function techList(array, name) {
   return techArray;
 }
 
-console.log(techList([], "Matheus"));
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function numberOfRepetition(array, number) {
+  let count = 0;
+  for (let num of array) {
+    if (number === num) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function findRepeated(array) {
+  let repeatedObject = {};
+  for (let num of array) {
+    repeatedObject[num] = numberOfRepetition(array, num);
+  }
+
+  return repeatedObject;
+}
+
+function higherThan(array, number) {
+  let higher = false;
+  let object = findRepeated(array);
+  for (let repetition in object) {
+    if (object[repetition] >= number) {
+      higher = true;
+    }
+  }
+  return higher;
+}
+
+function isPhoneValid(array) {
+  let filtered = array.filter((value) => value > 9 || value < 0);
+  let greaterThanThree = higherThan(array, 3);
+
+  if (filtered.length > 0 || greaterThanThree) {
+    return false;
+  }
+
+  return true;
+}
+
+function addPhoneStructure(string) {
+  if (string.length === 0) {
+    string += '(';
+  }
+  if (string.length === 3) {
+    string += ')';
+  }
+  if (string.length === 9) {
+    string += '-';
+  }
+  return string;
+}
+
+function generatePhoneNumber(array) {
+  let phoneNumber = '';
+
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (!isPhoneValid(array)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  array.forEach((value) => {
+    phoneNumber = addPhoneStructure(phoneNumber);
+    phoneNumber += value;
+  });
+
+  return phoneNumber;
 }
 
 // Desafio 12
