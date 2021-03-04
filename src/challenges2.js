d822eb89a7133b7368da0cca7b940cf2dc60ef6b
Matheus F. Carretta <matheuscarretta@gmail.com>
2021-03-04 00:54:42 -0300

    Desafio 11 - Mascara para numero de celular

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,16 +7,60 @@ function techList(arrayList, name) {
   } else {
     arrayList.sort();
     for (let index in arrayList) {
-      organizedArray.push({tech: arrayList[index], name: name,})
+      organizedArray.push({
+        tech: arrayList[index],
+        name: name,
+      })
     }
   }
   return organizedArray;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumber) {
+  let countRepeatNumbers = 0;
+  let verifyRepeatNumbers = false;
+  let auxiliarString = '';
+  let aux = 0;
+  
+  if (arrayNumber.length > 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let index in arrayNumber) {
+      for (let secondIndex in arrayNumber) {
+        if (arrayNumber[index] === arrayNumber[secondIndex]) {
+          countRepeatNumbers += 1;
+        }
+      }
+      if (countRepeatNumbers >= 3) {
+        verifyRepeatNumbers = true;
+        countRepeatNumbers = 0;
+      } else {
+        countRepeatNumbers = 0;
+      }
+    }
+  }
+  for (let index in arrayNumber) {
+    if (arrayNumber[index] < 0 || arrayNumber[index] > 9 || verifyRepeatNumbers == true) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  } 
+ 
+  for(let index = 0; index < (arrayNumber.length +3); index += 1){
+    if(index == 0){
+      auxiliarString = auxiliarString + '(';
+    } else if (index == 3){
+      auxiliarString = auxiliarString + ')';
+    } else if ( index == 9){
+      auxiliarString = auxiliarString + '-';
+    } else {
+      auxiliarString = auxiliarString + arrayNumber[aux];
+      aux += 1;
+    }
+  }
+  console.log(auxiliarString)
 }
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 // Desafio 12
 function triangleCheck() {
