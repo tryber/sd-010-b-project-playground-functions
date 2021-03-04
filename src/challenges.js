a5820068dc547d3985b51bdd9dabfe58a4272e7e
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-03-04 12:53:26 -0300

    Subindo modificações

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 
-function compareTrue(boolean1, boolean2) {
+function compareTrue(boolean1,boolean2) {
 
   if (boolean1 === true && boolean2 === true) {
     return true;
@@ -12,8 +12,7 @@ function compareTrue(boolean1, boolean2) {
 // Desafio 2
 function calcArea(base, height) {
   let areaTriangulo;
-  areaTriangulo = (base*height) / 2;
-  
+  areaTriangulo = (base * height) / 2;
   return areaTriangulo;
 }
 
@@ -22,11 +21,10 @@ function splitSentence(frase) {
   let arrayFrase = [];
   arrayFrase = frase.split(" ");
   return arrayFrase;
-  }
+}
 
 // Desafio 4
 function concatName(primeiroItem) {
-  
   let conectaItem = (primeiroItem[primeiroItem.length - 1]) + ", " + (primeiroItem[0]);
   return conectaItem;
 
@@ -47,12 +45,12 @@ function highestCount(repeteMaior) {
   
   let maiorNumero;
   let qntNumeroMaior = 0;
-  for(let cont=0;cont < repeteMaior.length-1; cont += 1) {
-    if (repeteMaior[cont] >= repeteMaior[cont+1]) {
+  for (let cont=0;cont < repeteMaior.length - 1; cont += 1) {
+    if (repeteMaior[cont] >= repeteMaior[cont + 1]) {
       maiorNumero = repeteMaior[cont];
     }
   }
-  for(let cont=0;cont < repeteMaior.length; cont+=1) {
+  for (let cont=0;cont < repeteMaior.length; cont+=1) {
     if (maiorNumero === repeteMaior[cont]) {
       qntNumeroMaior += 1;
     }
@@ -65,17 +63,17 @@ function catAndMouse(mouse,cat1,cat2) {
   let distancia1 = Math.abs (cat1 - mouse);
   let distancia2 = Math.abs (cat2 - mouse);
 
-  if(distancia1 > distancia2){
+  if (distancia1 > distancia2) {
     
     let ganhardorCat2 = "cat2";
     return ganhardorCat2;
   
-  } else if (distancia1< distancia2){
+  } else if (distancia1< distancia2) {
     
     let ganhardorCat1 = "cat1";
     return ganhardorCat1;
 
-  } else if (distancia1 === distancia2){
+  } else if (distancia1 === distancia2) {
     let ratoFoge = "os gatos trombam e o rato foge";
     return ratoFoge;
   }
@@ -86,9 +84,9 @@ function catAndMouse(mouse,cat1,cat2) {
 function fizzBuzz(divisivelArray) {
   let resultadoDividir = [];
 
-  for(let cont=0; cont < divisivelArray.length; cont += 1){
+  for (let cont=0; cont < divisivelArray.length; cont += 1) {
 
-    if(divisivelArray[cont] % 3 == 0 && divisivelArray[cont] % 5 != 0) {
+    if (divisivelArray[cont] % 3 == 0 && divisivelArray[cont] % 5 != 0) {
       resultadoDividir.push("fizz");
     } else if (divisivelArray[cont] % 5 == 0 && divisivelArray[cont] % 3 != 0) {
       resultadoDividir.push("buzz");
@@ -109,26 +107,26 @@ function encode(paramCodificado) {
   
     let arrayCodificado = paramCodificado.split('');
 
-  for(cont=0; cont < arrayCodificado.length; cont += 1){
+  for(let cont=0; cont < arrayCodificado.length; cont += 1){
     if (arrayCodificado[cont] == a){
       
-      arrayCodificado[cont] = 1;
+      arrayCodificado[cont] = "1";
 
     } else if (arrayCodificado[cont] == e) {
 
-      arrayCodificado[cont] = 2;
+      arrayCodificado[cont] = "2";
 
     } else if (arrayCodificado[cont] == i) {
 
-      arrayCodificado[cont] = 3;
+      arrayCodificado[cont] = "3";
       
     } else if (arrayCodificado[cont] == o) {
       
-      arrayCodificado[cont] = 4;
+      arrayCodificado[cont] = "4";
 
     } else if (arrayCodificado[cont] == u){
 
-      arrayCodificado[cont] = 5;
+      arrayCodificado[cont] = "5";
 
     }
   }
@@ -138,26 +136,26 @@ function decode(paramDecodificado) {
 
   let arrayDecodificado = paramDecodificado.split('');
 
-  for(cont=0; cont < arrayDecodificado.length; cont += 1){
+  for(let cont=0; cont < arrayDecodificado.length; cont += 1){
     if (arrayDecodificado[cont] == 1){
       
-      arrayDecodificado[cont] = a;
+      arrayDecodificado[cont] = "a";
 
     } else if (arrayDecodificado[cont] == 2) {
 
-      arrayDecodificado[cont] = e;
+      arrayDecodificado[cont] = "e";
 
     } else if (arrayDecodificado[cont] == 3) {
 
-      arrayDecodificado[cont] = i;
+      arrayDecodificado[cont] = "i";
       
     } else if (arrayDecodificado[cont] == 4) {
       
-      arrayDecodificado[cont] = o;
+      arrayDecodificado[cont] = "o";
 
     } else if (arrayDecodificado[cont] == 5){
 
-      arrayDecodificado[cont] = u;
+      arrayDecodificado[cont] = "u";
 
     }
   }
