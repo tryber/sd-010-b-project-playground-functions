a8523435bdb14944abb15d748c83682bd51b2484
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-04 19:55:17 -0300

    Tentativa 5 correçao do EsLint

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,20 +2,20 @@
 function techList(tech, name) {
   tech.sort();
   let techArray = [];
-  if (tech == (0)) {
+  if (tech.length === 0) {
     techArray = techArray.toString();
     techArray = 'Vazio!';
   } else {
     for (let index = 0; index < tech.length; index += 1) {
       let technology = {};
-      technology['name'] = name;
-      technology['tech'] = tech[index];
+      technology.name = name;
+      technology.tech = tech[index];
       techArray.push(technology);
     }
   }
   return techArray;
 }
-// console.log(techList([], "Leandro"));
+// console.log(techList(["React", "CSS"], "Leandro"));
 
 // Desafio 11
 function generatePhoneNumber() {
