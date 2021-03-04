48ca4b75b1e22f017f387770f05466ee13dd46d8
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-04 12:14:29 -0300

    Adicionando resoluçao do exercicio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,16 +15,43 @@ function techList(techArray, yourName) {
   }
   return objArr;
 }
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+//console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrNumbers) {
+  /* let phoneNumber = [];
+  let openParenthesis = '(';
+  let closeParenthesis = ')';
+  let blank = ' ';
+  let dash = '-';
+  phoneNumber.push(openParenthesis);
+  //xNNxxNNNNNxNNNN
+  for (let index = 0; index < arrNumbers.length; index += 1) {
+    if (index >= 0 && index<=1) {
+      
+    }
+  } */
 }
+//console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let sumLinesAB = lineA+lineB;
+  let sumLinesAC = lineA+lineC;
+  let sumLinesBC = lineB+lineC;
+  let diffLinesAB = Math.abs(lineA - lineB);
+  let diffLinesAC = Math.abs(lineA - lineC);
+  let diffLinesBC = Math.abs(lineB - lineC);
+  let isTriangle;
+
+  if ((lineA > sumLinesBC) || (lineB > sumLinesAC) || (lineC > sumLinesAB)) {
+    isTriangle = false;
+  } else if ((lineA < diffLinesBC) || (lineB < diffLinesAC) || (lineC < diffLinesAB)) {
+    isTriangle = false;
+  } else {
+    isTriangle = true;
+  }
+  return isTriangle;
 }
 
 // Desafio 13
