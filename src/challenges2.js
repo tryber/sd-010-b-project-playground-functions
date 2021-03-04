994eba8d5ec3437f606eb4dbd8537fac94dbb86b
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-04 01:32:28 -0300

    Adiciona ao desafio 11 as restriçoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,40 @@ function techList (arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayNumbers) {
   // seu código aqui
+  let resp = ''
+  let numbers = arrayNumbers
+  // verificações
+  let bigThenTree = false; // maior que três
+  let negativeAndNine = false; // existe negativo e maior que 9
+  let rept = 0;
+  for (let num in numbers) {
+    let compare = numbers[num]; // variável de comparação que recebe os numeros
+    // negativos
+    if (compare < 0 || compare > 9) {
+      negativeAndNine = true;
+    }
+    // contagem de repetidos
+    let contNum = 0;  // zera a contagem para cada numero
+    for (let n in numbers) {
+      if (compare === numbers[n]){
+        contNum += 1;
+        // Quantidade de numeros repetidos for maior que 3
+        if (contNum >= 3) {
+          bigThenTree = true;
+        }
+      }
+    }
+  }
+  // first condition
+  if (numbers.length != 11) {
+    resp = "Array com tamanho incorreto."
+  // second condition
+  } else if (negativeAndNine === true || bigThenTree === true) {
+    resp = "não é possível gerar um número de telefone com esses valores"
+  } 
+  return resp;
 }
 
 // Desafio 12
