53f633cc89866823598f76f94b2540ffab691042
legalvao1 <legalvao1@gmail.com>
2021-03-04 10:31:03 -0300

    colocanco espaço no retorno da  funçao numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,37 +2,37 @@
 function techList(array, name) {
   let listaDeTecnologias = [];
   for (let index = 0; index < array.sort().length; index += 1) {
-    listaDeTecnologias.push( {
+    listaDeTecnologias.push({
       tech: array[index],
       name: name,
     });
   }
   if (listaDeTecnologias.length === 0) {
-    return 'Vazio!'
-  };
+    return 'Vazio!';
+  }
   return listaDeTecnologias;
 }
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber(array) {
-  let telefone = ''
-  if (array.length != 11) {
-    return "Array com tamanho incorreto.";
+  let telefone = '';
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
   }
-  for (let i = 0; i < array.length; i +=1) {
+  for (let i = 0; i < array.length; i += 1) {
     let numeroRepetido = 0;
-    for (let repetiçoes = 0; repetiçoes < array.length; repetiçoes += 1){
+    for (let repetiçoes = 0; repetiçoes < array.length; repetiçoes += 1) {
       if (array[i] === array[repetiçoes]) {
         numeroRepetido += 1;
       }
     }
-    if (array[i] < 0 || array[i] > 9 || numeroRepetido >= 3){
-      return "não é possível gerar um número de telefone com esses valores";
+    if (array[i] < 0 || array[i] > 9 || numeroRepetido >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
     } 
     telefone += array[i];
   }
-  return `(${telefone[0]}${telefone[1]})${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`
+  return `(${telefone[0]}${telefone[1]}) ${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`;
 }
 
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
