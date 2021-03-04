36494324871710bb004180026b85c59917002a91
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-04 00:13:49 -0300

    Alterando ordem desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -46,14 +46,14 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distance1 = (Math.abs.mouse - Math.abs.cat1);
-  let distance2 = (Math.abs.mouse - Math.abs.cat2);
-  if (distance1 < distance2) {
-    return "cat2";
-  } if (distance2 > distance1) {
-    return 'cat1';
-  } if (distance1 === distance2) {
+  let distance1 = Math.abs(cat1 - mouse);
+  let distance2 = Math.abs(cat2 - mouse);
+  if (distance1 === distance2) {
     return 'os gatos trombam e o rato foge'
+  } else if (distance1 > distance2) {
+    return 'cat2';
+  } else {
+    return 'cat1';
   }
 }
 
