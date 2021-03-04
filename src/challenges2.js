537a0a73d34ceb1c191051114b1029e9e18762ce
Thiago Kachinsky <tjk091@gmail.com>
2021-03-04 11:11:29 -0300

    Ajustes finais

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,59 +13,10 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Thiago'));
 
 // Desafio 11
 function generatePhoneNumber(number) {
-  let a = number.slice();
-
-  const repeat = function repeatedNumbers() {
-    let current = null;
-    let cnt = 0;
-    for (let i = 0; i < number.length; i++) {
-      if (number[i] !== current) {
-        if (cnt > 0) {
-          return cnt + 1;
-        }
-        current = number[i];
-        cnt = 1;
-      } else {
-        cnt++;
-      }
-    } 
-};
-
-
-  console.log(repeat());
-
-  const arrNum = function arrayNumbers() {
-    // Verificando a quantidade correta de números do array
-    if (number.length === 11) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const validator = function isValid() {
-    let validaNum = [];
-    // Verificando se os números são válidos
-    for (let i = 0; i < number.length; i++) {
-      if (number[i] < 0 || number[i] > 9) {
-        return false;
-      } else {
-        validaNum += validaNum;
-      }
-    }
-    return validaNum;
-  };
-
-  if (validator() === false || repeat() >= 3) {
-    return 'não é possível gerar um número de telefone com esses valores';
-  } else if (arrNum() === false) {
-    return 'Array com tamanho incorreto.';
-  } else {
-    return `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
-  }
+  
 }
 
-console.log(generatePhoneNumber([1, 1, 1, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
