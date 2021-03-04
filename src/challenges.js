530262bb6aa26e5bdf7c9f9cd0710d0add895a90
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-03-04 09:39:58 -0300

    attempt tp solve challenge 3

--- src/challenges.js
+++ src/challenges.js
@@ -12,11 +12,10 @@ function compareTrue(item1, item2) {
 function calcArea(base, height) {
   return (base * height) / 2;
 }
-console.log(calcArea(51, 1));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(content) {
+  return content.split(' ');
 }
 
 // Desafio 4
