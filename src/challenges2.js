336c09885e7fd373e3f3c55680851cd8454522aa
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-03 23:46:00 -0300

    Adiciona o desafio 10, retorna a lista de objetos em ordem.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,17 @@
 // Desafio 10
-function techList() {
+function techList (arrayTech, name) {
   // seu código aqui
-  
+  let arrayKnowHow = [];
+  let order = arrayTech.sort() // coloca o array em ordem
+  for (let t in order) {
+    let knowHow = {};     // cria um objeto para cada tech
+    knowHow.tech = order[t];
+    knowHow.name = name;
+    arrayKnowHow.push(knowHow);  // adiciona o objeto no array
+  }
+  return arrayKnowHow;   
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
