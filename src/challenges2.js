c95e989bd301b59c8e960def2455d3075c3cf3fb
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-04 19:30:13 -0300

    Editei o desafio 10 para se adequar ao sLint

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,14 +2,14 @@
 function techList(tech, name) {
   tech.sort();
   let techArray = [];
-  if (tech == (0)) {
+  if (tech === (0)) {
     techArray = techArray.toString();
-    techArray = "Vazio!";
+    techArray = 'Vazio!';
   } else {
     for (let index = 0; index < tech.length; index += 1) {
       let technology = {};
-      technology["name"] = name;
-      technology["tech"] = tech[index];
+      technology.name = name;
+      technology.tech = tech[index];
       techArray.push(technology);
     }
   }
