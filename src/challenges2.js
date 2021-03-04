ab3b7d1ddec64f540e34c734460003437ccdc3bd
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-03 21:31:11 -0300

    10º exercicio 'Tech List' feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let techArray = [];
+  for (let element of array) {
+    let techObject = {
+      tech: element,
+      name,
+    };
+    techArray.push(techObject);
+  }
+
+  if (!techArray.length) {
+    return 'Vazio!';
+  }
+
+  return techArray;
 }
+
+console.log(techList([], "Matheus"));
 
 // Desafio 11
 function generatePhoneNumber() {
