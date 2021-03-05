003d9c9e0fd53dfb5e1a0e8b6f2de4dce44c3eed
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-05 16:39:32 -0300

    challenge 11 update 1.2

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,27 +12,37 @@ function techList(tech, name) {
 
 // Desafio 11
 function generatePhoneNumber(phone) {
-  let count = 0; let result = '';
-  if (phone.length != 11) result = 'Array com tamanho incorreto.';
-  else result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
+  let result = '';
 
-  for (let i = 0; i < phone.length; i += 1) {
-    if (phone[i] < 0 || phone[i] > 9) {
-      result = 'não é possível gerar um número de telefone com esses valores';
-    }
-    for (let j = 0; j < phone.length; j += 1) {
-      count = 0;
-      if (phone[j] === phone[i]) count += 1;
+  if (phone.length > 11 || phone.length < 11){
+      result = "Array com tamanho incorreto.";
+  }
+  else {
+    result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
+    for (let i = 0; i < phone.length; i += 1) {
+      let count = 0;
+        if (phone[i] > 9 || phone[i] < 0) {
+        result = "não é possível gerar um número de telefone com esses valores";
+        }
+        for (let j = 0; j < phone.length; j += 1) {
+          if (phone[i] == phone[j]) {
+          count += 1;
+        }
+        if (count > 2){
+          result = "não é possível gerar um número de telefone com esses valores";
+        }
+      }
     }
   }
-
-  if (count > 2) result = 'não é possível gerar um número de telefone com esses valores';
   return result;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
+  let result = false; let lines = [];
+  lines.push(Math.abs(lineA), Math.abs(lineB), Math.abs(lineC));
 
+  if ((lines[0] + lines[1]))
 }
 
 // Desafio 13
