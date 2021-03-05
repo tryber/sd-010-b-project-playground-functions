d52169789e7232b2a774c4cfb07e05cf4588f2c6
Cesar Lopez <cglopez874@hotmail.com>
2021-03-05 04:48:00 -0300

    Primeiros dois exercicios

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,21 @@
 // Desafio 1
-function compareTrue() {
-  "teste" // seu código aqui
+function compareTrue(valor1, valor2) {
+  // seu código aqui
+if (valor1 == true && valor2 == true) {
+  return true;
+}
+else {
+  return false;
+}
+
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+let areatriangulo;
+areatriangulo = (base*height)/2;
+return areatriangulo;
 }
 
 // Desafio 3
