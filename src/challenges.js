73742d9e7e5d8a827f5dd17405ad8ef8a867427d
Gabriel Ribeiro <grbeiro237@gmail.com>
2021-03-05 20:30:05 -0300

    parte 8 corrigida 7

--- src/challenges.js
+++ src/challenges.js
@@ -60,17 +60,16 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(array) {
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] % 3 === 0) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      array[i] = 'fizzBuzz';
+    } else if (array[i] % 3 !== 0 && array[i] % 5 !== 0) {
+    array[i] = 'bug!';
+    } else if (array[i] % 3 === 0) {
       array[i] = 'fizz';
     } else if (array[i] % 5 === 0) {
       array[i] = 'buzz';
     }
-    if ((array[i] % 3) === 0 && (array[i] % 5) === 0) {
-      array[i] = 'fizzBuzz';
-    } else if ((array[i] % 3) !== 0 && (array[i] % 5) !== 0) {
-      array[i] = 'bug!';
-    }
-  }
+  } 
   return array;
 }
 
