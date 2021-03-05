0bd9e9078b8f5d0a3f0c784def97f435eb218e56
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-05 16:18:33 -0300

    challenge 11 complete

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,15 +4,28 @@ function techList(tech, name) {
   let result = [];
   if (list.length > 1) {
     for (let i = 0; i < list.length; i += 1) {
-      result.push({tech: list[i], name: name});
+      result.push({ tech: list[i], name: name });
     }
   } else result = 'Vazio!';
   return result;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phone) {
+  let count = 0;
+  if (phone.length != 11) result = "Array com tamanho incorreto.";
+  else result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
 
+  for (i in phone) {
+    if (phone[i] < 0 || phone[i] > 9) result = 'não é possível gerar um número de telefone com esses valores1';
+    for (j in phone) {
+      count = 0;
+      if (phone[j] == phone[i]) count += 1;
+    }
+  }
+
+  if (count >= 3) result = 'não é possível gerar um número de telefone com esses valores2';
+  return result;
 }
 
 // Desafio 12
