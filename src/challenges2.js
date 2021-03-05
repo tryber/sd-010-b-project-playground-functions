1b70eb4ab632da6abe6fcae453fa52e4505100a6
root <diegoserafim1@gmail.com>
2021-03-05 16:01:19 -0300

    D11 teste com for ifs e elses

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,16 +20,42 @@ function techList(array, name) {
 function generatePhoneNumber(array) {
   // seu código aqui
   let padraoTelefone;
-  let diferenteDoPadrao;
+  let padrao;
+  let parentesesNumero = '(';
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] < 0 || array[index] > 9){
-      padraoTelefone = true;
-      return padraoTelefone;
+    if (padraoTelefone[array[index]]) {
+      padraoTelefone[array[index]] += 1;
+      return Object.values(padraoTelefone).sort().reverse()[0];
+    } else {
+        padraoTelefone[array[index]] = 1;
     }
-    if (array.length > 11 || array.length < 11 || array.length === 0) {
-      diferenteDoPadrao = true;
-      return diferenteDoPadrao;
+    if (padraoTelefone[array] >= 3) {
+      padrao = false;
+      return padrao;
     }
+    if (array[index] < 0 || array[index] >9) {
+      padrao = true;
+      return padrao;
+    }
+    if (index === 2) {
+      parentesesNumero += ')';
+      parentesesNumero += array[index];
+      return parentesesNumero;
+    }
+    if (index === 7) {
+      parentesesNumero += '-';
+      parentesesNumero += array[index];
+      return parentesesNumero;
+    }
+    if (array.length === 11) {
+      let numeroExcedente = padrao[array];
+      let limiteRepete = padrao[array];
+      if (numeroExcedente && limiteRepete) {
+        return padrao(array);
+      }
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    return 'Array com tamanho incorreto.';
   }
 }
 
