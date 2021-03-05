82e4a8b3c39e5b1b35e32324393e90c89a1bb56e
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-05 13:17:25 -0300

    Arrumando requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -75,7 +75,7 @@ function highestCount(numero) {
 function catAndMouse(mouse, cat1, cat2) {
     if ((mouse * cat2) > cat1){
       return 'cat1'
-  } else if ((mouse * cat2) < cat1){
+  } else if ((mouse * cat1) < cat2){
       return 'cat2'
   } else {
       return 'os gatos trombam e o rato foge'
