e10782eb880e6c1c7c33f0188eba8cecf40e4310
Carlos <cemargato@gmail.com>
2021-03-05 14:01:19 -0300

    acertando exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,18 +1,18 @@
 // Desafio 10
 function techList(technology, name) {
   technology.sort();
+  if (technology.length === 0) { //conceito de array vazio lido no site https://www.freecodecamp.org/news/check-if-javascript-array-is-empty-or-not-with-length/ 
+    return 'Vazio!';
+  }
   let register = [] //analisei o raciocinio da colega Leticia Galvão - T10B na questao,  porque não estava registrando todos os looping vi que ao inves de objeto ela usava um array 
   for (let index in technology) {
-    if (technology === 0) { //conceito de array vazio lido no site https://www.freecodecamp.org/news/check-if-javascript-array-is-empty-or-not-with-length/ 
-      return 'Vazio!';
-    } else {
-      register.push({
-        tech: technology[index],
-        name: name
-      }
-      )
+    register.push({
+      tech: technology[index],
+      name: name
     }
+    )
   }
+
   return register;
 }
 
