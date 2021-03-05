bc4c68ce4e03ab22e5e43f255fe40b230c274d5f
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-05 16:24:27 -0300

    challenge 11 update 1.1

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,25 +12,27 @@ function techList(tech, name) {
 
 // Desafio 11
 function generatePhoneNumber(phone) {
-  let count = 0;
-  if (phone.length != 11) result = "Array com tamanho incorreto.";
+  let count = 0; let result = '';
+  if (phone.length != 11) result = 'Array com tamanho incorreto.';
   else result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
 
-  for (i in phone) {
-    if (phone[i] < 0 || phone[i] > 9) result = 'não é possível gerar um número de telefone com esses valores1';
-    for (j in phone) {
+  for (let i = 0; i < phone.length; i += 1) {
+    if (phone[i] < 0 || phone[i] > 9) {
+      result = 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let j = 0; j < phone.length; j += 1) {
       count = 0;
-      if (phone[j] == phone[i]) count += 1;
+      if (phone[j] === phone[i]) count += 1;
     }
   }
 
-  if (count >= 3) result = 'não é possível gerar um número de telefone com esses valores2';
+  if (count > 2) result = 'não é possível gerar um número de telefone com esses valores';
   return result;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+
 }
 
 // Desafio 13
