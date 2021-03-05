d09a75e2e7d955f8fed9a0945bcf49ac9cbcbffd
Camila <camilafabiabh9@gmail.com>
2021-03-04 21:56:32 -0300

    Enfim adicionando function 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+//(Referencia para saber metodo sort) https://www.w3schools.com/jsref/jsref_sort.asp
+function techList(name) {
+  let listaArray = name.sort();
+  let nome = 'Lucas';
+  let returnName = [];
+  if(listaArray.length > 1) {
+    for(let index = 0; index < listaArray.length; index += 1) {    
+      returnName.push({tech: listaArray[index], name:nome });
+      } 
+  } else {
+      returnName = 'Vazio!';
+    }
+  return returnName;
 }
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"]));
+
 
 // Desafio 11
 function generatePhoneNumber() {
