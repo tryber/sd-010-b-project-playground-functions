ed7a34cc9ccdbe422c905d49406e118ac9d7474c
Gabriel Ribeiro <grbeiro237@gmail.com>
2021-03-05 20:34:41 -0300

    parte 8 corrigida 8

--- src/challenges.js
+++ src/challenges.js
@@ -62,13 +62,13 @@ function fizzBuzz(array) {
   for (let i = 0; i < array.length; i += 1) {
     if (array[i] % 3 === 0 && array[i] % 5 === 0) {
       array[i] = 'fizzBuzz';
-    } else if (array[i] % 3 !== 0 && array[i] % 5 !== 0) {
-    array[i] = 'bug!';
     } else if (array[i] % 3 === 0) {
       array[i] = 'fizz';
     } else if (array[i] % 5 === 0) {
       array[i] = 'buzz';
-    }
+    } else {
+      array[i] = 'bug!';
+      } 
   } 
   return array;
 }
