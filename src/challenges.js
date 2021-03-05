3ce5c04f2f4f60289133ea3dea14f309e991f6bd
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-05 12:09:58 -0300

    Arrumado exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -83,7 +83,7 @@ function catAndMouse(mouse, cat1, cat2) {
 } 
 console.log(catAndMouse(0, 3, 2))
 console.log(catAndMouse(0, 6, 12))
-console.log(catAndMouse(0, 0, 0))
+console.log(catAndMouse(0, 6, 6))
 
 // Desafio 8
 function fizzBuzz(numeros) {
