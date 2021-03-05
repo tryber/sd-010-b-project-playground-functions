5fe3f06f942fdd19a9266fc9f34fef790f122500
Michelle <mls.srbek@gmail.com>
2021-03-05 15:47:43 -0300

    validArray function

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,6 +17,36 @@ function techList(technologies, name) {
 }
 
 // Desafio 11
+function validArray(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  for (let index = 0; index < array.lenght; index += 1) {
+    let count = 0;
+    for (let internIndex = 0; internIndex < array.lenght; internIndex += 1) {
+      if (array[index] === array[internIndex]) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+
+
+}
+
+
+
+
 function generatePhoneNumber() {
   // seu código aqui
 }
