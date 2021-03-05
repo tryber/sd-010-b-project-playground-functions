dd1b75e6b2f84d623175bf94d4d046d25b989c38
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-05 17:21:52 -0300

    challenge 13 complete

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,24 +13,19 @@ function techList(tech, name) {
 // Desafio 11
 function generatePhoneNumber(phone) {
   let result = '';
-
-  if (phone.length > 11 || phone.length < 11){
-      result = "Array com tamanho incorreto.";
+  if (phone.length > 11 || phone.length < 11) {
+    result = 'Array com tamanho incorreto.';
   }
   else {
     result = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
     for (let i = 0; i < phone.length; i += 1) {
       let count = 0;
         if (phone[i] > 9 || phone[i] < 0) {
-        result = "não é possível gerar um número de telefone com esses valores";
+        result = 'não é possível gerar um número de telefone com esses valores';
         }
         for (let j = 0; j < phone.length; j += 1) {
-          if (phone[i] == phone[j]) {
-          count += 1;
-        }
-        if (count > 2){
-          result = "não é possível gerar um número de telefone com esses valores";
-        }
+          if (phone[i] == phone[j]) count += 1;
+          if (count > 2) result = 'não é possível gerar um número de telefone com esses valores';
       }
     }
   }
@@ -41,7 +36,6 @@ function generatePhoneNumber(phone) {
 function triangleCheck(lineA, lineB, lineC) {
   let result = false; let line = [];
   line.push(Math.abs(lineA), Math.abs(lineB), Math.abs(lineC));
-
   if ((line[0] + line[1]) > line[2] && (line[0] - line[1]) < line[2]) {
     if ((line[0] + line[2]) > line[1] && (line[0] - line[2]) < line[1]) {
       if ((line[2] + line[1]) > line[0] && (line[2] - line[1]) < line[0]) {
@@ -53,9 +47,17 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(word) {
+  let number = word.replace(/[^0-9]/g,''); //pesquisei sobre isso em stackoverflow
+  let array = number.split(''); let result = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+  result += parseInt(array[i]);
 }
+return (result === 1 ? result+' copo de água' : result +' copos de água');
+}
+
+console.log(hydrate('1 cerveja'));
 
 module.exports = {
   generatePhoneNumber,
