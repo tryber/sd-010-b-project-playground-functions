7e8a9fa5cdca7ad121af07df4cc27ddeca9fe0b1
Carlos <cemargato@gmail.com>
2021-03-05 11:20:06 -0300

    trocando objeto por array no exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,72 +1,42 @@
 // Desafio 10
 function techList(technology, name) {
-  let register = {}
-  let final = {}
+  let register = [] //analisei o raciocinio da colega Leticia Galvão - T10B na questao, porque não estava registrando todos os looping vi que ao inves de objeto ela usava um array 
   for (let index in technology) {
     if (technology === 0) { //conceito de array vazio lido no site https://www.freecodecamp.org/news/check-if-javascript-array-is-empty-or-not-with-length/ 
       register = 'vazio'
     } else {
-      register.tech = technology[index];
-      register.name = name;
+      register.push({
+        tech: technology[index],
+        name: name
+      }
+      )
     }
-    return register;
   }
-
+  return register;
 }
-//   crie uma função que recebe um array de nomes de tecnologias que você quer aprender. Essa função deve receber também um segundo parâmetro chamado name com um nome.
-
-// Para cada tecnologia no array, crie um objeto com a seguinte estrutura:
-
-// {
-//   tech: "NomeTech",
-//   name: name
-// }
-// Estes objetos devem ser inseridos em uma nova lista em ordem crescente a partir do campo tech no objeto.
-
-// A saída da sua função deve ser uma lista de objetos ordenada pelo campo tech dos objetos com o formato acima.
-
-// Exemplo:
-
-// Entradas da função:
-
-// ["React", "Jest", "HTML", "CSS", "JavaScript"]
-// "Lucas"
-
-// // Saída:
-
-// [
-//   {
-//     tech: "CSS",
-//     name: "Lucas"
-//   },
-//   {
-//     tech: "HTML",
-//     name: "Lucas"
-//   },
-//   {
-//     tech: "JavaScript",
-//     name: "Lucas"
-//   },
-//   {
-//     tech: "Jest",
-//     name: "Lucas"
-//   },
-//   {
-//     tech: "React",
-//     name: "Lucas"
-//   }
-// ]
-// Caso o array venha vazio sua função deve retornar 'Vazio!'
-
-// O que será verificado:
-
-// Retorne uma lista de objetos ordenados quando é passada uma lista com 5 tecnologias deve
-
-// Retorne a mensagem de erro 'Vazio!' quando a lista tiver com 0 tecnologias
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+  //   // seu código aqui
+  //   Crie uma função chamada generatePhoneNumber que receba uma array com 11 números e retorne um número de telefone, respeitando parênteses, traços e espaços.
+
+  // Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], generatePhoneNumber deverá retornar (12) 34567-8901.
+
+  // Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho incorreto.".
+
+  // Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses valores".
+
+  // O que será verificado:
+
+  // Retorne a string "Array com tamanho incorreto." caso o array tenha o tamanho diferente de 11
+
+  // Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum dos números da array seja menor que 0
+
+  // Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número da array seja maior que 9
+
+  // Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número da array se repeti 3 vezes ou mais
+
+  // Retorne um número de telefone, respeitando parênteses, traços e espaços caso os números da array estejam de acordo com as restrições
 }
 
 // Desafio 12
