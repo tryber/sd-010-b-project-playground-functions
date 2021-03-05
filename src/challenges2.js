85735231a6881b0651e9b59c4e78d38eca15caee
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-03-04 22:45:18 -0300

    requsioto 11 correçoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,56 @@
+// Frameworks
+function phoneNumberValidator(arr) {
+  let maxNumber = Math.max(...arr);
+  let minNumber = Math.min(...arr);
+  let repeated = validatorRepeat(arr);
+
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (maxNumber > 9 || minNumber < 0 || repeated >= 3) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return false;
+}
+
+function validatorRepeat(arr) {
+  let repeated = 0;
+  let qtt = arr.length;
+  arr.forEach((e) => {
+    let cont = null;
+
+    for (let i = 0; i < qtt; i+=1) {
+      if (e === arr[i]) {
+        cont+=1;
+      }
+    }
+
+    if (cont > repeated) repeated = cont;
+  });
+
+  return repeated;
+}
+
+function phoneNumber(arr) {
+  let qtt = arr.length;
+  let phone = null;
+
+  phone = `(${arr[0]}${arr[1]}) `;
+
+  for (let i = 2; i <= 6; i+=1) {
+    phone += arr[i];
+  }
+
+  phone += '-';
+
+  for (let i = 7; i < qtt; i+=1) {
+    phone += arr[i];
+  }
+
+  return phone;
+}
+
 // Desafio 10
 function techList(array, paramterName) {
   let list = [];
@@ -35,59 +88,6 @@ function triangleCheck() {
 // Desafio 13
 function hydrate() {
   // seu código aqui
-}
-
-//Frameworks 
-function phoneNumberValidator(arr) {
-  let maxNumber = Math.max(...arr);
-  let minNumber = Math.min(...arr);
-  let repeated = validatorRepeat(arr);
-
-  if (arr.length !== 11) { 
-    return 'Array com tamanho incorreto.';
-  }
-
-  if (maxNumber > 9 || minNumber < 0 || repeated >= 3){
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
-  return false;
-}
-
-function validatorRepeat(arr) {
-  let repeated = 0;
-  let qtt = arr.length;
-  arr.forEach((e) => {
-    let cont = null;
-
-    for (let i = 0; i < qtt; i ++) {
-      if (e === arr[i]) {
-        cont ++;
-      }
-    }
-
-    if (cont > repeated) repeated = cont;
-  });
-
-  return repeated;
-}
-
-function phoneNumber(arr) {
-  let qtt = arr.length;
-  let phone = null;
-
-  phone = `(${arr[0]}${arr[1]}) `;
-
-  for (let i = 2; i <= 6; i ++) {
-    phone += arr[i];
-  }
-
-  phone += "-";
-
-  for (let i = 7; i < qtt; i ++){
-    phone += arr[i];
-  }
-
-  return phone;
 }
 
 module.exports = {
