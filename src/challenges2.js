dc96763fd065d41a969ddd7829cd04bc2902f558
João Victor Pistorio <joao.pistorio@gmail.com>
2021-03-05 22:08:17 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,26 @@
+function printArray(array) {
+  for (let i = 0; i < array.length; i += 1) { console.log(Object.values(array[i])); }
+}
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let list = {}; let ordened = ['']; let response = [];
+
+  if (array.length > 0) {
+    console.log(`[Requisito 10] - Array Original: ${array}`);
+    ordened = array.sort();
+    console.log(`[Requisito 10] - Array Ordenado: ${ordened}`);
+
+    for (let i = 0; i < ordened.length; i += 1) {
+      list = {};
+      list.tech = ordened[i]; list.name = name;
+      response[i] = list;
+    }
+
+    printArray(response);
+  } else { response = 'Vazio!'; console.log(`Resposta: ${response}`); }
+
+  return response;
 }
 
 // Desafio 11
@@ -17,6 +37,10 @@ function triangleCheck() {
 function hydrate() {
   // seu código aqui
 }
+
+let tech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+// let vazio = [];
+techList(tech, 'João');
 
 module.exports = {
   generatePhoneNumber,
