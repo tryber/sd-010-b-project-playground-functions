bcadad8457331a1543bc1e16c8947a24913f7bac
fredmartins <fredweb22@gmail.com>
2021-03-06 21:52:35 -0300

    adicionando desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -38,8 +38,16 @@ function highestCount(numbers) {
   }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Pos = Math.abs(mouse - cat1);
+  let cat2Pos = Math.abs(mouse - cat2);
+  if (cat1Pos < cat2Pos) {
+    return "cat1";
+  } else if (cat2Pos < cat1Pos){
+    return "cat2"
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
