6b34c00ebd9c4508aa6c3eadf24baa3d8dfcff9e
isTorres <ismaeltorres612@gmail.com>
2021-03-06 23:27:29 -0300

    lint problems

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,34 +4,31 @@ function techList() {
 }
 
 // Desafio 11
-function numTests(number) {
-  let result = [];
-  
-  for (let i in number) {
-    if (number.length !== 11) {
-      return "Array com tamanho incorreto.";
+// function numTests(number) {
+//   let result = [];
+//   for (let i in number) {
+//     if (number.length !== 11) {
+//       return "Array com tamanho incorreto.";
 
-    } else if (number[i] < 0) {
-      return "não é possível gerar um número de telefone com esses valores";
+//     } else if (number[i] < 0) {
+//       return "não é possível gerar um número de telefone com esses valores";
 
-    } else if (number[i] > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
+//     } else if (number[i] > 9) {
+//       return "não é possível gerar um número de telefone com esses valores";
 
-    } else {
-      result.push(number[i]);
-    }
-  }
-  return result
-}
-let cell = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-console.log(numTests(cell));
+//     } else {
+//       result.push(number[i]);
+//     }
+//   }
+//   return result
+// }
+// let cell = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+// console.log(numTests(cell));
 
-function generatePhoneNumber(number) {
-  let result = '';
-  
-
-  let phoneNumber = `(${}) ${} - ${}`;
-  return result
+function generatePhoneNumber() {
+  // let result = '';
+  // let phoneNumber = `(${}) ${} - ${}`;
+  // return result
 }
 
 // Desafio 12
