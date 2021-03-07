69e719ab9d3f846fce4b945ae2e8ace2c5e50ebd
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-07 10:57:41 -0300

    requisitos 1,2,3,4,5,7

--- src/challenges.js
+++ src/challenges.js
@@ -20,8 +20,11 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(stringName) {
-
+    return stringName[stringName.length-1] +", " +stringName[0];
 }
+let stringName = ['foguete', 'não', 'tem', 'ré']
+console.log(concatName(stringName));
+    
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -31,20 +34,42 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-// seu código aqui
-}
+function highestCount(values) {
+  let higher = 0;
+  let acres =0;
+  for ( let num in values){
+    if (values[num] > higher){
+      higher = values[num]
+   
+    }
+    }
 
+}
+console.log(highestCount([2,3,5,9]));
+  
 // Desafio 7
-function catAndMouse() {
-// seu código aqui
+function catAndMouse(mouse,cat1, cat2) {
+  let catnumber1 = Math.abs(mouse-cat1);
+  let catnumber2 = Math.abs(mouse -cat2);
+  if (catnumber1 < catnumber2){
+    return "cat1"
+  } else if (catnumber2 < catnumber1){
+    return "cat2"
+  }else{
+    return "Os gatos trombam e o rato foge"
+  }
 }
-
+console.log(catAndMouse(3 ,4, 4));
 // Desafio 8
-function fizzBuzz() {
-// seu código aqui
-}
-
+function fizzBuzz(array) {
+  for (let index in array){
+    if ( array[index] % 3 == 0){
+    }return "fizz" 
+    }
+   
+  }
+console.log(fizzBuzz([2,15,7,9,45]));
+  
 // Desafio 9
 function encode() {
 // seu código aqui
@@ -67,5 +92,6 @@ module.exports = {
 }
 console.log(compareTrue( true, false));
 console.log(calcArea(5, 2));
-console.log(splitSentence("go tybe"));
+console.log(splitSentence("go trybe"));
+
 console.log(footballPoints(0,0));
