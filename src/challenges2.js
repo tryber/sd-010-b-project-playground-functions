76c201cd90e3ac8dbe97681ba78be611828a7ad4
Carlos Vieira <appencigr@gmail.com>
2021-03-07 09:58:05 -0300

    Desafio 13 feito.

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,6 +16,34 @@ function techList(nameTechs, nameUser) {
 }
 
 // Desafio 11
+function repetTresVezes(number) {
+  for (let index = 0; index < number.length; index += 1) {
+    let count = [];
+    for (let index2 = 0; index2 < number.length; index2 += 1) {
+      if (number[index] === number[index2]) {
+        count.push('x');
+      }
+      if (count.length > 2) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+function isCheckConditions(number) {
+  let mesage = 'não é possível gerar um número de telefone com esses valores';
+  if (number.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } if (repetTresVezes(number) === true) {
+    return mesage;
+  }
+  for (let index = 0; index < number.length; index += 1) {
+    if (number[index] < 0 || number[index] > 9) {
+      return mesage;
+    }
+  }
+  return false;
+}
 function generatePhoneNumber(number) {
   // seu código aqui
   let conditionTrue = isCheckConditions(number);
@@ -31,37 +59,7 @@ function generatePhoneNumber(number) {
     }
     return numberPhone;
   }
-  let result = conditionTrue;
-  return result;
-}
-function isCheckConditions(number) {
-  let mesage = 'não é possível gerar um número de telefone com esses valores';
-  if (number.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  if (repetTresVezes(number) === true){
-    return mesage;
-  }
-  for (let index = 0; index < number.length; index += 1) {
-    if (number[index] < 0 || number[index] > 9) {
-      return mesage;
-    }
-  }
-  return false;
-}
-function repetTresVezes(number) {
-  for (let index = 0; index < number.length; index += 1) {
-    let count = [];
-    for (let index2 = 0; index2 < number.length; index2 += 1) {
-      if (number[index] === number[index2]) {
-        count.push('x');
-      }
-      if (count.length > 2) {
-        return true;
-      }
-    }
-  }
-  return false;
+  return conditionTrue;
 }
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -77,8 +75,51 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
+function returnNumberOfString(str){
+  let numEcontrado = [];
+  for (let index = 0; index < str.length; index += 1) {
+    switch (str[index]) {
+      case '0':
+      numEcontrado.push(str[index]);
+      break;
+      case '1':
+      numEcontrado.push(str[index]);
+      break;
+      case '2':
+      numEcontrado.push(str[index]);
+      break;
+      case '3':
+      numEcontrado.push(str[index]);
+      break;
+      case '4':
+      numEcontrado.push(str[index]);
+      break;
+      case '5':
+      numEcontrado.push(str[index]);
+      break;
+      case '6':
+      numEcontrado.push(str[index]);
+      break;
+      case '7':
+      numEcontrado.push(str[index]);
+      break;
+      case '8':
+      numEcontrado.push(str[index]);
+      break;
+      case '9':
+      numEcontrado.push(str[index]);
+    }
+  }
+  return numEcontrado;
+}
+function hydrate(str) {
   // seu código aqui
+  let numEcontrado = returnNumberOfString(str);
+  let result = 0;
+  for (let index = 0; index < numEcontrado.length; index += 1) {
+    result += Number(numEcontrado[index]);
+  }
+  return `${result} copos de água`;
 }
 
 module.exports = {
