f3fe8d8d4853778818a333120933a3a5c9a41bdf
Nikolas <nikolas.silva1990@gmail.com>
2021-03-08 12:52:38 -0300

    Desafio 9 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -94,11 +94,12 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(inputText) {
+  return inputText.replace(/a/g, "1").replace(/e/g, "2").replace(/i/g, "3").replace(/o/g, "4").replace(/u/g, "5")
 }
+
 function decode() {
-  // seu código aqui
+  return inputText.replace(/1/g, "a").replace(/2/g, "e").replace(/3/g, "i").replace(/4/g, "o").replace(/5/g, "u")
 }
 
 module.exports = {
