b7e4f44ddd61b36cb7d0d577b5037e3febe3cb35
Nikolas <nikolas.silva1990@gmail.com>
2021-03-08 15:23:32 -0300

    Desafio 10 revisado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,40 +1,46 @@
 // Desafio 10
-function techList(arrTech, name) {
-  /*  let box = [{ tech: "React", name: "Nikolas" }, {}]
-   box[0].name = "Paulo"
-   console.log(box[0]); */
 
-  arrTech = arrTech.sort();
+/*  let box = [{ tech: "React", name: "Nikolas" }, {}]
+ box[0].name = "Marcos"
+ console.log(box[0]); */
 
-  let arrNewTech = [];
+function techList(tech, name) {
 
-  for (let i = 0; i < arrTech.legth; i++) {
-    arrNewTech.push({
-      tech: arrTech[i],
-      name: name
-    })
+  if (tech.length !== 0) {
+    tech = tech.sort();
+
+    let arrTech = [];
+
+    for (let i = 0; i < tech.length; i++) {
+      arrTech.push({
+        tech: tech[i],
+        name: name
+      });
+    }
+    return arrTech;
   }
-  return arrNewTech;
-}
+  else {
+    return "Vazio!"
+  }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+  // Desafio 11
+  function generatePhoneNumber() {
+    // seu código aqui
+  }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+  // Desafio 12
+  function triangleCheck() {
+    // seu código aqui
+  }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+  // Desafio 13
+  function hydrate() {
+    // seu código aqui
+  }
 
-module.exports = {
-  generatePhoneNumber,
-  techList,
-  hydrate,
-  triangleCheck,
-};
+  module.exports = {
+    generatePhoneNumber,
+    techList,
+    hydrate,
+    triangleCheck,
+  };
