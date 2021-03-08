552b3c9f661a8ade4a3f9c06dcfdc4580a3b1cd5
Junior030 <juniorhenrique030@gmail.com>
2021-03-08 16:06:19 -0300

    reduzindo a complexidade do exercicio  9

--- src/challenges.js
+++ src/challenges.js
@@ -108,25 +108,23 @@ console.log(fizzBuzz([7, 9]));
 // https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
 // usando o metodo replace para remover a ocorerencia de vogais
 function encode(string) {
-  string = string.replace(/a/gi, "1");
-  string = string.replace(/e/gi, "2");
-  string = string.replace(/i/gi, "3");
-  string = string.replace(/o/gi, "4");
-  string = string.replace(/u/gi, "5");
+  string = string.replace(/a/gi, '1');
+  string = string.replace(/e/gi, '2');
+  string = string.replace(/i/gi, '3');
+  string = string.replace(/o/gi, '4');
+  string = string.replace(/u/gi, '5');
   return string;
 }
 function decode(string) {
-  string = string.replace(/1/gi, "a");
-  string = string.replace(/2/gi, "e");
-  string = string.replace(/3/gi, "i");
-  string = string.replace(/4/gi, "o");
-  string = string.replace(/5/gi, "u");
+  string = string.replace(/1/gi, 'a');
+  string = string.replace(/2/gi, 'e');
+  string = string.replace(/3/gi, 'i');
+  string = string.replace(/4/gi, 'o');
+  string = string.replace(/5/gi, 'u');
   return string;
 }
 console.log(encode('hi there!'));
 console.log(decode('h3 th2r2!'));
-
-
 
 module.exports = {
   calcArea,
