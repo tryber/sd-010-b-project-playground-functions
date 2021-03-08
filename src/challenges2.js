cc97bf5433f226d194d8bc18ee581821cee68887
Valeriandreoni <valeriandreoni@gmail.com>
2021-03-07 22:50:56 -0300

    segunda parte do desafio

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,30 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(array,name) {
+  let tecnologias = array.sort();
+  let nome = name;
+  let objeto = {};
+  let novoarray =[];
+  
+  if (tecnologias.length==0) {
+    return "Vazio"
+    
+  }
+  
+  for (let index in tecnologias) {
+    objeto={tech: tecnologias[index],
+      name: nome}
+      novoarray.push(objeto)
+  
+    }
+  
+   
+    return novoarray
+  } 
+  
+    console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
+  "Lucas"))
+  
+
 
 // Desafio 11
 function generatePhoneNumber() {
