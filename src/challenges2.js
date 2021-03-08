0aad0ca8be1b7dff7e20be33d9e56523f49fd8dc
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-07 21:51:18 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,19 @@
 // Desafio 10
-function techList() {
-// seu código aqui
+function techList(tech, name) {
+  if (tech.length < 1) {
+    return 'Vazio!';
+  }
+  let out = [];
+  tech.sort();
+  for (var tc in tech) {
+    let out_obj = {};
+    out_obj.tech = tech[tc];
+    out_obj.name = name;
+    out.push(out_obj);
+    
+  }
+  return out;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
 // seu código aqui
