d43c8c0f858fb33dd97c3accff6b05265ea99e1d
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-08 20:14:00 -0300

    Requisitos 5, 6 e 7

--- src/challenges.js
+++ src/challenges.js
@@ -36,18 +36,42 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  points = 3 * wins + ties;
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let highest;
+  let times;
+  for (let i = 0; i < array.length; i += 1) {
+    if ((array[i] > highest) || (highest == null)) {
+      highest = array[i];
+      times = 0;
+    }
+    if (array[i] == highest) {
+      times += 1;
+    }
+  }
+  return times;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(cat1 - mouse);
+  let dist2 = Math.abs(cat2 - mouse);
+  let result;
+  if (dist1 < dist2) {
+    result = 'cat1';
+  }
+  else if (dist2 < dist1) {
+    result = 'cat2';
+  }
+  else {
+    result = 'os gatos trombam e o rato foge';
+  }
+  return result;
 }
 
 // Desafio 8
