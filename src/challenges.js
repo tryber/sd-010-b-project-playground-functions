3764714e1139d0f64e72ed5565cf912a308157f7
Thiago Leite <leite.tjs@gmail.com>
2021-03-08 01:05:36 -0300

    some lint fixes

--- src/challenges.js
+++ src/challenges.js
@@ -11,22 +11,21 @@ function calcArea(base, altura) {
 }
 // Desafio 3
 function splitSentence(string) {
-  
   return string.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  let lastFirst = '';  
+  let lastFirst = '';
   lastFirst = array[array.length - 1] + ', '  + array[0];
-  return lastFirst;   
+  return lastFirst;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let pontos = 0;
   if (wins > 0 || ties > 0) {
-    return pontos = (wins * 3) + (ties * 1);
+     pontos = (wins * 3) + (ties * 1);
   }
   return pontos;
 }
@@ -34,26 +33,27 @@ function footballPoints(wins, ties) {
 function highestCount(numbers) {
   let highNumber = 0;
   let highNumberCount = 0;
-  for ( i = 0; i < numbers.length;  i += 1) {
+  for ( let i = 0; i < numbers.length; i += 1) {
     if (numbers[i] > highNumber) {
       highNumber = numbers[i];
     }
   }
-  for ( let j = 0; j < numbers.length; j += 1 ) {
-    if (numbers[j] === highNumber) {
+  for ( let j = 0; j < numbers.length; j += 1) {
+    if (numbers[j] === highNumber){
      highNumberCount = (highNumberCount + 1);
-     }
+    }
   }
   return highNumberCount  
 }
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   //usei o math.abs pra pegar os valores absolutos das diferenças, referência > https://javascript.plainenglish.io/javascript-algorithm-cats-and-a-mouse-fd60fb1811ba
+  let result = '';
   if ( (Math.abs(cat1 - mouse)) < (Math.abs(cat2 - mouse)) === true) {
     result = 'cat1';
     return result;
-  } else if ( (Math.abs(cat2 - mouse)) < (Math.abs(cat1 - mouse)) === true ) {
-    result = 'cat2'
+  } else if ( (Math.abs(cat2 - mouse)) < (Math.abs(cat1 - mouse)) === true) {
+    result = 'cat2';
     return result;
   } else if ( Math.abs(cat1 - mouse) === (cat2 - mouse) ) {
     result = 'os gatos trombam e o rato foge';
@@ -63,7 +63,7 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbers) {
      
-  frase = [];
+  let frase = [];
   for ( let i = 0; i < numbers.length; i += 1) {
     if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
       frase[i] = 'fizzBuzz';
@@ -83,6 +83,7 @@ function fizzBuzz(numbers) {
 // Desafio 9
 function encode(text) {
   //usei o replace() nativa para fazer as respectivas mudanças nas strings, referência do replace() > https://www.w3schools.com/jsref/jsref_replace.asp
+
   text = text.replace(/a/g, '1');
   text = text.replace(/e/g, '2');
   text = text.replace(/i/g, '3');
@@ -93,6 +94,7 @@ function encode(text) {
 
 function decode(text) {
   //usei o replace() para fazer as respectivas mudanças nas strings, referência do replace() > https://www.w3schools.com/jsref/jsref_replace.asp
+
   text = text.replace(/1/g, 'a');
   text = text.replace(/2/g, 'e');
   text = text.replace(/3/g, 'i');
@@ -100,8 +102,6 @@ function decode(text) {
   text = text.replace(/5/g, 'u');
   return text;
 }
-
-
 
 module.exports = {
   calcArea,
