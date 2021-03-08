9db73c89c8f9209711b42a35d44ccb4c76a66982
Nikolas <nikolas.silva1990@gmail.com>
2021-03-08 13:01:34 -0300

    Desafios 7 e 9 Revisados

--- src/challenges.js
+++ src/challenges.js
@@ -48,8 +48,8 @@ function highestCount(arrayNumbers) {
   return maxNumberRepeat
 }
 
-// Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
+// Desafio 7 - OtherWay
+/* function catAndMouse(mouse, cat1, cat2) {
   let diffCat1 = Math.abs(cat1 - mouse)
   let diffCat2 = Math.abs(cat2 - mouse)
 
@@ -62,6 +62,31 @@ function catAndMouse(mouse, cat1, cat2) {
   else {
     return "os gatos trombam e o rato foge"
   }
+} */
+
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+  let mouseCat1;
+  let mouseCat2;
+
+  if (mouse > cat1) {
+    mouseCat1 = mouse - cat1;
+  } else {
+    mouseCat1 = cat1 - mouse;
+    }
+
+  if (mouse > cat2) {
+    mouseCat2 = mouse - cat2;
+  } else {
+    mouseCat2 = cat2 - mouse;
+    }
+
+  if (mouseCat1 < mouseCat2) {
+    return 'Cat1';
+  } else if (mouseCat1 > mouseCat2) {
+    return 'Cat2';
+  }
+  return 'Os gatos trombam e o rato foge.';
 }
 
 // Desafio 8
@@ -98,7 +123,7 @@ function encode(inputText) {
   return inputText.replace(/a/g, "1").replace(/e/g, "2").replace(/i/g, "3").replace(/o/g, "4").replace(/u/g, "5")
 }
 
-function decode() {
+function decode(inputText) {
   return inputText.replace(/1/g, "a").replace(/2/g, "e").replace(/3/g, "i").replace(/4/g, "o").replace(/5/g, "u")
 }
 
