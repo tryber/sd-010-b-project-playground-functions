bd95c14f21d19e5663b0dadf7fe20a5b539645b8
edersonrodara <edersonrodara@gmail.com>
2021-03-08 15:32:43 -0300

    tentativa3 - questão 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,17 @@
 // Desafio 10
 
-function techList(array) {
+function techList(array, name) {
   let obj = {};
   for (let index = 0; index < array.length; index += 1) {    
     obj.tech = array[index];
-    obj.name = name
-    if (array[index] === '') {
+    obj.name = name;
+    if (array[index] === 0) {
       console.log('Vazio!');
     }
   }
   return obj;
 }
-console.log (techList(["React", "Jest", "HTML", "CSS", "JavaScript"]));
+console.log (techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Ederson'));
 
 // Desafio 11
 function generatePhoneNumber() {
