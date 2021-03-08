fed198d8967d5d24d41eac4abacb9283712aa541
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-08 19:56:10 -0300

    finalizando exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
-// Desafio 10
-function techList() {
-    // seu código aqui
-}
+// Desafio 10 // Neste desafio contei com a ajuda dos meus colegas Carlos Vieira e Wellington Passo, que auxiliaram a enxergar algumas fragilidades do código.
+function techList(arrayTech,name) {
+  let finalList = [];
+  let object = {
+    arrayTech,
+    name
+  };
+  if (object.arrayTech.length === 0 || object.name === "") {
+    return 'Vazio!';
+  }
+    let orderlyTechList = object.arrayTech.sort();
+      for (let key in orderlyTechList) {
+          finalList.push({
+          tech: orderlyTechList[key],
+          name: name
+          });
+    }
+  return finalList;
+  }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+console.log(techList([], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
