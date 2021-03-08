42848e0bd38c104bc38bab7fc7d25cc1d0b9d61b
Breno Rodrigues <brenorm@gmail.com>
2021-03-08 12:39:10 -0300

    Implementaçao Desafio 11: generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,7 +8,7 @@ function techList(techArray, name) {
 }
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Breno'));
 
-// Desafio 10 - Função extra1
+// Desafio 10 - Função extra
 function objectArrayGenerator(arraylist, name) {
   let objectArray = [];
   for (let index = 0; index < arraylist.length; index += 1) {
@@ -22,9 +22,33 @@ function objectArrayGenerator(arraylist, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumberArray) {
+  if (phoneNumberArray.length !== 11) {
+    return "Array com tamanho incorreto.";
+  }
+
+  for (let index = 0; index < phoneNumberArray.length; index += 1) {
+    if ((phoneNumberArray[index] < 0) || (phoneNumberArray[index] > 9)) {
+      return "não é possível gerar um número de telefone com esses valores2";
+    }
+  }
+
+  for (let index1 = 0; index1 < phoneNumberArray.length; index1 += 1) {
+    let repeatCounter = 0;
+    for (let index2 = 1; index2 < phoneNumberArray.length; index2 += 1){
+      if (phoneNumberArray[index1] === phoneNumberArray[index2]) {
+        repeatCounter += 1;
+      }
+
+      if (repeatCounter === 3) {
+        return "não é possível gerar um número de telefone com esses valores3";
+      }
+    }
+  }
+
+  return (`(${phoneNumberArray[0]}${phoneNumberArray[1]}) ${phoneNumberArray[2]}${phoneNumberArray[3]}${phoneNumberArray[4]}${phoneNumberArray[5]}${phoneNumberArray[6]}-${phoneNumberArray[7]}${phoneNumberArray[8]}${phoneNumberArray[9]}${phoneNumberArray[10]}`);
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
