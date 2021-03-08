ece8e36918783e763cb837276b11132735e42c1b
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-03-07 23:34:49 -0300

    resolvendo exercicio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -46,9 +46,14 @@ function generatePhoneNumber(arrayTelefone) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA,lineB,lineC) {
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) || lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC) || 
+  lineC < (lineB + lineA) && lineC > Math.abs(lineB - lineA)) {
+    return true;
+  }
+  return false;
+  }
+  
 
 // Desafio 13
 function hydrate() {
