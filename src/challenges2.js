b8904813ab25d3466bcccea075c8865bee4d947e
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-03-07 21:57:48 -0300

    Tentando resolver o exercicio 11 do projeto

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,9 +17,37 @@ function techList(arrayTecnologia,name) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayTelefone) {
+  let numeroArray =""
+  let duplicado = 0;
+    
+  for (let cont = 0; cont < arrayTelefone.length; cont += 1){
+
+    if (arrayTelefone.length != 11){
+      return  "Array com tamanho incorreto";
+    }
+   
+    if (arrayTelefone[cont] < 0 || arrayTelefone[cont] > 9){
+      return "não é possivel gerar um número de telefone com esses valores";
+    }
+
+    if (arrayTelefone == arrayTelefone[cont]){
+      duplicado += 1;
+    }
+
+
+    for(cont = 2; cont <arrayTelefone.length; cont += 1){
+    numeroArray = numeroArray + arrayTelefone[cont].toString();
+  }
 }
+  if (duplicado >2){
+    return "não é possivel gerar um número de telefone com esses valores";
+  }
+  let numeroTelefone = "(" + arrayTelefone[0].toString() + arrayTelefone[1].toString() + ") " + numeroArray;
+  
+  return numeroTelefone;
+}
+
 
 // Desafio 12
 function triangleCheck() {
