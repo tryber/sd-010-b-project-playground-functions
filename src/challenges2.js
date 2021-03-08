e49ceb25ca0d8305a6b44fee448dedabc05e06f0
riquee <strikedv@hotmail.com>
2021-03-08 18:51:47 -0300

    Testando saidas triangle

--- src/challenges2.js
+++ src/challenges2.js
@@ -36,8 +36,8 @@ function generatePhoneNumber(array) {
 }
 // console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  return 'test'
 }
 
 // REFERÊNCIAS: https://stackoverflow.com/ , https://developer.mozilla.org/
