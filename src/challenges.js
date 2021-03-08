e746686adc664a57252e065402845716435c66ba
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-08 18:16:21 -0300

    Mais correções

--- src/challenges.js
+++ src/challenges.js
@@ -11,19 +11,19 @@ function compareTrue(var1, var2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return(base * height / 2);
+  return ((base * height) / 2);
 }
 
 // Desafio 3
 function splitSentence(text) {
-  let word = [""];
+  let word = [''];
   let wordCount = 0;
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] != " ") {
-      word[wordCount] = word[wordCount] + text[i];
+  for (let i = 0; i < text.length; i += 1) {
+    if (text[i] !== ' ') {
+      word[wordCount] += text[i];
     } else {
-      word.push("");
-      wordCount++;
+      word.push('');
+      wordCount += 1;
     }
   }
   return word;
@@ -31,7 +31,7 @@ function splitSentence(text) {
 
 // Desafio 4
 function concatName(array) {
-  let string = array[array.length-1] + ", " + array[0];
+  let string = array[array.length - 1] + ', ' + array[0];
   return string;
 }
 
