bf4b22e2cb026c3490079fc6bee693f853e0a267
riquee <strikedv@hotmail.com>
2021-03-07 22:00:59 -0300

    Corrigindo return

--- src/challenges.js
+++ src/challenges.js
@@ -57,7 +57,7 @@ let check = (number) => {
   if (number % 3 === 0 && number % 5 === 0) return 'fizzBuzz';
   if (number % 3 === 0) return 'fizz';
   if (number % 5 === 0) return 'buzz';
-  return 'fizz';
+  return 'bug!';
 };
 function fizzBuzz(number) {
   let newArray = [];
