f6d729ec59a187f8026a69622762b06c01adf4b0
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-07 22:47:14 -0300

    correçao da funçao techList(), adicionado a verificaçao de array vazio e returnando a array em ordem afabetica (item10)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,15 +1,24 @@
 // Desafio 10
 
 function techList(listaTech, listaName) {
-  // seu código aqui
-  let arr = [];
-  let newArry = [];
+  if (listaTech.length === 0) { return 'Vazio!'; }
+  let arr = [], newArry = [];
 
-  for (let element in listaTech) {
-    arr.push(['tech', listaTech[element]], ['name', listaName]);
-    newArry.push(obj = Object.fromEntries(arr));
+  function converteArrayEmObj() {
+    for (let element in listaTech) {
+      arr.push(['tech', listaTech[element]], ['name', listaName]);
+      newArry.push(obj = Object.fromEntries(arr));
+    }
+    return newArry;
   }
-  return newArry;
+  let arrayConvertido = converteArrayEmObj();
+  // código retirado do forum https://pt.stackoverflow.com/questions/46600/como-ordenar-uma-array-de-objetos-com-array-sort
+  arrayConvertido.sort(function (a, b) { // função criada dentro do parâmetro .sort para fazer a comparação dos valores na propriedade "tech" e colocar em ordem alfabética
+    if (a.tech < b.tech) { return -1; }
+    if (a.tech > b.tech) { return 1; }
+    return 0;
+  });
+  return arrayConvertido;
 }
 
 // Desafio 11
