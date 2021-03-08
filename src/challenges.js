f0ee8a49a45c70c067ff9d379dbd1d7955676fbc
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-07 23:30:05 -0300

    REFACTOR: Refactoring the challenges.js file for Linter errors

--- src/challenges.js
+++ src/challenges.js
@@ -44,7 +44,7 @@ function footballPoints(wins, ties) {
 // console.log(highestCount(numbers));
 function getBigger(arrayNumbers) {
   let bigger = 0;
-  for (let i = 0; i < arrayNumbers.length; i++) {
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
     if (arrayNumbers[i] >= bigger) {
       bigger = arrayNumbers[i];
     }
@@ -56,7 +56,7 @@ function manyTimes(arrayNumbers) {
   let times = 0;
   for (let i = 0; i < arrayNumbers.length; i += 1) {
     if (getBigger(arrayNumbers) === arrayNumbers[i]) {
-      times++;
+      times += 1;
     }
   }
   return times;
@@ -70,83 +70,89 @@ function highestCount(arrayNumbers) {
 // let m = 5, c1 = 2, c2 = 3;
 // console.log(catAndMouse(m, c1, c2));
 function catAndMouse(mouse, cat1, cat2) {
-  let cat1Distance, cat2Distance;
-  if (mouse > cat1) {
-    cat1Distance = mouse - cat1;
-  } else {
-    cat1Distance = cat1 - mouse;
-  }
-  if (mouse > cat2) {
-    cat2Distance = mouse - cat2;
-  } else {
-    cat2Distance = cat2 - mouse;
-  }
+  let cat1Distance;
+  let cat2Distance;
+  let winnerCat;
 
-  if (cat1Distance < cat2Distance) {
-    return "cat1";
-  } else if (cat2Distance < cat1Distance) {
-    return "cat2";
+  cat1Distance = cat1 - mouse;
+  cat2Distance = cat2 - mouse;
+
+  if (Math.abs(cat1Distance) < Math.abs(cat2Distance)) {
+    winnerCat = 'cat1';
+  } else if (Math.abs(cat2Distance) < Math.abs(cat1Distance)) {
+    winnerCat = 'cat2';
   } else {
-    return "os gatos trombam e o rato foge";
+    winnerCat = 'os gatos trombam e o rato foge';
   }
+  return winnerCat;
 }
 
 // Desafio 8
-// let array = [9, 25];
+// let array = [15, 9, 5, 4];
 // console.log(fizzBuzz(array));
+function mod3(number) {
+  if ((number % 3) === 0) {
+    return true;
+  }
+}
+
+function mod5(number) {
+  if ((number % 5) === 0) {
+    return true;
+  }
+}
+
+function verifyEachN(number) {
+  if (mod3(number) && mod5(number)) {
+    number = 'fizzBuzz';
+  } else if (mod3(number)) {
+    number = 'fizz';
+  } else if (mod5(number)) {
+    number = 'buzz';
+  } else {
+    number = 'bug!';
+  }
+  return number;
+}
+
 function fizzBuzz(numbers) {
-  for (let i = 0; i < numbers.length; i++) {
-    if (((numbers[i] % 3) === 0) && ((numbers[i] % 5) === 0)) {
-      numbers[i] = "fizzBuzz";
-    } else if ((numbers[i] % 3) === 0) {
-      numbers[i] = "fizz";
-    } else if ((numbers[i] % 5) === 0) {
-      numbers[i] = "buzz";
-    } else {
-      numbers[i] = "bug!"
-    }
+  for (let i = 0; i < numbers.length; i += 1) {
+    numbers[i] = verifyEachN(numbers[i]);
+    // if (mod3(numbers[i]) && mod5(numbers[i])) {
+    //   numbers[i] = 'fizzBuzz';
+    // } else if (mod3(numbers[i])) {
+    //   numbers[i] = 'fizz';
+    // } else if (mod5(numbers[i])) {
+    //   numbers[i] = 'buzz';
+    // } else {
+    //   numbers[i] = 'bug!';
+    // }
   }
   return numbers;
 }
 
 // Desafio 9
 // I found the replace() function at https://www.w3schools.com/jsref/jsref_replace.asp
-// let code = 'hi, there!';
-// console.log(encode(code));
+// let code = '1 cachaça, 5 cervejas e 1 copo de vinho';
 function encode(string) {
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === 'a') {
-      string = string.replace(string[i], 1);
-    } else if (string[i] === 'e') {
-      string = string.replace(string[i], 2);
-    } else if (string[i] === 'i') {
-      string = string.replace(string[i], 3);
-    } else if (string[i] === 'o') {
-      string = string.replace(string[i], 4);
-    } else if (string[i] === 'u') {
-      string = string.replace(string[i], 5);
-    }
-  }
+  string = string.replace(/a/g, 1);
+  string = string.replace(/e/g, 2);
+  string = string.replace(/i/g, 3);
+  string = string.replace(/o/g, 4);
+  string = string.replace(/u/g, 5);
   return string;
 }
-// code = 'h3, th2r2!';
-// console.log(decode(code));
+// console.log(encode(code));
+// code = '1 cachaça, 5 cervejas e 1 copo de vinho';
 function decode(string) {
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === '1') {
-      string = string.replace(string[i], 'a');
-    } else if (string[i] === '2') {
-      string = string.replace(string[i], 'e');
-    } else if (string[i] === '3') {
-      string = string.replace(string[i], 'i');
-    } else if (string[i] === '4') {
-      string = string.replace(string[i], 'o');
-    } else if (string[i] === '5') {
-      string = string.replace(string[i], 'u');
-    }
-  }
+  string = string.replace(/1/g, 'a');
+  string = string.replace(/2/g, 'e');
+  string = string.replace(/3/g, 'i');
+  string = string.replace(/4/g, 'o');
+  string = string.replace(/5/g, 'u');
   return string;
 }
+// console.log(decode(code));
 
 module.exports = {
   calcArea,
