50f1e8d090926be09cc8aec8010346cc660a46f1
Valeriandreoni <valeriandreoni@gmail.com>
2021-03-07 23:18:18 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,21 +3,16 @@ function techList(array,name) {
   let tecnologias = array.sort();
   let nome = name;
   let objeto = {};
-  let novoarray =[];
+  let novoarray = [];
   
-  if (tecnologias.length===0) {
-    return "Vazio"
-    
-  }
-  
+  if (tecnologias.length === 0) {
+    return "Vazio!";
+     }
   for (let index in tecnologias) {
-    objeto={tech: tecnologias[index],
+    objet o= {tech: tecnologias[index],
       name: nome}
       novoarray.push(objeto)
-  
     }
-  
-   
     return novoarray
   }console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
   "Lucas"))
