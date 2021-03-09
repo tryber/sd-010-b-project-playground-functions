5b1cba06500972012f495fb8a16311022bb29f5d
fredmartins <fredweb22@gmail.com>
2021-03-09 10:56:59 -0300

    tentativa desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,8 +4,17 @@ let techList = () => {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(argArray, names) {
+  let result = [];
+  argArray.sort();
+  for (let argName of argArray) {
+    let object = {
+      tech: argName,
+      names: `${names}`,
+    };
+    result.push(object);
+  }
+  return result;
 }
 
 // Desafio 12
@@ -14,9 +23,9 @@ function generatePhoneNumber() {
 // Por exemplo, os números 3, 4 e 5 podem formar um triângulo porque 3 4 > 5, 4 5 > 3 e 5 3 > 4.
 // Em contraste, os números 1, 2 e 5 não podem formar um triângulo porque 1 2 < 5.
 function triangleCheck(sideA, sideB, sideC) {
-  let checkA = sideA < sideB + sideC && sideA > Math.abs(sideB - sideC);
-  let checkB = sideB < sideA + sideC && sideB > Math.abs(sideA - sideC);
-  let checkC = sideC < sideB + sideA  && sideC > Math.abs(sideB - sideA);
+  let checkA = sideA < sideB + sideC && sideA; 
+  let checkB = sideB < sideA + sideC && sideB;
+  let checkC = sideC < sideB + sideA && sideC;
     return checkA && checkB && checkC;
 }
 
