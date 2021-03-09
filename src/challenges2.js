46d86e0734ad2287742def3f00c728d866f8c5e0
Valeriandreoni <valeriandreoni@gmail.com>
2021-03-08 22:01:52 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,10 +18,54 @@ function techList(array,name) {
   "Lucas"))
   
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array){
+  let contador = 0;
+  let contador2 = 0;
+  let mascara = "(**)****-****";
+  mascara = mascara.split("");
+  let intContador = 0;
+  let aNumero=array;
+  
+  if (aNumero.length !== 11){
+      return "Array com tamanho incorreto."
+  };
+  
+  for (let index in aNumero){
+    numero=aNumero[index];
+    for (index2 in aNumero){
+      if (aNumero[index] == aNumero[index2]) {
+        intContador++
+      }
+    }
+    if (intContador >= 3|| numero < 0|| numero > 9){
+      return "não é possível gerar um número de telefone com esses valores"
+    }else {
+      intContador = 0
+    }
+    
+    if (mascara[contador2] == "*"){
+      mascara[contador2] = aNumero[contador]
+    } else {
+        contador--
+    }
+    contador++;
+    contador2++;
+    if (index > 8){
+      mascara[contador2+1] = aNumero[contador+1]
+    }
+    if (index > 9){
+      mascara[contador2+2] = aNumero[contador+2]
+    }
+  }
+  mascara = mascara.join("");
+  return mascara;
 }
 
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, -9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7,9, 0, 1]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
