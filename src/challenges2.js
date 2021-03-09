785c9f164466ba628cc786431c73eeabb1b193af
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-08 23:10:52 -0300

    Executado requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,14 +15,22 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
      
 // Desafio 11
 function generatePhoneNumber(phoneNumber) {
-  let repeatedNumbers = [];
+  let numbers = [phoneNumber];
+  let repeatedNumbers = []
 
  for (let key = 0; key < phoneNumber.length; key += 1){ 
+   for (let key2 = 1; key2 <= 9; key2 +=1){
+  
+     }
+   }
    if (phoneNumber.length !== 11){
      return "Array com tamanho incorreto"
-   } else if (phoneNumber[key] < 0 || phoneNumber[key] > 9){
+   } else if (phoneNumber[key] < 0) { 
      return "não é possível gerar um número de telefone com esses valores"
-   } else {
+   } else if (phoneNumber[key] > 9){
+     return "não é possível gerar um número de telefone com esses valores"
+   }
+    else {
      return "(" + phoneNumber[0] + phoneNumber[1] + ")" + phoneNumber[2] + phoneNumber[3] + phoneNumber[4] + phoneNumber[5] + phoneNumber[6] + "-" + phoneNumber[7] + phoneNumber[8] + phoneNumber[9] + phoneNumber[10]
    }
   }
@@ -30,7 +38,7 @@ function generatePhoneNumber(phoneNumber) {
 console.log(generatePhoneNumber([1, 2, 3, 4, 7, 6, 5, 8, 4, 9]))
 console.log(generatePhoneNumber([1, 2, 3, 4, 7, 6, 5, 8, -4, 9, 2]))
 console.log(generatePhoneNumber([1, 2, 3, 4, 10, 6, 5, 8, 4, 9, 2]))
-console.log(generatePhoneNumber([1, 2, 3, 4, 7, 2, 5, 8, 4, 9, 2]))
+console.log(generatePhoneNumber([1, 2, 3, 4, 7, 2, 5, 8, 6, 9, 2]))
 console.log(generatePhoneNumber([1, 2, 3, 4, 7, 6, 5, 8, 4, 9, 2]))
 
 // Desafio 12
@@ -50,9 +58,12 @@ console.log(triangleCheck(10, 20, 8))
 console.log(triangleCheck(10, 14, 8))
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let quantidade = parseInt(string)
+  return quantidade + " " + "copos de agua"
 }
+console.log(hydrate("2 copos de cerveja"))
+console.log(hydrate("7 copos de uisque"))
 
 module.exports = {
   generatePhoneNumber,
