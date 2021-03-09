41e6a7421f8248793fd7c0e1eaa6560886724e67
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-09 12:30:33 -0400

    Requisito 12, tentativa 2

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,46 +4,12 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber(telNumbers) {
-  let menor = false
-	let maior = false
-	let maisQueTres = false
-
-	for (let index in telNumbers) {
-		if (telNumbers[index] < 0) {
-			menor = true
-		} else if (telNumbers[index] > 9) {
-			maior = true
-		}
-	}
-
-	if (contadorVezes(telNumbers) == false) {
-		maisQueTres = true
-	}
-
-	if (telNumbers.length != 11) {
-
-		return 'Array com tamanho incorreto'
-
-	} else if (maior || menor || maisQueTres) {
-
-		return "não é possível gerar um número de telefone com esses valores"
-	} else {
-		stringNumber = ''
-
-  	telNumbers.unshift('(')
-  	telNumbers.splice(3,0,')')
-    
-    stringNumber += telNumbers[index]
-	}
-
-  return stringNumber
+function generatePhoneNumber() {
+  
 }
-
-
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC
-  ) {
+function triangleCheck(lineA, lineB, lineC) {
+  
   if (
 		Math.abs(lineB+lineC) < lineA < lineB+lineC &&
 		Math.abs(lineA+lineC) < lineB < lineA+lineC &&
