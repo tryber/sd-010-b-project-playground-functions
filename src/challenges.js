0ae87f9fa1668f6f239801e428fa064e6cdf2c42
Wadson <wadson.dev92@gmail.com>
2021-03-09 14:02:31 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -17,8 +17,8 @@ function calcArea(base, height) {
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let res = frase.split(" ")
 }
 
 // Desafio 4
