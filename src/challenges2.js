685deb2b158501e2d6fd82e90f1f33358a6d467d
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-09 00:32:23 -0300

    Refatora desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,25 +18,6 @@ return guardaArrayFinal;
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Alexandre"))
 console.log(techList([], "Alexandre"))
 
-function techList(tech, name) {
-  if (tech.length != 0){
-    let techOrdem = tech.sort();
-    let objetosArray = [];
-    for (index = 0; index < techOrdem.length; index ++) {
-      let objeto = {
-        tech: techOrdem[index],
-        name: name,
-      }
-      objetosArray.push(objeto);
-    }
-    return objetosArray;
-  } else {
-    return "Vazio!";
-  }
-}
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Alexandre"))
-
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
