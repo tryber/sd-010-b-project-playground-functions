96593c2a0027ac885b07fae23c15df50a7399437
Thiago Kachinsky <tjk091@gmail.com>
2021-03-12 21:53:38 -0300

    Numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,10 +13,69 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Thiago'));
 
 // Desafio 11
 function generatePhoneNumber(number) {
+  let num = number
+
+  if (number.length !== 11) {
+    return `Array com tamanho incorreto`;
+  } 
+
+  for (let i = 0; i < number.length; i += 1) {
+    if (number[i] > 9 || number[i] < 0) {
+      return `não é possível gerar um número de telefone com esses valores`
+    }
+  }
+
+  let qtdElementos1 = 0
+  let qtdElementos2 = 0
+  let qtdElementos3 = 0
+  let qtdElementos4 = 0
+  let qtdElementos5 = 0
+  let qtdElementos6 = 0
+  let qtdElementos7 = 0
+  let qtdElementos8 = 0
+  let qtdElementos9 = 0
+  let qtdElementos0 = 0
+
+  num.forEach((element) => {
+    if (element === 1 ) {
+      return qtdElementos1++
+    }
+    if (element === 2 ) {
+      return qtdElementos2++
+    }
+    if (element === 3 ) {
+      return qtdElementos3++
+    }
+    if (element === 4 ) {
+      return qtdElementos4++
+    }
+    if (element === 5 ) {
+      return qtdElementos5++
+    }
+    if (element === 6 ) {
+      return qtdElementos6++
+    }
+    if (element === 7 ) {
+      return qtdElementos7++
+    }
+    if (element === 8 ) {
+      return qtdElementos8++
+    }
+    if (element === 9 ) {
+      return qtdElementos9++
+    }
+    if (element === 0 ) {
+      return qtdElementos0++
+    }
+  });
+
+  if (qtdElementos1 >= 3 || qtdElementos2 >= 3 || qtdElementos3 >= 3 || qtdElementos4 >= 3 || qtdElementos5 >= 3 || qtdElementos6 >= 3 || qtdElementos7 >= 3 ||qtdElementos8 >= 3 || qtdElementos9 >= 3 || qtdElementos0 >= 3 ) {
+    return `não é possível gerar um número de telefone com esses valores`
+  }
   
 }
 
-console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
+console.log(generatePhoneNumber([0, 2, 3, 4, 0, 2, 7, 4, 4, 9, 4]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
