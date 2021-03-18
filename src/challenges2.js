48df707c120760217ab715aa5e2770fe2b6687a3
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-18 14:39:32 -0400

    Requisito 10, implementada função de lista de tecnologias.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,28 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techno, name) {
+  let newArray = [];
+  techno.sort();
+
+  if (techno.length === 0) {
+    return "Vazio!";
+  } else {
+    for (let index = 0; index < techno.length; index += 1) {
+      let arrayAux = [];
+      let index0 = "tech: \""+techno[index]+"\"";
+      let index1 = "name: \""+name+"\"";
+      arrayAux.push(index0);
+      arrayAux.push(index1);
+      newArray.push(arrayAux);
+    }
+
+    return newArray;
+  }
 }
 
+techList();
 // Desafio 11
 function generatePhoneNumber() {
-  
+
 }
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
