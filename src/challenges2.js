244a3aeac0c07b90ee1163c179eb8d23655560f4
Camila <camilafabiabh9@gmail.com>
2021-03-18 11:30:03 -0300

    Adicionando nova skill

--- src/challenges2.js
+++ src/challenges2.js
@@ -38,28 +38,28 @@ function phoneRepeater(array) {
   let number = 0;
   let tamanho = [];
   for (let index = 0; index < array.length; index += 1) {
-    for( let index2 = 0; index < array.length; index2 += 1) {
-    if(array[index] === array[index2]){
-    number += 1;    
+    for ( let index2 = 0; index < array.length; index2 += 1) {
+    if (array[index] === array[index2]) {
+        number += 1;
+      }
+    }
+    tamanho.push(number);
+    number = 0;
   }
-}
-  tamanho.push(number);
-  number = 0;
-}
-return tamanho;
+  return tamanho;
 }
 function numberPhoneRepete (array) {
   let repete = true;
   let tamanho = phoneRepeater(array);
   for (let index = 0; index < tamanho.length; index += 1) {
-    if( tamanho[index] > 2){
+    if ( tamanho[index] > 2) {
       repete = false;
-    }    
+    }
   }
   return repete;
 }
 function generatePhoneNumber(array) {
-  if(PhoneNumberTamanho(array) === false) {
+  if (PhoneNumberTamanho(array) === false) {
     return 'Array com tamanho incorreto.';
   }
   if(phoneValues(array) === false || numberPhoneRepete(array) === false) {
@@ -67,7 +67,7 @@ function generatePhoneNumber(array) {
   }
   return'(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}';
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
