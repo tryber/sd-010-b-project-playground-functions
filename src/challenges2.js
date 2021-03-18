92d8805144e7d0629b5c45036997569de04b0f47
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-18 15:57:20 -0400

    Requisito 11, implementada função de número de telefone.

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,18 +12,51 @@ function techList(techno, name) {
         tech: techno[index],
         name: nome
       }
-
       newArray.push(objList);
     }
-
     return newArray;
   }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numberTel) {
+  let verificaTamanho = 0;
+  let auxIndex = 0;
 
+  if (numberTel.length != 11) {
+    return "Array com tamanho incorreto.";
+  } else {
+    for (let indexVerTamanho = 0; indexVerTamanho < numberTel.length; indexVerTamanho += 1) {
+      if (numberTel[indexVerTamanho] > 9 || numberTel[indexVerTamanho] < 0) {
+        verificaTamanho = 1;
+      }
+    }
+
+    if (verificaTamanho === 1) {
+      return "não é possível gerar um número de telefone com esses valores";
+    } else {
+      let formatedNumber = '';
+
+      for (let indexCreateNumber = 0; indexCreateNumber < 15; indexCreateNumber++) {
+        if (indexCreateNumber === 0) {
+          formatedNumber += "(";
+        } else if (indexCreateNumber === 3) {
+          formatedNumber += ")";
+        } else if (indexCreateNumber === 4) {
+          formatedNumber += " ";
+        } else if (indexCreateNumber === 10) {
+          formatedNumber += "-";
+        } else {
+          formatedNumber += numberTel[auxIndex];
+          auxIndex += 1;
+        }
+      }
+      return formatedNumber;
+    }
+  }
 }
+
+generatePhoneNumber();
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
 
