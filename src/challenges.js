e38dd361b8b66c9eceb85123d47aaa07c0f688b2
thosijulio <thosijulio@gmail.com>
2021-03-20 17:16:12 -0300

    correcao V3 exercicio 6

--- src/challenges.js
+++ src/challenges.js
@@ -35,8 +35,10 @@ function highestCount(arrayNumeros) {
   let maiorNumero;
   let quantidadeAparicoes = 0;
   for (let index = 0; index < arrayNumeros.length; index += 1) {
-    if (arrayNumeros[index] < arrayNumeros[index + 1]) {
-      maiorNumero = arrayNumeros[index + 1];
+    if(arrayNumeros[(index + 1)]) {
+      if (arrayNumeros[index] < arrayNumeros[index + 1]) {
+        maiorNumero = arrayNumeros[index + 1];
+      }
     }
   }
   for (let index = 0; index < arrayNumeros.length; index += 1) {
