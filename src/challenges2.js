2e861b52f3c6620667810f737be82c3d60aaab5b
denis-rossati <denis.rossati@hotmail.com>
2021-04-20 20:39:47 -0300

    commit final do dia

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,7 +6,7 @@ function techList(array, name) {
   array.push({ tech, name });
   return array;
  }, [])
- 
+
  if (newArray.length === 0) return 'Vazio!';
  return newArray;
 }
@@ -83,30 +83,9 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate(string) {
-  // seu código aqui
 
-  let cC = '';  //cC = checkCharacter
-  let storeNumbers= 0;
-  for (let i = 0; i < string.length; i += 1) {
-    cC = (string[i]);
-
-    if (cC == 1) {
-      storeNumbers += 1;
-    } else if (cC ==2) {
-      storeNumbers += 2
-    } else if (cC == 3) {
-      storeNumbers += 3
-    } else if (cC == 4) {
-      storeNumbers += 4
-    } else if (cC == 5) {
-      storeNumbers += 5
-    }
-  }
-  if (storeNumbers > 1) {
-    return (`${storeNumbers} copos de água`)
-  } else {
-    return (`${storeNumbers} copo de água`)
-  }
+  const split = string.split('');
+  return split.filter((item) => typeof item === 'number');
 }
 
 module.exports = {
