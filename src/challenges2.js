57920a7ef191956458d7d04755f09572a0f6e896
Vinicius <vini_bodra@hotmail.com>
2021-04-26 19:16:00 -0300

    Implementando generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,28 +3,60 @@ https://github.com/tryber/sd-09-project-playground-functions/blob/tkdcris-projec
 */
 
 // Desafio 10
-function techList(arei, name){
-
-    let objetoTechName = [];
-    if(arei.length!==0){
-        for(i in arei.sort()){
-            objetoTechName.push({tech: arei[i], name})
-        
-        }
-        return objetoTechName
-    } else {
-        return "Vazio!"
+function techList(arei, name) {
+  let objetoTechName = [];
+  if (arei.length !== 0) {
+    for (i in arei.sort()) {
+      objetoTechName.push({ tech: arei[i], name });
     }
+    return objetoTechName;
+  }
+  return 'Vazio!';
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function rejectTelephoneOne(numero) {
+  if (numero < 0) return 1;
+  if (numero > 9) return 1;
+  return 0;
 }
+
+function rejectTelephoneTwo(numero) {
+  let arrayComparativa = [];
+  let contadorDeLetras = 0;
+  arrayComparativa.push(numero);
+  for (let i = 0; i < arrayComparativa.length; i += 1) {
+    if (numero === arrayComparativa[i]) contadorDeLetras += 1;
+  }
+  if (contadorDeLetras >= 3) return 1;
+  return 0;
+}
+
+function telefonizadorDeNumeros(telefone) {
+  const telefoneConvertido = `(${telefone[0]}${telefone[1]})${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`;
+  return telefoneConvertido;
+}
+
+function generatePhoneNumber(telefone) {
+  if (telefone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let verificadorNumeros = 0;
+  telefone.forEach((numero) => {
+    verificadorNumeros += rejectTelephoneOne(numero);
+    verificadorNumeros += rejectTelephoneTwo(numero);
+  });
+  if (verificadorNumeros > 0) return 'não é possível gerar um número de telefone com esses valores';
+  return telefonizadorDeNumeros(telefone);
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
+
 }
 
 // Desafio 13
