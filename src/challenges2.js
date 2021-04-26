53e5cf39c7ad108ec364cdbe5ea815ccc4922543
Vinicius <vini_bodra@hotmail.com>
2021-04-26 20:03:40 -0300

    Implementando generatePhoneNumber.30k

--- src/challenges2.js
+++ src/challenges2.js
@@ -46,11 +46,13 @@ function generatePhoneNumber(telefone) {
     verificadorNumeros += rejectTelephoneOne(numero);
     verificadorNumeros += rejectTelephoneTwo(numero);
   });
+  arrayComparativa = [];
   if (verificadorNumeros > 0) return 'não é possível gerar um número de telefone com esses valores';
   return telefonizadorDeNumeros(telefone);
 }
 
-console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
+console.log(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(lado1, lado2, lado3) {
