c82b26b24ccc8ba8cb0659b1fc01a3f4ad3e336c
Lara Capila <laracapilak@gmail.com>
2021-04-27 12:12:11 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,9 +13,23 @@ const techList = (arrayTech, name) => {
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lara'));
 
 // Desafio 11
-function generatePhoneNumber() {
-
+function generatePhoneNumber(arr) {
+  const transformString = arr.join('');
+  const ddd = transformString.substring(0, 2);
+  const firstFiveNumbs = transformString.substring(2, 7);
+  const forNumbsEnd = transformString.substring(7, 11);
+  const result = `(${ddd})${firstFiveNumbs}-${forNumbsEnd}`;
+  const errorMessage = 'não é possível gerar um número de telefone com esses valores';
+  const errorMessage2 = 'Array com tamanho incorreto';
+  for (let key in arr) {
+    if (arr.length !== 11) {
+      console.log(errorMessage2); 
+    } else if ((arr[key] < 0) || (arr[key] > 9)) {
+      console.log(errorMessage);
+    } return result;
+  }
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2]));
 
 // Desafio 12
 function triangleCheck() {
